0b74ca6b7c0e3f0b9066852298a6e54a9112fadd
Adam <adamyoungy678@gmail.com>
2023-08-26 21:38:46 +0100

    refactor: Update OurClinic page with StickyCards component

--- src/app/(about)/clinic/page.tsx
+++ src/app/(about)/clinic/page.tsx
@@ -1,18 +1,11 @@
 import PageHero from '@/components/blocks/PageHero/PageHero';
-import ReviewLink from '@/components/blocks/ReviewLink/ReviewLink';
 import Button from '@/components/Button/Button';
 import { DisappearingFeatures } from '@/components/DisappearingScrollFeatures/DisappearingScrollFeatures';
 import { FloatingPhone } from '@/components/FloatingPhone/FloatingPhone';
-import GetDirectionsForm from '@/components/GetDirectionsForm/GetDirectionsForm';
 import PromotionForm from '@/components/PromotionForm/PromotionForm';
 import SteppedProgress from '@/components/SteppedProgress/SteppedProgress';
-import dynamic from 'next/dynamic';
+import { StickyCards } from '@/components/StickyCards/StickyCards';
 import Link from 'next/link';
-import { type JSX, type SVGProps } from 'react';
-import { Instagram } from 'react-feather';
-const GoogleMap = dynamic(async () => await import('@/components/blocks/GoogleMap/GoogleMap'), {
-  ssr: false,
-});
 
 export default function OurClinic() {
   return (
@@ -101,189 +94,9 @@ export default function OurClinic() {
         </div>
       </section>
 
-      {/* Virtual Tour (Coming Soon) Section */}
-      {/* <section className='py-16 md:py-24 lg:py-32'>
-        <div className='container mx-auto px-4 md:px-6 lg:px-8'>
-          <h2 className='text-3xl md:text-4xl lg:text-5xl font-bold mb-8 text-center'>Virtual Tour (Coming Soon)</h2>
-          <p className='text-lg md:text-xl lg:text-2xl text-muted-foreground mb-8 text-center'>
-            We’re working on providing a virtual tour of our clinic so you can explore our facilities from the comfort
-            of your home. Stay tuned for updates!
-          </p>
-        </div>
-      </section> */}
+      <StickyCards />
 
       <PromotionForm />
-
-      {/* Patient Testimonials (Coming Soon) Section */}
-      <section className='py-16 md:py-24 lg:py-32 bg-muted'>
-        <div className='container mx-auto px-4 md:px-6 lg:px-8'>
-          <h2 className='text-3xl md:text-4xl lg:text-5xl font-bold mb-8 text-center'>Had an Appointment with Us?</h2>
-          <p className='text-lg md:text-xl lg:text-2xl text-muted-foreground mb-8 text-center'>
-            Be one of the first to share your experience with Supernova Dental Clinic. We’d love to hear your feedback!
-          </p>
-          <div className='flex justify-center'>
-            <ReviewLink />
-          </div>
-        </div>
-      </section>
-
-      {/* Follow Us Section */}
-      <section className='py-16 md:py-24 lg:py-32 bg-lightGrey'>
-        <div className='container mx-auto px-4 md:px-6 lg:px-8'>
-          <h2 className='text-3xl md:text-4xl lg:text-5xl font-bold mb-8 text-center text-gold'>Keep in the loop!</h2>
-          <p className='text-lg md:text-xl lg:text-2xl text-cream mb-8 text-center'>
-            Come check out the Supernova Staff Instagram and tay updated with the latest news, promotions, and dental
-            care tips by following us on social media.
-          </p>
-          <Link href='/social'>
-            <Instagram className='w-12 h-12 text-gold mx-auto' />
-          </Link>
-        </div>
-      </section>
-
-      {/* <div className='flex flex-col items-center justify-start py-12 bg-gray-50'>
-        <div className='w-full max-w-7xl px-6 sm:px-8 lg:px-12'>
-          <section className='mb-12'>
-            <h1 className='text-4xl sm:text-5xl font-bold my-8 text-center sm:text-left text-grey'>
-              Find Supernova Dental Clinic
-            </h1>
-            <p className='text-lg sm:text-xl mb-6 text-center sm:text-left text-lightGrey'>
-              Locate Supernova Dental Clinic on the map. Find our address, contact information, and directions.
-            </p>
-
-            <div className='my-6'>
-              <GoogleMap />
-            </div>
-
-            <div className='grid grid-cols-1 md:grid-cols-2 gap-8'>
-              <div className='md:col-span-1 mt-12'>
-                <h2 className='text-2xl sm:text-3xl font-semibold mb-4 text-center sm:text-left text-gray'>
-                  Our Address
-                </h2>
-                <ul className='text-center sm:text-left'>
-                  <li className='mb-3'>Marsh Lane, Huntworth,</li>
-                  <li className='mb-3'>Bridgwater, Alliance Building</li>
-                  <li className='mb-3'>TA6 6LQ</li>
-                  <li className='mb-3 sm:max-w-80'>
-                    <GetDirectionsForm />
-                  </li>
-                </ul>
-              </div>
-
-              <div className='md:col-span-1 mt-12'>
-                <h2 className='text-2xl sm:text-3xl font-semibold mb-4 text-center sm:text-left text-gray'>
-                  Contact Us
-                </h2>
-                <div className='flex flex-col items-center sm:items-start'>
-                  <p className='text-base sm:text-lg mb-2 text-center sm:text-left text-lightGrey'>
-                    Phone: (123) 456-7890
-                  </p>
-                  <p className='text-base sm:text-lg mb-2 text-center sm:text-left text-lightGrey'>
-                    Email:{' '}
-                    <a href='mailto:info@supernovadentalclinic.com' className='underline'>
-                      info@supernovadentalclinic.com
-                    </a>
-                  </p>
-                  <p className='text-base sm:text-lg mb-2 text-center sm:text-left text-lightGrey'>
-                    Website:{' '}
-                    <a
-                      href='http://www.supernovadentalclinic.com'
-                      className='underline'
-                      target='_blank'
-                      rel='noopener noreferrer'
-                    >
-                      www.supernovadentalclinic.com
-                    </a>
-                  </p>
-                </div>
-              </div>
-            </div>
-          </section>
-        </div>
-      </div> */}
     </div>
-  );
-}
-
-function ClockIcon(props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) {
-  return (
-    <svg
-      {...props}
-      xmlns='http://www.w3.org/2000/svg'
-      width='24'
-      height='24'
-      viewBox='0 0 24 24'
-      fill='none'
-      stroke='currentColor'
-      strokeWidth='2'
-      strokeLinecap='round'
-      strokeLinejoin='round'
-    >
-      <circle cx='12' cy='12' r='10' />
-      <polyline points='12 6 12 12 16 14' />
-    </svg>
-  );
-}
-
-function SmileIcon(props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) {
-  return (
-    <svg
-      {...props}
-      xmlns='http://www.w3.org/2000/svg'
-      width='24'
-      height='24'
-      viewBox='0 0 24 24'
-      fill='none'
-      stroke='currentColor'
-      strokeWidth='2'
-      strokeLinecap='round'
-      strokeLinejoin='round'
-    >
-      <circle cx='12' cy='12' r='10' />
-      <path d='M8 14s1.5 2 4 2 4-2 4-2' />
-      <line x1='9' x2='9.01' y1='9' y2='9' />
-      <line x1='15' x2='15.01' y1='9' y2='9' />
-    </svg>
-  );
-}
-
-function StarIcon(props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) {
-  return (
-    <svg
-      {...props}
-      xmlns='http://www.w3.org/2000/svg'
-      width='24'
-      height='24'
-      viewBox='0 0 24 24'
-      fill='none'
-      stroke='currentColor'
-      strokeWidth='2'
-      strokeLinecap='round'
-      strokeLinejoin='round'
-    >
-      <polygon points='12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2' />
-    </svg>
-  );
-}
-
-function WifiIcon(props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) {
-  return (
-    <svg
-      {...props}
-      xmlns='http://www.w3.org/2000/svg'
-      width='24'
-      height='24'
-      viewBox='0 0 24 24'
-      fill='none'
-      stroke='currentColor'
-      strokeWidth='2'
-      strokeLinecap='round'
-      strokeLinejoin='round'
-    >
-      <path d='M12 20h.01' />
-      <path d='M2 8.82a15 15 0 0 1 20 0' />
-      <path d='M5 12.859a10 10 0 0 1 14 0' />
-      <path d='M8.5 16.429a5 5 0 0 1 7 0' />
-    </svg>
   );
 }
