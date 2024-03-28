446ee168665eb9604d8a8db893cb64009a513add
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-03-28 22:55:58 +0000

    render google map

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Cta from './components/blocks/Cta/Cta';
+import GoogleMap from './components/blocks/GoogleMap/GoogleMap';
 
 export default function Home() {
   return (
-    <main className='min-h-screen'>
+    <main className='min-h-screen flex flex-col gap-4'>
       <Cta />
+      <GoogleMap />
     </main>
   );
 }
