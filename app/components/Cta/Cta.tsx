3755343b7c8e1051690010bc9c4f1dd3651fdafb
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-03-28 17:20:12 +0000

    add component with video

---
+++ app/components/Cta/Cta.tsx
@@ -0,0 +1,14 @@
+export default function Cta() {
+  return (
+    <div className='h-screen w-screen overflow-hidden'>
+      <video
+        className='object-cover w-full h-full'
+        src='https://skylinedentaltucson.com/wp-content/uploads/2022/10/hero_loop_no_mask-1080p-compressed.mp4'
+        loop
+        autoPlay
+        playsInline
+        muted
+      />
+    </div>
+  );
+}
