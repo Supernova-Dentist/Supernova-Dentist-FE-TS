38f1e6fd80113deb1b878d830fb7491780bb17f4
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-03-28 18:32:18 +0000

    add hover effect to buttons

--- app/components/blocks/Cta/CtaButtons.tsx
+++ app/components/blocks/Cta/CtaButtons.tsx
@@ -4,12 +4,12 @@ export default function CtaButtons() {
   return (
     <div className='flex flex-wrap gap-6 items-center justify-center'>
       <Button
-        className='uppercase text-xl font-semibold bg-[#C29470] text-black px-4 py-2 w-[25rem]'
+        className='hover:opacity-90 transition-opacity duration-150 uppercase text-xl font-semibold bg-[#C29470] text-black px-4 py-2 w-[25rem]'
         text='Request appointment'
       />
       <Button
-        className='uppercase text-xl font-semibold bg-[#C29470] text-black px-4 py-2 w-[25rem]'
-        text='(01785) 555-555'
+        className='hover:opacity-90 transition-opacity duration-150 uppercase text-xl font-semibold bg-[#C29470] text-black px-4 py-2 w-[25rem]'
+        text='Call (01785) 555-555'
       />
     </div>
   );
