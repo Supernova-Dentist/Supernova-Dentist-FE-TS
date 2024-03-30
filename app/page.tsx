7cf8695196a6361865dd7c512606e3830dc3ba17
Adam Young <adamyoungy678@gmail.com>
2024-03-30 21:19:29 +0000

    feat: Add CrispChat component to page.tsx with a comment

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import CrispChat from './components/blocks/CrispChat/CrispChat';
 import Cta from './components/blocks/Cta/Cta';
 import Footer from './components/blocks/Footer/Footer';
 import GoogleMap from './components/blocks/GoogleMap/GoogleMap';
@@ -8,6 +9,8 @@ export default function Home() {
       <Cta />
       <GoogleMap />
       <Footer />
+      {/* Could possibly add an aria-hidden='true' to a div and put the CrispChat component inside  */}
+      <CrispChat />
     </main>
   );
 }
