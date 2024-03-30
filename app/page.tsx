8801e4b43f124bd47e6d323b6201dde54f3ec54a
Adam Young <adamyoungy678@gmail.com>
2024-03-30 23:45:34 +0000

    fix: added dynamic import to fix map loading state

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
 import CrispChat from './components/blocks/CrispChat/CrispChat';
 import Cta from './components/blocks/Cta/Cta';
 import Footer from './components/blocks/Footer/Footer';
-import GoogleMap from './components/blocks/GoogleMap/GoogleMap';
+// import GoogleMap from './components/blocks/GoogleMap/GoogleMap';
 import MainNav from './components/blocks/MainNav/MainNav';
+
+import dynamic from 'next/dynamic';
+
+const GoogleMap = dynamic(async () => await import('./components/blocks/GoogleMap/GoogleMap'), { ssr: false });
 
 export default function Home() {
   return (
