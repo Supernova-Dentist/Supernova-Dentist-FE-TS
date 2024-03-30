20df1805ed80ef8827b42f8d3441487786b8146a
Adam Young <adamyoungy678@gmail.com>
2024-03-30 22:23:16 +0000

    feat: Add MainNav to page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Cta from './components/blocks/Cta/Cta';
 import Footer from './components/blocks/Footer/Footer';
 import GoogleMap from './components/blocks/GoogleMap/GoogleMap';
+import MainNav from './components/blocks/MainNav/MainNav';
 
 export default function Home() {
   return (
     <main className='min-h-screen flex flex-col'>
+      <MainNav />
       <Cta />
       <GoogleMap />
       <Footer />
