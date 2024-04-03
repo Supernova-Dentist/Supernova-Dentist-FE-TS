8f3188e9f527e1a51907991f6d62419815bb8b85
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-04-03 20:58:53 +0100

    Added header and nav to root layout as custom Layou component would not show nav and footer on any other page except for home.

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,11 @@
 import Cta from './components/blocks/Cta/Cta';
 import GoogleMap from './components/blocks/GoogleMap/GoogleMap';
-import Layout from './components/blocks/Layout/Layout';
 
 export default function Home() {
   return (
-    <Layout>
+    <div>
       <Cta />
       <GoogleMap />
-    </Layout>
+    </div>
   );
 }
