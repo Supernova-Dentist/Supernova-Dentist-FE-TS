d7201f990cb2d02df9e5556ec05d027b623feb75
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-04-03 22:45:53 +0100

    render steps in home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Cta from './components/blocks/Cta/Cta';
 import GoogleMap from './components/blocks/GoogleMap/GoogleMap';
+import Steps from './components/blocks/Steps/Steps';
 
 export default function Home() {
   return (
     <div>
       <Cta />
+      <Steps />
       <GoogleMap />
     </div>
   );
