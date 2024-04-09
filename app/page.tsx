db35339326c1d7b8df0eb4a3219cf0d2cfb8da26
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-04-09 13:42:25 +0100

    fix import path

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import dynamic from 'next/dynamic';
 import Cta from './components/blocks/Cta/Cta';
 import Reviews from './components/blocks/Reviews/Review';
 import Steps from './components/blocks/Steps/Steps';
-import GalleryCarousel from './components/GalleryCarousel/GalleryCarousel';
+import GalleryCarousel from './components/blocks/GalleryCarousel/GalleryCarousel';
 import ScrollToTopButton from './components/ScrollToTopButton/ScrollToTopButton';
 const GoogleMap = dynamic(async () => await import('./components/blocks/GoogleMap/GoogleMap'), {
   ssr: false,
