82326edc1a2779e58bb060c984c7490504591e40
Adam <adamyoungy678@gmail.com>
2024-04-20 21:55:08 +0100

    Chore: Add type declarations for @heroicons/react/outline module

---
+++ types/heroicons.d.ts
@@ -0,0 +1,6 @@
+// types/heroicons.d.ts
+declare module '@heroicons/react/outline' {
+  import type React from 'react';
+  export const MenuIcon: React.FC<React.SVGProps<SVGSVGElement>>;
+  export const XIcon: React.FC<React.SVGProps<SVGSVGElement>>;
+}
