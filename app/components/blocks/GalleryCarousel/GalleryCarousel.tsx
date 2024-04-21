96b420bcd760428a6aa3cf021d7bb87f07009d5b
Adam <adamyoungy678@gmail.com>
2024-04-21 10:29:08 +0100

    Refactor: Improve responsiveness and styling of GalleryCarousel component

--- app/components/blocks/GalleryCarousel/GalleryCarousel.tsx
+++ app/components/blocks/GalleryCarousel/GalleryCarousel.tsx
@@ -17,16 +17,21 @@ export default function GalleryCarousel() {
         See pictures of our state-of-the-art facility
       </h2>
       <div className='w-full mx-auto px-4 lg:px-20'>
-        <Carousel opts={{ loop: true }} plugins={[Autoplay({ delay: 5000 })]} className='w-full'>
+        <Carousel
+          opts={{ loop: true }}
+          plugins={[Autoplay({ delay: 5000 })]}
+          className='w-full cursor-grab'
+        >
           <CarouselContent>
             {Array.from({ length: 8 }).map((_, index) => (
               <CarouselItem
                 key={`Carousel image ${index + 1}`}
-                className='w-full sm:basis-1/2 lg:basis-1/3 xl:basis-1/4'
+                className='w-full sm:basis-1/2 lg:basis-1/3'
               >
                 <div className='p-1'>
                   <Card>
-                    <CardContent className='flex items-center justify-center p-0 aspect-auto'>
+                    <CardContent className='flex items-center justify-center p-0 aspect-w-16 aspect-h-9'>
+                      {/* TODO: ADD NEXTJS IMAGE COMPONENT HERE */}
                       <img
                         className='object-cover w-full h-full'
                         src={`/assets/images/carousel-${index + 1}.png`}
