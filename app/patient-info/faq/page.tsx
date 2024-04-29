b161fd799cd0f42e99bdbaffb7692737f5f47b1d
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-04-29 14:38:57 +0100

    add patient info dummy pages

---
+++ app/patient-info/faq/page.tsx
@@ -0,0 +1,3 @@
+export default function Faq() {
+  return <h1 className='h-screen flex justify-center text-3xl mt-12'>Faq page</h1>;
+}
---
+++ app/patient-info/insurance/page.tsx
@@ -0,0 +1,3 @@
+export default function Insurance() {
+  return <h1 className='h-screen flex justify-center text-3xl mt-12'>Insurance page</h1>;
+}
