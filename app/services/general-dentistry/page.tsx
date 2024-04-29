e178d5a655f185248a888b557ccfbfeb30eb4ec5
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-04-29 14:39:06 +0100

    add services dummy pages

---
+++ app/services/general-dentistry/page.tsx
@@ -0,0 +1,3 @@
+export default function GeneralDentistry() {
+  return <h1 className='h-screen flex justify-center text-3xl mt-12'>General dentistry page</h1>;
+}
---
+++ app/services/orthodontics/page.tsx
@@ -0,0 +1,3 @@
+export default function Orthodontics() {
+  return <h1 className='h-screen flex justify-center text-3xl mt-12'>Orthodontics page</h1>;
+}
