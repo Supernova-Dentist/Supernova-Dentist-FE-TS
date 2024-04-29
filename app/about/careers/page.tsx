9e349fed7a69a468e0aa327c7deb1195dbda2544
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-04-29 14:38:26 +0100

    add about dummy pages

---
+++ app/about/careers/page.tsx
@@ -0,0 +1,3 @@
+export default function Careers() {
+  return <h1 className='h-screen flex justify-center text-3xl mt-12'>Careers page</h1>;
+}
---
+++ app/about/clinic/page.tsx
@@ -0,0 +1,3 @@
+export default function Clinic() {
+  return <h1 className='h-screen flex justify-center text-3xl mt-12'>Clinic page</h1>;
+}
