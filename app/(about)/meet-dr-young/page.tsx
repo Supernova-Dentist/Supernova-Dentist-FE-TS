b6fb7599b476d17c810d61e77158772576285386
Adam <adamyoungy678@gmail.com>
2024-04-29 20:59:26 +0100

    Feat: Add new page for Dr. Young in about section

---
+++ app/(about)/meet-dr-young/page.tsx
@@ -0,0 +1,10 @@
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Supernova Dental | Team',
+  description: 'Meet the founder of Supernova Dental',
+};
+
+export default function meetDoctorYoung() {
+  return <h1 className='h-screen flex justify-center text-3xl mt-12'>Scott page</h1>;
+}
