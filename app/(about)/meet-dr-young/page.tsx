2e65d640feb25ebb3ef0c60b2c893d07e8dfbff2
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-05-07 15:28:31 +0100

    refactor pages to use new components

--- app/(about)/meet-dr-young/page.tsx
+++ app/(about)/meet-dr-young/page.tsx
@@ -1,11 +1,14 @@
 import type { Metadata } from 'next';
+import dynamic from 'next/dynamic';
 import Steps from '@/components/blocks/Steps/Steps';
 import GalleryCarousel from '@/components/blocks/GalleryCarousel/GalleryCarousel';
-import dynamic from 'next/dynamic';
 import PageHero from '@/components/blocks/PageHero/PageHero';
 import ImageAndText from '@/components/blocks/ImageAndText/ImageAndText';
+import DefaultTextBox from '@/components/blocks/ImageAndText/DefaultTextBox';
+import TextBox from '@/components/TextBoxContainer/TextBox';
 import dummyDentist from '../../../public/assets/images/dummy-dentist.jpeg';
 import dummyDentistTwo from '../../../public/assets/images/meet-dr-young-2.png';
+import textBoxContent from './textBoxContent';
 const GoogleMap = dynamic(async () => await import('@/components/blocks/GoogleMap/GoogleMap'), {
   ssr: false,
 });
@@ -16,8 +19,6 @@ export const metadata: Metadata = {
 };
 
 export default function MeetDoctorYoung() {
-  const titleText = { emphasis: 'About', main: 'Dr. Young' };
-
   return (
     <>
       <PageHero
@@ -29,19 +30,17 @@ export default function MeetDoctorYoung() {
         <ImageAndText
           src={dummyDentist}
           alt='Dr Young'
-          title={titleText}
-          descriptionOne='Dr. James Raymond is now the fifth dentist in his family, with his father, uncle, brother, and sister sharing in the profession. He recognized at an early age the amazing gift his father was able to share with the community and the appreciation and many friendships that developed through a lifetime of service in dentistry. Dr. Raymond attended the University of North Carolina at Chapel Hill and graduated from the honors program with a B.S. in Biology and attended Tufts University School of Dental Medicine. From his initial courses at Tufts, Dr. Raymond developed a passion for esthetic and restorative dentistry and he excelled at crown, bridge, and implant prostheses. He continues his professional studies by attending local monthly study clubs, including the Spear Study Club and Pima Study Club. He is a graduate of the Kois Center in Seattle, WA, a distinction few dentists achieve.'
+          textBox={
+            <DefaultTextBox
+              title='Dr. Young'
+              emphasis='About'
+              descriptionOne='Dr. James Raymond is now the fifth dentist in his family, with his father, uncle, brother, and sister sharing in the profession. He recognized at an early age the amazing gift his father was able to share with the community and the appreciation and many friendships that developed through a lifetime of service in dentistry. Dr. Raymond attended the University of North Carolina at Chapel Hill and graduated from the honors program with a B.S. in Biology and attended Tufts University School of Dental Medicine. From his initial courses at Tufts, Dr. Raymond developed a passion for esthetic and restorative dentistry and he excelled at crown, bridge, and implant prostheses. He continues his professional studies by attending local monthly study clubs, including the Spear Study Club and Pima Study Club. He is a graduate of the Kois Center in Seattle, WA, a distinction few dentists achieve.'
+            />
+          }
         />
-        <ImageAndText
-          src={dummyDentistTwo}
-          alt='Dr Young'
-          descriptionOne='Beyond his work at the office, Dr. Raymond is passionate about his involvement in dental outreach programs, both within the United States and abroad. During his most recent mission trip abroad, Dr. Raymond spent 10 days in the Dominican Republic, providing free dental care to underserved children. He also serves on the board of the Arizona Dental Foundation and spearheaded the dental charity TDC Cares (Tucson Dental Community Cares) providing services to veterans and job-seeking homeless citizens of Tucson. Learn more about Skyline Dental’s charity and community efforts here.'
-          descriptionTwo='On the weekends, you will find Dr. Raymond either trail running in the Catalinas or sitting atop Mt. Lemmon, pondering the existence of pine trees in the desert with his wife, Jessie, and dog, Rudy. Apart from spending time with his family, Dr. Raymond loves traveling, yoga, surfing, and golf.'
-          imagePosition='right'
-          containerBg='bg-white'
-          titleColor='text-black'
-          descriptionColor='text-black'
-        />
+        <ImageAndText src={dummyDentistTwo} alt='Dr Young' imagePosition='right'>
+          <TextBox textOne={textBoxContent.textOne} textTwo={textBoxContent.textTwo} />
+        </ImageAndText>
       </div>
       <div className='my-24'>
         <Steps />
--- app/(about)/team/page.tsx
+++ app/(about)/team/page.tsx
@@ -1,11 +1,15 @@
-import dynamic from 'next/dynamic';
 import type { Metadata } from 'next';
+import dynamic from 'next/dynamic';
 import Steps from '@/components/blocks/Steps/Steps';
 import GalleryCarousel from '@/components/blocks/GalleryCarousel/GalleryCarousel';
-import dummyDentist from '../../../public/assets/images/dummy-dentist.jpeg';
-import dummyTeam from '../../../public/assets/images/dummy-team-2.jpeg';
 import PageHero from '@/components/blocks/PageHero/PageHero';
 import ImageAndText from '@/components/blocks/ImageAndText/ImageAndText';
+import DefaultTextBox from '@/components/blocks/ImageAndText/DefaultTextBox';
+import TextBox from '@/components/TextBoxContainer/TextBox';
+import Button from '@/components/Button/Button';
+import textBoxContent from './textBoxContent';
+import dummyDentist from '../../../public/assets/images/dummy-dentist.jpeg';
+import dummyTeam from '../../../public/assets/images/dummy-team-2.jpeg';
 const GoogleMap = dynamic(async () => await import('@/components/blocks/GoogleMap/GoogleMap'), {
   ssr: false,
 });
@@ -16,9 +20,6 @@ export const metadata: Metadata = {
 };
 
 export default function Team() {
-  const titleTextOne = { emphasis: 'About', main: 'Supernova Dental' };
-  const titleTextTwo = { main: 'Dr Young' };
-
   return (
     <>
       <PageHero bgImage='bg-team-cta' title='Meet the team' description='Behind Supernova Dental' />
@@ -26,26 +27,28 @@ export default function Team() {
         <ImageAndText
           src={dummyTeam}
           alt='Meet the team'
-          title={titleTextOne}
-          descriptionOne="We offer dental services in Tucson, AZ, ranging from general and family dentistry to cosmetic
-                procedures. We use state-of-the-art technology along with trusted techniques to give you the bright and
-                healthy smile you've always dreamed of."
-          descriptionTwo="One of the main things that sets us apart from other dental practices is our passionate, skilled staff.
-                Our team is comprised of talented dental professionals that are committed to providing personalized,
-                high-quality care with service that will leave you smiling. From the front desk to the dental chair, you
-                can rest assured that you're in good hands!"
+          textBox={
+            <DefaultTextBox
+              title='Supernova Dental'
+              emphasis='About'
+              descriptionOne="We offer dental services in Tucson, AZ, ranging from general and family dentistry to cosmetic
+          procedures. We use state-of-the-art technology along with trusted techniques to give you the bright and
+          healthy smile you've always dreamed of."
+              descriptionTwo="One of the main things that sets us apart from other dental practices is our passionate, skilled staff.
+          Our team is comprised of talented dental professionals that are committed to providing personalized,
+          high-quality care with service that will leave you smiling. From the front desk to the dental chair, you
+          can rest assured that you're in good hands!"
+            />
+          }
         />
-        <ImageAndText
-          src={dummyDentist}
-          alt='Dentist'
-          title={titleTextTwo}
-          descriptionOne="“I'm so proud of my team here at Skyline Dental. We do such great work together serving our
-                patients and our community!”"
-          containerBg='bg-white'
-          descriptionOneColor='text-black'
-          titleColor='text-black'
-          imagePosition='right'
-        />
+        <ImageAndText src={dummyDentist} alt='Dentist' imagePosition='right'>
+          <TextBox title={textBoxContent.title} textOne={textBoxContent.textOne}>
+            <Button
+              className='hover:opacity-90 transition-opacity duration-150 uppercase text-xl bg-gold text-black px-4 py-2 sm:w-fit w-full mt-8'
+              text="Read Dr. Young's bio"
+            />
+          </TextBox>
+        </ImageAndText>
       </div>
       <div className='my-24'>
         <Steps />
