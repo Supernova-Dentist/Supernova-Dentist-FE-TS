7352263bf12c5bf6d295bc0d1066e766e102a5b8
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-05-07 15:28:10 +0100

    small refactor to use render props and child prop

--- app/components/blocks/ImageAndText/ImageAndText.tsx
+++ app/components/blocks/ImageAndText/ImageAndText.tsx
@@ -1,50 +1,25 @@
 import Image, { type StaticImageData } from 'next/image';
-import TextBox from './TextBox';
 
 type ImageAndTextProps = {
   src: StaticImageData | string;
   alt: string;
-  descriptionOne: string;
-  descriptionTwo?: string;
   imagePosition?: 'left' | 'right';
-  title?: {
-    main: string;
-    emphasis?: string;
-  };
-  containerBg?: string;
-  descriptionColor?: string;
-  titleColor?: string;
+  children?: React.ReactNode;
+  textBox?: React.ReactNode;
 };
 
-export default function ImageAndText({
-  src,
-  alt,
-  title,
-  descriptionOne,
-  descriptionTwo,
-  imagePosition = 'left',
-  containerBg,
-  descriptionColor,
-  titleColor,
-}: ImageAndTextProps) {
+// Can still improve this components customizability
+export default function ImageAndText({ src, alt, children, textBox, imagePosition = 'left' }: ImageAndTextProps) {
   return (
     <div
       className={`flex flex-col lg:flex-row w-full min-h-[420px] ${
         imagePosition === 'right' ? 'lg:flex-row-reverse' : ''
       }`}
     >
-      <div className='relative w-full lg:w-1/2 h-[420px] lg:h-[550px]'>
+      <div className='relative w-full lg:w-1/2 min-h-[420px] lg:min-h-[550px]'>
         <Image className='object-cover' fill src={src} alt={alt} />
       </div>
-      <TextBox
-        main={title?.main}
-        emphasis={title?.emphasis}
-        descriptionOne={descriptionOne}
-        descriptionTwo={descriptionTwo}
-        containerBg={containerBg}
-        descriptionColor={descriptionColor}
-        titleColor={titleColor}
-      />
+      {textBox ?? children}
     </div>
   );
 }
