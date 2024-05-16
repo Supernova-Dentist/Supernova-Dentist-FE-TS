6444b25dbde5e64f632e9c28385779eb53d3007c
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-05-16 15:44:42 +0100

    re center cta text on home page

--- app/components/blocks/Cta/CtaText.tsx
+++ app/components/blocks/Cta/CtaText.tsx
@@ -9,11 +9,13 @@ export default function CtaText({ title, description, uppercase }: CtaTextProps)
   const defaultDescription = 'A better life through better dentistry';
 
   return (
-    <div className='flex flex-col items-center gap-4 max-w-[50rem] px-4'>
-      <h1 className='uppercase text-6xl text-slate-100 text-center'>{title ?? defaultTitle}</h1>
-      <p className={`${uppercase !== undefined ? 'uppercase' : null} text-2xl text-slate-100 text-center`}>
-        {description ?? defaultDescription}
-      </p>
+    <div className='flex justify-center'>
+      <div className='flex flex-col items-center gap-4 max-w-[50rem] px-4'>
+        <h1 className='uppercase text-6xl text-slate-100 text-center'>{title ?? defaultTitle}</h1>
+        <p className={`${uppercase !== undefined ? 'uppercase' : null} text-2xl text-slate-100 text-center`}>
+          {description ?? defaultDescription}
+        </p>
+      </div>
     </div>
   );
 }
