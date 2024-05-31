30324db543e8b383aa115178d9f9e7555c7b4187
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-05-31 18:28:51 +0100

    Small changes to mock data to test different review lengths

--- app/components/GoogleReview/googleReviewMockData.ts
+++ app/components/GoogleReview/googleReviewMockData.ts
@@ -9,47 +9,47 @@ export default [
   },
   {
     id: 2,
-    name: 'John Smith',
-    date: '02-05-23',
+    name: 'David Jones',
+    date: '19-07-23',
     rating: 4,
     review:
-      "I've been searching for a dentist that I felt comfortable with for years and years & can confidently say they exist- here at Supernova. They explained the steps of the appointment, give me a heads up when something might be uncomfortable, made sure throughout my cleaning and exam that I was comfortable and doing OK, and at no point pressured me to buy any cosmetic or more intensive procedures (yes, I've had both of those experiences elsewhere). Most importantly, they didn't talk down to me with any aspect of my appointment (big time have had this experience elsewhere). I'm so happy I've finally found my dentist I won't dread going to every 6 months!",
+      'Supernova Dental is such a great find. They have it all here. The dentist is very personable, down to earth, and serious about dental health, and the overall health of his patients. The hygienist are very informative and have great bedside manner, complimenting the Dentists sentiments and his recommendations. The office itself is just sooooooo beautiful! Simple and beautiful. When you get a cleaning they give you a tote. Im so excited for my next visit. Thanks so much !',
   },
   {
     id: 3,
-    name: 'John Smith',
-    date: '02-05-23',
+    name: 'Adam Tate',
+    date: '09-02-23',
     rating: 5,
     review:
-      "I've been searching for a dentist that I felt comfortable with for years and years & can confidently say they exist- here at Supernova. They explained the steps of the appointment, give me a heads up when something might be uncomfortable, made sure throughout my cleaning and exam that I was comfortable and doing OK, and at no point pressured me to buy any cosmetic or more intensive procedures (yes, I've had both of those experiences elsewhere). Most importantly, they didn't talk down to me with any aspect of my appointment (big time have had this experience elsewhere). I'm so happy I've finally found my dentist I won't dread going to every 6 months!",
+      "I was looking for a dentist in the area and found Dr. Pham's practice with so many 5 star reviews. He deserves them! The office is clean and modern and the doctor and staff are all super nice. Dr. Pham is very conscious of any possible discomfort and checks that his patients are okay while he's working. When I needed additional scans taken for my replacement crown, I didn't hear the voicemail until after the office was closed. They were able to text me that they had an appointment available the next day they were open, which is super convenient! I have experienced some anxiety when visiting dentists in the past, but Dr. Pham has relieved that anxiety quite a bit!",
   },
   {
     id: 4,
-    name: 'John Smith',
-    date: '02-05-23',
+    name: 'Liam Becker',
+    date: '22-09-23',
     rating: 4,
     review:
-      "I've been searching for a dentist that I felt comfortable with for years and years & can confidently say they exist- here at Supernova. They explained the steps of the appointment, give me a heads up when something might be uncomfortable, made sure throughout my cleaning and exam that I was comfortable and doing OK, and at no point pressured me to buy any cosmetic or more intensive procedures (yes, I've had both of those experiences elsewhere). Most importantly, they didn't talk down to me with any aspect of my appointment (big time have had this experience elsewhere). I'm so happy I've finally found my dentist I won't dread going to every 6 months!",
+      'Dr. Young was AMAZING!!!! And his assistant Leanne (I hope I am spelling that right) so sweet!! I generally have anxiety going to the dentist and I felt at easy and comfortable. Dr.Pham walked me through everything and was very attentive! I will DEFINITELY be back 🙂',
   },
   {
     id: 5,
-    name: 'John Smith',
-    date: '02-05-23',
+    name: 'Benjamin James',
+    date: '20-12-23',
     rating: 4,
     review:
       "I've been searching for a dentist that I felt comfortable with for years and years & can confidently say they exist- here at Supernova. They explained the steps of the appointment, give me a heads up when something might be uncomfortable, made sure throughout my cleaning and exam that I was comfortable and doing OK, and at no point pressured me to buy any cosmetic or more intensive procedures (yes, I've had both of those experiences elsewhere). Most importantly, they didn't talk down to me with any aspect of my appointment (big time have had this experience elsewhere). I'm so happy I've finally found my dentist I won't dread going to every 6 months!",
   },
   {
     id: 6,
-    name: 'John Smith',
-    date: '02-05-23',
+    name: 'Katy Davis',
+    date: '12-01-23',
     rating: 5,
     review:
       "I've been searching for a dentist that I felt comfortable with for years and years & can confidently say they exist- here at Supernova. They explained the steps of the appointment, give me a heads up when something might be uncomfortable, made sure throughout my cleaning and exam that I was comfortable and doing OK, and at no point pressured me to buy any cosmetic or more intensive procedures (yes, I've had both of those experiences elsewhere). Most importantly, they didn't talk down to me with any aspect of my appointment (big time have had this experience elsewhere). I'm so happy I've finally found my dentist I won't dread going to every 6 months!",
   },
   {
     id: 7,
-    name: 'John Smith',
+    name: 'Mollie Johnson',
     date: '02-05-23',
     rating: 4,
     review:
@@ -57,15 +57,15 @@ export default [
   },
   {
     id: 8,
-    name: 'John Smith',
-    date: '02-05-23',
+    name: 'Isaac Kennedy',
+    date: '30-03-23',
     rating: 4,
     review:
       "I've been searching for a dentist that I felt comfortable with for years and years & can confidently say they exist- here at Supernova. They explained the steps of the appointment, give me a heads up when something might be uncomfortable, made sure throughout my cleaning and exam that I was comfortable and doing OK, and at no point pressured me to buy any cosmetic or more intensive procedures (yes, I've had both of those experiences elsewhere). Most importantly, they didn't talk down to me with any aspect of my appointment (big time have had this experience elsewhere). I'm so happy I've finally found my dentist I won't dread going to every 6 months!",
   },
   {
     id: 9,
-    name: 'John Smith',
+    name: 'Natasha Foster',
     date: '02-05-23',
     rating: 4,
     review:
