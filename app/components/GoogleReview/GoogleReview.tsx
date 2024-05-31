3a1887283af1b601f8b5d38c2095f79953d76dd3
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-05-31 16:05:56 +0100

    add hide and show full review functionality + review hover effect

--- app/components/GoogleReview/GoogleReview.tsx
+++ app/components/GoogleReview/GoogleReview.tsx
@@ -1,4 +1,7 @@
+'use client';
+
 import Image from 'next/image';
+import { useState } from 'react';
 import { IoStar } from 'react-icons/io5';
 
 type GoogleReviewProps = {
@@ -9,35 +12,48 @@ type GoogleReviewProps = {
 };
 
 export default function GoogleReview({ name, date, review, rating }: GoogleReviewProps) {
+  const [isExpanded, setIsExpanded] = useState(false);
   const reviewStars = Array(rating).fill(0);
 
+  const toggleReadMore = () => {
+    setIsExpanded(!isExpanded);
+  };
+
   return (
-    <div className='bg-slate-50 p-5 min-w-[230px]  overflow-hidden'>
-      <div className='flex flex-col'>
-        <div className='flex  justify-between gap-2'>
-          <span className='text-sm leading-none font-semibold'>{name}</span>
-          <Image
-            src='/assets/svgs/GoogleTrustIcon.svg'
-            width={48}
-            height={48}
-            className='w-[20px] h-[20px]'
-            alt='google trust'
-          />
+    <div className='min-w-[230px] flex flex-col justify-between transform transition-transform duration-300 ease-in-out hover:-translate-y-2'>
+      <div className='bg-slate-50 p-5 shadow-md cursor-pointer rounded-sm'>
+        <div className='flex flex-col'>
+          <div className='flex justify-between gap-2'>
+            <span className='text-sm leading-none'>{name}</span>
+            <Image
+              src='/assets/svgs/GoogleTrustIcon.svg'
+              width={48}
+              height={48}
+              className='w-[20px] h-[20px]'
+              alt='google trust'
+            />
+          </div>
+          <span className='text-gray-500 text-xs leading-none'>{date}</span>
         </div>
-        <span className='text-gray-500 text-xs leading-none'>{date}</span>
-      </div>
-      <div className='flex mt-4 mb-2'>
-        {reviewStars.map((_, i) => (
-          <IoStar color='#F6BB06' key={i} />
-        ))}
-      </div>
-      <div>
-        <p className='text-[15px] max-h-[87px] overflow-y-hidden'>{review}</p>
-      </div>
-      <div>
-        <button type='button' className='text-xs text-gray-500 leading-none'>
-          Read more
-        </button>
+        <div className='flex mt-4 mb-2'>
+          {reviewStars.map((_, i) => (
+            <IoStar color='#F6BB06' key={i} />
+          ))}
+        </div>
+        <div>
+          <p
+            className={`text-[15px] ${
+              isExpanded ? 'max-h-auto' : 'max-h-[87px] overflow-hidden'
+            } transition-all duration-300`}
+          >
+            {review}
+          </p>
+        </div>
+        <div>
+          <span className='text-xs text-gray-500 leading-none cursor-pointer' onClick={toggleReadMore}>
+            {isExpanded ? 'Hide' : 'Read more'}
+          </span>
+        </div>
       </div>
     </div>
   );
--- app/components/blocks/GoogleReviews/GoogleReviews.tsx
+++ app/components/blocks/GoogleReviews/GoogleReviews.tsx
@@ -3,7 +3,7 @@ import googleReviewMockData from '@/components/GoogleReview/googleReviewMockData
 
 export default function GoogleReviews() {
   return (
-    <div className='flex gap-4 my-24 justify-center items-center bg-cream px4 py-12'>
+    <div className='flex gap-4 my-24 justify-center bg-cream px-4 py-12'>
       {googleReviewMockData.map(({ id, name, review, rating, date }) => (
         <GoogleReview key={id} name={name} review={review} rating={rating} date={date} />
       ))}
