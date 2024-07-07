f0d35d1c30e06389c8faa40faf35b34addd2d42c
Adam <adamyoungy678@gmail.com>
2024-07-07 21:49:35 +0100

    Refactor: Update colors to use variables in insta modal

--- app/(about)/social/utils/InstagramPostModal.tsx
+++ app/(about)/social/utils/InstagramPostModal.tsx
@@ -23,10 +23,7 @@ export default function InstagramPostModal({ post, onClose }) {
 
   return (
     <div className='fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50'>
-      <div
-        ref={modalRef}
-        className='bg-white rounded-lg overflow-hidden shadow-xl max-w-3xl w-full aspect-w-1 aspect-h-1'
-      >
+      <div ref={modalRef} className='rounded-lg overflow-hidden shadow-xl max-w-3xl w-full aspect-w-1 aspect-h-1'>
         <div className='relative w-full h-full'>
           <button className='absolute top-2 right-2 text-gray-500 hover:text-gray-700 z-50' onClick={onClose}>
             X
@@ -34,18 +31,18 @@ export default function InstagramPostModal({ post, onClose }) {
           {/* Mock image */}
           <img src={mockImageUrl} alt='Instagram Post' className='w-full h-full object-cover rounded-t-lg' />
         </div>
-        <div className='p-6'>
+        <div className='p-6 bg-lightGrey'>
           <div className='h-20 overflow-y-auto'>
-            <p className='text-sm mb-2'>{post.caption}</p>
+            <p className='text-sm mb-2 text-cream'>{post.caption}</p>
           </div>
           <div className='flex items-center justify-between'>
             <div className='flex items-center gap-2'>
               <HeartIcon className='w-5 h-5 text-red-500' />
-              <span className='text-sm font-medium'>{post.likes}</span>
+              <span className='text-sm font-medium text-cream'>{post.likes}</span>
             </div>
             <div className='flex items-center gap-2'>
-              <ChatBubbleIcon className='w-5 h-5' />
-              <span className='text-sm font-medium'>{post.comments}</span>
+              <ChatBubbleIcon className='w-5 h-5 text-cream' />
+              <span className='text-sm font-medium text-cream'>{post.comments}</span>
             </div>
           </div>
         </div>
