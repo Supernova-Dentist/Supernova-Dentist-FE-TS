0e2572b3bf96f4402c8797dd9b10e1115dc20729
Adam Young <adamyoungy678@gmail.com>
2024-07-13 16:23:36 +0100

    feat: Add pricing items data to Pricing page

---
+++ app/(about)/pricing/utils/pricingItems.ts
@@ -0,0 +1,17 @@
+export const pricingItems: PricingItem[] = [
+  {
+    title: 'Basic',
+    price: '$10',
+    description: 'This is a basic plan',
+  },
+  {
+    title: 'Pro',
+    price: '$20',
+    description: 'This is a pro plan',
+  },
+  {
+    title: 'Enterprise',
+    price: '$30',
+    description: 'This is an enterprise plan',
+  },
+];
