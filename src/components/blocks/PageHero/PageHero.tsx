8ddd1f4647031ed8292bc64f9887434b40712493
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-07-14 16:49:20 +0100

    add props for more control over UI

--- src/components/blocks/PageHero/PageHero.tsx
+++ src/components/blocks/PageHero/PageHero.tsx
@@ -4,15 +4,23 @@ import CtaText from '../Cta/CtaText';
 type PageHeroProps = {
   bgImage?: string;
   title: string;
-  description: string;
+  description?: string;
+  showButtons?: boolean;
+  className?: string;
 };
 
-export default function PageHero({ bgImage, title, description }: PageHeroProps) {
+export default function PageHero({
+  bgImage,
+  title,
+  description,
+  className = 'bg-center-top',
+  showButtons = true,
+}: PageHeroProps) {
   return (
-    <div className={`${bgImage} h-[650px] w-full bg-no-repeat bg-cover bg-center-top`}>
+    <div className={`${bgImage} h-[650px] w-full bg-no-repeat bg-cover ${className}`}>
       <div className='flex flex-col justify-center items-center gap-20 h-full'>
         <CtaText title={title} description={description} />
-        <CtaButtons />
+        {showButtons ? <CtaButtons /> : null}
       </div>
     </div>
   );
