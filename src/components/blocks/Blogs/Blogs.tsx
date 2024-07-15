4b7711054cc85a523d2f1eecd18af03c171e7846
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-07-15 12:13:10 +0100

    mobile responsiveness

--- src/components/blocks/Blogs/Blogs.tsx
+++ src/components/blocks/Blogs/Blogs.tsx
@@ -4,9 +4,9 @@ import blogMockData from './blogMockData';
 
 export default function Blogs() {
   return (
-    <section className='mx-auto max-w-[1460px] my-20'>
+    <section className='mx-auto max-w-[1556px] my-20 px-6 sm:px-12'>
       <div className='flex justify-center'>
-        <div className='flex flex-wrap gap-12'>
+        <div className='flex flex-wrap gap-12 justify-center'>
           {blogMockData.map(({ id, title, date, description }) => (
             <React.Fragment key={id}>
               <BlogBox id={id} title={title} date={date} description={description} />
