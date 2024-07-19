f58e5360b92de6dab5bae997347409f8f0e3a8b7
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-07-19 14:47:59 +0100

    fetch all blog posts from wordpress api

--- src/components/blocks/Blogs/Blogs.tsx
+++ src/components/blocks/Blogs/Blogs.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import BlogBox from '@/components/BlogBox/BlogBox';
-import blogMockData from './blogMockData';
 
 // Layout for blogs is a hacky atm. Consider using 3 columns instead or a grid system
-export default function Blogs() {
+export default async function Blogs() {
+  const res = await fetch('https://public-api.wordpress.com/wp/v2/sites/0xkamecd6.wordpress.com/posts');
+  const data = await res.json();
+
   return (
     <section className='mx-auto max-w-[1556px] my-20 px-6 sm:px-12'>
       <div className='flex justify-center'>
         <div className='flex flex-wrap gap-12 justify-center'>
-          {blogMockData.map(({ id, title, date, description }) => (
-            <React.Fragment key={id}>
-              <BlogBox id={id} title={title} date={date} description={description} />
+          {data?.map((post: any) => (
+            <React.Fragment key={post.id}>
+              <BlogBox id={post.id} title={post.title.rendered} date={post.date} />
             </React.Fragment>
           ))}
         </div>
