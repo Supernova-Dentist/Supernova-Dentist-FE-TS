18ae84587ccac1b6cd98a6ab96a9610b79383fd5
Adam Young <adamyoungy678@gmail.com>
2024-07-20 22:56:00 +0100

    refactor: Update fetchMediaById to use NEXT_PUBLIC_WORDPRESS_API_BASE_URL and use posts endpoint

--- src/services/wordpress/fetchMediaById.ts
+++ src/services/wordpress/fetchMediaById.ts
@@ -1,10 +1,12 @@
 export default async function fetchMediaById(id) {
   try {
-    const res = await fetch(`${process.env.WORDPRESS_API_BASE_URL}/wp/v2/media/${id}`);
+    const res = await fetch(`${process.env.NEXT_PUBLIC_WORDPRESS_API_BASE_URL}/posts/${id}`);
     if (!res.ok) {
       throw new Error(`HTTP error! Status: ${res.status}`);
     }
     const data = await res.json();
+    // Ensure the class_list is included in the returned data
+
     return data;
   } catch (error) {
     console.error(`Error fetching media file with ID ${id}:`, error);
