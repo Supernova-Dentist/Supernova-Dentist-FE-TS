4f2b097e6fc7e15ee1c4b1f30fb4a4fd20d3ee60
Adam Young <adamyoungy678@gmail.com>
2024-07-21 01:14:45 +0100

    feat: Add MacbookScroll component to home page

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import GalleryCarousel from './components/blocks/GalleryCarousel/GalleryCarousel
 import GoogleReviews from './components/blocks/GoogleReviews/GoogleReviews';
 import Reviews from './components/blocks/Reviews/Review';
 import Steps from './components/blocks/Steps/Steps';
+import { MacbookScroll } from './components/ui/macbook-scroll';
 const GoogleMap = dynamic(async () => await import('./components/blocks/GoogleMap/GoogleMap'), {
   ssr: false,
 });
@@ -13,6 +14,7 @@ const GoogleMap = dynamic(async () => await import('./components/blocks/GoogleMa
 export default function Home() {
   return (
     <main>
+      <MacbookScroll />
       <Cta />
       <Services />
       <Steps />
