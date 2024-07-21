ab15fcdb99c476f33845394df96abfc80a679e4a
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-07-21 13:42:40 +0100

    fix layout of blogs

--- src/components/BlogBox/BlogBox.tsx
+++ src/components/BlogBox/BlogBox.tsx
@@ -1,12 +1,13 @@
 import Link from 'next/link';
 import { type Blog } from '../blocks/Blogs/blogMockData';
 import Button from '../Button/Button';
+import formatDate from '@/utils/formatDate';
 
 export default function BlogBox({ id, title, date, description }: Blog) {
   return (
-    <article className='flex flex-col gap-3 p-6 border border-solid border-gray-800 rounded-sm max-w-[454px]'>
+    <article className='flex flex-col gap-3 p-6 border border-solid border-gray-800 rounded-sm '>
       <h4 className='text-xl'>{title}</h4>
-      <span className='text-xs text-gray-500'>{date}</span>
+      <span className='text-xs text-gray-500'>{formatDate(date)}</span>
       <p className='text-gray-500'>{description}</p>
       <Link href={`/blog/${id}`}>
         <Button className='w-fit uppercase text-gold text-lg underline'>Read More</Button>
--- src/components/blocks/Blogs/Blogs.tsx
+++ src/components/blocks/Blogs/Blogs.tsx
@@ -2,20 +2,15 @@ import React from 'react';
 import BlogBox from '@/components/BlogBox/BlogBox';
 import fetchBlogPosts from '@/services/wordpress/fetchPosts';
 
-// Layout for blogs is a hacky atm. Consider using 3 columns instead or a grid system
 export default async function Blogs() {
   const data = await fetchBlogPosts();
 
   return (
-    <section className='mx-auto max-w-[1556px] my-20 px-6 sm:px-12'>
-      <div className='flex justify-center'>
-        <div className='flex flex-wrap gap-12 justify-center'>
-          {data?.map((post: any) => (
-            <React.Fragment key={post.id}>
-              <BlogBox id={post.id} title={post.title.rendered} date={post.date} />
-            </React.Fragment>
-          ))}
-        </div>
+    <section className='container mx-auto max-w-[1500px] px-4 py-12'>
+      <div className='grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3'>
+        {data?.map((post: any) => (
+          <BlogBox key={post.id} id={post.id} title={post.title.rendered} date={post.date} />
+        ))}
       </div>
     </section>
   );
