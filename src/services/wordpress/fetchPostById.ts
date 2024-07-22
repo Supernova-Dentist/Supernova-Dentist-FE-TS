a9d545682391629bbabb686166ea9704a44922ed
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-07-22 10:31:35 +0100

    add some error checks for the responses

--- src/services/wordpress/fetchPostById.ts
+++ src/services/wordpress/fetchPostById.ts
@@ -1,6 +1,11 @@
 export default async function fetchBlogPostById(id: string) {
   try {
     const res = await fetch(`${process.env.WORDPRESS_API_BASE_URL}/posts/${id}`);
+
+    if (!res.ok) {
+      throw new Error(`HTTP error! status: ${res.status}`);
+    }
+
     const data = await res.json();
 
     return data;
--- src/services/wordpress/fetchPosts.ts
+++ src/services/wordpress/fetchPosts.ts
@@ -3,11 +3,20 @@ import { truncateText } from '@/utils/format/truncateString';
 export default async function fetchBlogPosts() {
   try {
     const res = await fetch(`${process.env.WORDPRESS_API_BASE_URL}/posts`);
+
+    if (!res.ok) {
+      throw new Error(`HTTP error! status: ${res.status}`);
+    }
+
     const data = await res.json();
+
+    if (!Array.isArray(data)) {
+      throw new Error('Unexpected data format: Expected an array');
+    }
 
     const postsWithTruncatedExcerpt = data?.map((post: unknown) => ({
       ...post,
-      excerpt: truncateText(post.excerpt.rendered, 60),
+      excerpt: truncateText(post.excerpt.rendered, 100),
     }));
 
     return postsWithTruncatedExcerpt;
