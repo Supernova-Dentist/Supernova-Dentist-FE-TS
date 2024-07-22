9311d2be046f123f17390ab722ff668c753e482a
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-07-22 10:05:16 +0100

    format date and restyle blog preview

--- src/components/BlogBox/BlogBox.tsx
+++ src/components/BlogBox/BlogBox.tsx
@@ -1,16 +1,24 @@
 import Link from 'next/link';
 import { type Blog } from '../blocks/Blogs/blogMockData';
 import Button from '../Button/Button';
-import formatDate from '@/utils/formatDate';
+import formatDate from '@/utils/format/formatDate';
 
-export default function BlogBox({ id, title, date, description }: Blog) {
+export default function BlogBox({ id, title, date, excerpt }: Blog) {
   return (
-    <article className='flex flex-col gap-3 p-6 border border-solid border-gray-800 rounded-sm '>
-      <h4 className='text-xl'>{title}</h4>
-      <span className='text-xs text-gray-500'>{formatDate(date)}</span>
-      <p className='text-gray-500'>{description}</p>
+    <article className='flex flex-col justify-between p-6 border border-solid border-gray-200 rounded-sm shadow-lg'>
+      <div className='flex flex-col'>
+        <h4 className='text-xl mb-2'>{title}</h4>
+        <span className='text-xs mb-4 text-gray-500'>{formatDate(date)}</span>
+        {excerpt !== undefined ? (
+          <p className='text-gray-500 mb-6' dangerouslySetInnerHTML={{ __html: excerpt }}></p>
+        ) : null}
+      </div>
+
       <Link href={`/blog/${id}`}>
-        <Button className='w-fit uppercase text-gold text-lg underline'>Read More</Button>
+        {/* <Button className='w-fit uppercase text-gold text-lg underline'>Read More</Button> */}
+        <Button className='bg-gray-900 rounded-lg w-full px-8 py-2 text-gray-50 hover:bg-gray-800 duration-150'>
+          Read More
+        </Button>
       </Link>
     </article>
   );
