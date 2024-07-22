3412693f362025377d94792fbdf607b76b614b9a
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-07-22 11:36:59 +0100

    decode html entity in title of full blog post

--- src/app/blog/[blogId]/page.tsx
+++ src/app/blog/[blogId]/page.tsx
@@ -1,13 +1,14 @@
 import fetchBlogPostById from '@/services/wordpress/fetchPostById';
+import { decodeHtmlEntities } from '@/utils/format/decodeHtmlEntities';
 
 export default async function BlogArticle({ params }: { params: { blogId: string } }) {
   const { blogId } = params;
 
-  const data = await fetchBlogPostById(blogId);
+  const data: Post = await fetchBlogPostById(blogId);
 
   return (
     <article className='flex flex-col justify-center max-w-[800px] mx-auto my-12'>
-      <h1 className='text-2xl'>{data?.title.rendered}</h1>
+      <h1 className='text-2xl'>{decodeHtmlEntities(data.title.rendered)}</h1>
       <div className='flex flex-col gap-4' dangerouslySetInnerHTML={{ __html: data?.content?.rendered }}></div>
     </article>
   );
