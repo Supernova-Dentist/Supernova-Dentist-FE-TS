562d2abab0f2de5f043ee04702a0c911f61453c9
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-07-22 10:30:42 +0100

    change color of hover button color

--- src/components/BlogBox/BlogBox.tsx
+++ src/components/BlogBox/BlogBox.tsx
@@ -16,7 +16,7 @@ export default function BlogBox({ id, title, date, excerpt }: Blog) {
 
       <Link href={`/blog/${id}`}>
         {/* <Button className='w-fit uppercase text-gold text-lg underline'>Read More</Button> */}
-        <Button className='bg-gray-900 rounded-lg w-full px-8 py-2 text-gray-50 hover:bg-gray-800 duration-150'>
+        <Button className='bg-gray-900 rounded-lg w-full px-8 py-2 text-gray-50 hover:bg-gray-700 duration-150'>
           Read More
         </Button>
       </Link>
