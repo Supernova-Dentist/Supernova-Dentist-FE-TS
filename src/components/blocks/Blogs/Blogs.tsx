712d7d751638370e937d72473bbf80385d650647
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-07-22 11:27:01 +0100

    type posts in request function so not to use any.

--- src/components/blocks/Blogs/Blogs.tsx
+++ src/components/blocks/Blogs/Blogs.tsx
@@ -4,18 +4,18 @@ import fetchBlogPosts from '@/services/wordpress/fetchPosts';
 import { decodeHtmlEntities } from '@/utils/format/decodeHtmlEntities';
 
 export default async function Blogs() {
-  const data = await fetchBlogPosts();
+  const data: Post[] = await fetchBlogPosts();
 
   return (
     <section className='container mx-auto max-w-[1500px] px-4 py-12 sm:px-10'>
       <div className='grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3'>
-        {data?.map((post: any) => (
+        {data?.map((post: Post) => (
           <BlogBox
             key={post.id}
             id={post.id}
             title={decodeHtmlEntities(post.title.rendered)}
             date={post.date}
-            excerpt={post.excerpt}
+            excerpt={post.excerpt.rendered}
           />
         ))}
       </div>
--- src/services/wordpress/fetchPosts.ts
+++ src/services/wordpress/fetchPosts.ts
@@ -1,6 +1,6 @@
 import { truncateText } from '@/utils/format/truncateString';
 
-export default async function fetchBlogPosts() {
+export default async function fetchBlogPosts(): Promise<Post[]> {
   try {
     const res = await fetch(`${process.env.WORDPRESS_API_BASE_URL}/posts`);
 
@@ -8,15 +8,15 @@ export default async function fetchBlogPosts() {
       throw new Error(`HTTP error! status: ${res.status}`);
     }
 
-    const data = await res.json();
+    const data: Post[] = await res.json();
 
     if (!Array.isArray(data)) {
       throw new Error('Unexpected data format: Expected an array');
     }
 
-    const postsWithTruncatedExcerpt = data?.map((post: unknown) => ({
+    const postsWithTruncatedExcerpt = data?.map((post: Post) => ({
       ...post,
-      excerpt: truncateText(post.excerpt.rendered, 100),
+      excerpt: { rendered: truncateText(post.excerpt.rendered, 100) },
     }));
 
     return postsWithTruncatedExcerpt;
