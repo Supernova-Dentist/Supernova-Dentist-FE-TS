accaade0a0146d56fa6162a3ea5407715f320fc4
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-07-29 14:50:36 +0100

    add wrapper for fetching logic

---
+++ src/components/FetchBlogs/FetchBlogs.tsx
@@ -0,0 +1,18 @@
+import fetchBlogPosts from '@/services/wordpress/fetchPosts';
+import Blogs from '../blocks/Blogs/Blogs';
+
+export default async function FetchBlogsWrapper({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | string[] | undefined };
+}) {
+  const pageParam = Array.isArray(searchParams.page) ? searchParams.page[0] : searchParams.page;
+  const page = pageParam && !isNaN(parseInt(pageParam, 10)) ? parseInt(pageParam, 10) : 1;
+  const limit = 6;
+
+  const data = await fetchBlogPosts(limit, page);
+  const res = await fetch(`${process.env.NEXT_PUBLIC_WORDPRESS_API_BASE_URL}/posts`);
+  const totalPages = res.headers.get('X-WP-TotalPages');
+
+  return <Blogs data={data} page={page} totalPages={Number(totalPages)} />;
+}
