c2c2cb5814c98dcc732a7204d9d1e299f50ce5c6
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-07-29 08:06:40 +0100

    implement pagination logic

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -8,29 +8,58 @@ import {
   PaginationPrevious,
 } from '@/components/ui/pagination';
 
-export default function PaginationWrapper() {
+type PaginationWrapperProps = {
+  currentPage: number;
+  totalPages: number;
+  onPageChange: (i: number) => void;
+};
+
+export default function PaginationWrapper({ currentPage, totalPages, onPageChange }: PaginationWrapperProps) {
+  console.log({ totalPages, currentPage });
+
+  function createPageLinks() {
+    const pages = [];
+    for (let i = 1; i <= totalPages; i++) {
+      pages.push(
+        <PaginationItem key={i}>
+          <PaginationLink
+            href='#'
+            isActive={i === currentPage}
+            onClick={(e) => {
+              e.preventDefault();
+              onPageChange(i);
+            }}
+          >
+            {i}
+          </PaginationLink>
+        </PaginationItem>
+      );
+    }
+    return pages;
+  }
+
   return (
     <Pagination>
       <PaginationContent>
         <PaginationItem>
-          <PaginationPrevious href='#' />
+          <PaginationPrevious
+            href='#'
+            onClick={(e) => {
+              e.preventDefault();
+              if (currentPage > 1) onPageChange(currentPage - 1);
+            }}
+          />
         </PaginationItem>
+        {createPageLinks()}
+        {totalPages > 5 && <PaginationEllipsis />}
         <PaginationItem>
-          <PaginationLink href='#'>1</PaginationLink>
-        </PaginationItem>
-        <PaginationItem>
-          <PaginationLink href='#' isActive>
-            2
-          </PaginationLink>
-        </PaginationItem>
-        <PaginationItem>
-          <PaginationLink href='#'>3</PaginationLink>
-        </PaginationItem>
-        <PaginationItem>
-          <PaginationEllipsis />
-        </PaginationItem>
-        <PaginationItem>
-          <PaginationNext href='#' />
+          <PaginationNext
+            href='#'
+            onClick={(e) => {
+              e.preventDefault();
+              if (currentPage < totalPages) onPageChange(currentPage + 1);
+            }}
+          />
         </PaginationItem>
       </PaginationContent>
     </Pagination>
--- src/services/wordpress/fetchPosts.ts
+++ src/services/wordpress/fetchPosts.ts
@@ -2,7 +2,7 @@ import { truncateText } from '@/utils/format/truncateString';
 
 export default async function fetchBlogPosts(limit = 6, page = 1): Promise<Post[]> {
   try {
-    const res = await fetch(`${process.env.WORDPRESS_API_BASE_URL}/posts?per_page=${limit}&page=${page}`);
+    const res = await fetch(`${process.env.NEXT_PUBLIC_WORDPRESS_API_BASE_URL}/posts?per_page=${limit}&page=${page}`);
 
     if (!res.ok) {
       throw new Error(`HTTP error! status: ${res.status}`);
