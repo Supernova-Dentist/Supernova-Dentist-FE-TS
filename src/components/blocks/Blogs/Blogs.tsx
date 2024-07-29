dd543f2b82ca16359b88adc65d5d4e22bb6530d8
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-07-29 08:08:15 +0100

    fetch data on client and pass needed props to pagination wrapper

--- src/components/blocks/Blogs/Blogs.tsx
+++ src/components/blocks/Blogs/Blogs.tsx
@@ -1,16 +1,49 @@
-import React from 'react';
+'use client';
+
+import React, { useCallback, useEffect, useState } from 'react';
 import BlogBox from '@/components/BlogBox/BlogBox';
 import fetchBlogPosts from '@/services/wordpress/fetchPosts';
 import { decodeHtmlEntities } from '@/utils/format/decodeHtmlEntities';
 import PaginationWrapper from '@/components/Pagination/Pagination';
 
-export default async function Blogs() {
-  const data: Post[] = await fetchBlogPosts();
+export default function Blogs() {
+  // const data: Post[] = await fetchBlogPosts();
+  const [posts, setPosts] = useState<Post[]>([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+  const limit = 6;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const posts = await fetchBlogPosts(limit, currentPage);
+        setPosts(posts);
+
+        const res = await fetch(`${process.env.NEXT_PUBLIC_WORDPRESS_API_BASE_URL}/posts`);
+        const totalPosts = res.headers.get('x-wp-total');
+        console.log({ totalPages, limit });
+
+        if (totalPosts !== null) {
+          setTotalPages(Math.ceil(+totalPosts / limit));
+        }
+      } catch (error) {
+        console.log({ error });
+      }
+    };
+
+    void fetchData();
+  }, [currentPage]);
+
+  const handlePageChange = useCallback((page: number) => {
+    console.log({ page });
+
+    setCurrentPage(page);
+  }, []);
 
   return (
     <section className='container mx-auto max-w-[1500px] px-4 py-12 sm:px-10'>
       <div className='grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3 mb-12'>
-        {data?.map((post: Post) => (
+        {posts?.map((post: Post) => (
           <BlogBox
             key={post.id}
             id={post.id}
@@ -20,7 +53,7 @@ export default async function Blogs() {
           />
         ))}
       </div>
-      <PaginationWrapper />
+      <PaginationWrapper currentPage={currentPage} totalPages={totalPages} onPageChange={handlePageChange} />
     </section>
   );
 }
