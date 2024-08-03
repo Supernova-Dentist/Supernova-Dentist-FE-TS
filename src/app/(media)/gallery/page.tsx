ef00788b965a5c4d1f4f34cd33a4700844e0d0fe
Adam Young <adamyoungy678@gmail.com>
2024-08-03 22:02:11 +0100

    feat: Add PageHero component to Gallery page

--- src/app/(media)/gallery/page.tsx
+++ src/app/(media)/gallery/page.tsx
@@ -1,5 +1,6 @@
 import { type Metadata } from 'next';
 import GalleryGrid from './utils/GalleryGrid';
+import PageHero from '@/components/blocks/PageHero/PageHero';
 
 export const metadata: Metadata = {
   title: 'Smile Transformations | Supernova Dental',
@@ -9,9 +10,7 @@ export const metadata: Metadata = {
 export default function Gallery() {
   return (
     <>
-      <div className='flex justify-center items-center h-20 my-8'>
-        <h1 className='text-4xl font-bold text-center text-gray-800 '>Supernova Gallery</h1>
-      </div>
+      <PageHero bgImage='bg-blog' title='Gallery' showButtons={false} className='bg-center' />
       <GalleryGrid />
     </>
   );
