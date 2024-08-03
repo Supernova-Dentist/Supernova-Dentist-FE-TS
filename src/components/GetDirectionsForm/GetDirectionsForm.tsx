6f978e4b120cc66519463e1c5a7fb5e0701b08e7
Adam Young <adamyoungy678@gmail.com>
2024-08-03 23:47:17 +0100

    feat: Create GetDirectionsForm component for handling directions input and button click

---
+++ src/components/GetDirectionsForm/GetDirectionsForm.tsx
@@ -0,0 +1,30 @@
+// components/GetDirectionsForm.tsx
+'use client';
+import { useState } from 'react';
+
+const GetDirectionsForm = () => {
+  const [postcode, setPostcode] = useState('');
+
+  const handleGetDirections = () => {
+    const formattedPostcode = encodeURIComponent(postcode);
+    const directionsUrl = `https://www.google.com/maps/dir/?api=1&origin=${formattedPostcode}&destination=Marsh+Lane,+Huntworth,+Bridgwater,+Alliance+Building,+TA6+6LQ`;
+    window.open(directionsUrl, '_blank');
+  };
+
+  return (
+    <div className='flex items-center gap-4'>
+      <input
+        type='text'
+        value={postcode}
+        onChange={(e) => setPostcode(e.target.value)}
+        placeholder='Enter your postcode'
+        className='p-2 border border-gray-300 rounded-md'
+      />
+      <button onClick={handleGetDirections} className='bg-gold text-white px-4 py-2 rounded-md'>
+        Go
+      </button>
+    </div>
+  );
+};
+
+export default GetDirectionsForm;
