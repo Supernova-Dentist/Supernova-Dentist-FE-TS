ea164946b0c1fcdd75599ebaeec5c9b4d79a4527
Adam Young <adamyoungy678@gmail.com>
2024-08-03 15:54:33 +0100

    feat: Update EnquiryButton component to include referring page in query string

--- src/components/EquiryButton/EnquiryButton.tsx
+++ src/components/EquiryButton/EnquiryButton.tsx
@@ -2,10 +2,10 @@
 
 import Link from 'next/link';
 
-const EnquiryButton = () => {
+const EnquiryButton = ({ referringPage }) => {
   return (
     <Link
-      href='/enquiry'
+      href={`/enquiry?ref=${encodeURIComponent(referringPage)}`}
       className='bg-blue-500 text-white py-3 px-6 rounded-full shadow-lg transition-transform duration-300 ease-in-out hover:scale-110 hover:shadow-xl'
     >
       Enquiry
