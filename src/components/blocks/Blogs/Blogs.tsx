5ce1525af607501ba5e156830a5b431b6eac2368
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-08-05 07:35:12 +0100

    change name of data to posts

--- src/components/blocks/Blogs/Blogs.tsx
+++ src/components/blocks/Blogs/Blogs.tsx
@@ -1,18 +1,20 @@
 import { decodeHtmlEntities } from '@/utils/format/decodeHtmlEntities';
 import BlogBox from '@/components/BlogBox/BlogBox';
 import PaginationControls from '@/components/PaginationControls/PaginationControls';
+import BlogFilteringBar from '../BlogFilteringBar/BlogFilteringBar';
 
 type BlogsProps = {
-  data: Post[];
+  posts: Post[];
   page: number;
   totalPages: number;
 };
 
-export default async function Blogs({ data, page, totalPages }: BlogsProps) {
+export default async function Blogs({ posts, page, totalPages }: BlogsProps) {
   return (
     <section className='container mx-auto max-w-[1500px] px-4 py-12 sm:px-10'>
+      <BlogFilteringBar className='mb-12' />
       <div className='grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3 mb-12'>
-        {data?.map((post: Post) => (
+        {posts?.map((post: Post) => (
           <BlogBox
             key={post.id}
             id={post.id}
