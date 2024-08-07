f580faf51dc5a7ffeacca1083cddbcf11fd478af
Adam <adamyoungy678@gmail.com>
2024-08-07 17:55:03 +0100

    refactor: Update gallery page to use FetchGalleryWrapper component with searchParams

--- src/app/(media)/gallery/page.tsx
+++ src/app/(media)/gallery/page.tsx
@@ -1,17 +1,17 @@
-import { type Metadata } from 'next';
-import GalleryGrid from './utils/GalleryGrid';
 import PageHero from '@/components/blocks/PageHero/PageHero';
+import FetchGalleryWrapper from '@/components/FetchGallery/FetchGallery';
+import { type Metadata } from 'next';
 
 export const metadata: Metadata = {
   title: 'Smile Transformations | Supernova Dental',
   description: 'View our gallery of dental services and patient transformations at Supernova Dental.',
 };
 
-export default function Gallery() {
+export default function Gallery({ searchParams }: { searchParams: { [key: string]: string | string[] | undefined } }) {
   return (
     <>
       <PageHero bgImage='bg-blog' title='Gallery' showButtons={false} className='bg-center' />
-      <GalleryGrid />
+      <FetchGalleryWrapper searchParams={searchParams} />
     </>
   );
 }
