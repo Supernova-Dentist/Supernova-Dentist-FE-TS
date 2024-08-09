e202f5406d04fc9a744d03ae420111e94a1bd5ae
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-08-09 09:24:21 +0100

    add active filter class to buttons

--- src/components/BlogFilterButtons/BlogFilterButtons.tsx
+++ src/components/BlogFilterButtons/BlogFilterButtons.tsx
@@ -1,8 +1,9 @@
 'use client';
 
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '../Button/Button';
+import { cn } from '@/lib/utils';
 
 const filters = [
   {
@@ -20,6 +21,7 @@ const filters = [
 ];
 
 export default function BlogFilterButtons() {
+  const [activeFilter, setActiveFilter] = useState(0);
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const router = useRouter();
@@ -32,7 +34,9 @@ export default function BlogFilterButtons() {
 
     if (id === null || id === '0') {
       params.delete('categories');
+      setActiveFilter(0);
     } else {
+      setActiveFilter(Number(id));
       params.set('page', '1');
       params.set('categories', id);
     }
@@ -44,7 +48,16 @@ export default function BlogFilterButtons() {
     <div className='flex gap-4'>
       {filters.map(({ id, text }) => (
         <React.Fragment key={id}>
-          <Button text={text} id={id} className='bg-gray-100 px-6 py-2 rounded-sm' onClick={handleClick} />
+          <Button
+            text={text}
+            id={id}
+            className={cn(
+              `bg-gray-200 px-6 py-2 rounded-sm hover:bg-gray-100 duration-150 border border-transparent hover:border-gray-200 ${
+                activeFilter === id ? 'bg-gray-100 border-gray-200' : null
+              }`
+            )}
+            onClick={handleClick}
+          />
         </React.Fragment>
       ))}
     </div>
