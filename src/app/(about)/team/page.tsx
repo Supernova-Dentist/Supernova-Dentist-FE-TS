56299099c1cc186dd549ae125039bcc728bb1f5c
Adam Young <adamyoungy678@gmail.com>
2024-08-11 12:15:52 +0100

    feat: add placeholder team image

--- src/app/(about)/team/page.tsx
+++ src/app/(about)/team/page.tsx
@@ -9,23 +9,22 @@ export const metadata: Metadata = {
 
 export default function TeamPage() {
   return (
-    <div className='flex flex-col bg-lightGrey min-h-screen bg-gray-50'>
+    <div className='flex flex-col min-h-screen bg-lightGrey'>
       {/* Hero Section */}
-      <div className='relative w-full  bg-cover bg-center bg-no-repeat py-24 md:py-36 lg:py-48'>
+      <section className="flex flex-col justify-center items-center h-120 w-full bg-[url('/assets/images/aiTeamImage.jpg')] bg-cover bg-center bg-no-repeat py-20 md:py-32 lg:py-40">
         <h1 className='text-3xl font-extrabold text-center text-gold sm:text-4xl md:text-5xl'>Meet Our Dental Team</h1>
         <p className='mx-auto mt-4 max-w-2xl text-lg text-center text-white'>
           Discover the dedicated professionals who make our dental practice exceptional.
         </p>
+      </section>
+      <section className='w-full py-12 md:py-24 lg:py-32 bg-lightGrey'>
+        {/* Breadcrumb */}
+        <div className='mx-auto px-4 md:px-6 container pt-8 pb-4'>
+          <BreadCrumb />
+        </div>
 
-        <div className='absolute inset-0 bg-black opacity-30' />
-      </div>
-
-      {/* Breadcrumb */}
-      <div className='mx-auto px-4 md:px-6 container pt-8 pb-4'>
-        <BreadCrumb />
-      </div>
-
-      <TeamMemberGrid />
+        <TeamMemberGrid />
+      </section>
     </div>
   );
 }
