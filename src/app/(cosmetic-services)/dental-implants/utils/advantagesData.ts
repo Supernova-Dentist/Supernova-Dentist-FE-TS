ff1fe16d991ca8cc7182a77f1ef079ae50729d81
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-08-15 14:29:08 +0100

    add all data files for dental implants page

---
+++ src/app/(cosmetic-services)/dental-implants/utils/advantagesData.ts
@@ -0,0 +1,10 @@
+export default [
+  { id: 1, heading: 'Natural Appearance', text: 'Blends seamlessly with your existing teeth for a natural look.' },
+  { id: 2, heading: 'Durability', text: 'Provides a long-lasting solution with proper care.' },
+  { id: 3, heading: 'Enhanced Functionality', text: 'Restores full chewing ability and support for adjacent teeth.' },
+  {
+    id: 4,
+    heading: 'No Impact on Adjacent Teeth',
+    text: 'Unlike bridges, implants don’t require modification of neighboring teeth.',
+  },
+];
---
+++ src/components/ComparisonTable/tableData.ts
@@ -0,0 +1,28 @@
+export const tableHeadData = ['Feature', 'Dental Implants', 'Bridges', 'Dentures'];
+
+export const tableBodyData = [
+  {
+    feature: 'Longevity',
+    data: ['10-15 years or more', '5-10 years', '5-7 years'],
+  },
+  {
+    feature: 'Durability',
+    data: [true, true, false],
+  },
+  {
+    feature: 'Impact on Adjacent Teeth',
+    data: [false, true, false],
+  },
+  {
+    feature: 'Comfort and Fit',
+    data: [true, true, true],
+  },
+  {
+    feature: 'Maintenance',
+    data: ['Regular brushing and flossing', 'Requires special cleaning', 'Daily removal and cleaning'],
+  },
+  {
+    feature: 'Cost-effective',
+    data: [false, false, true],
+  },
+];
