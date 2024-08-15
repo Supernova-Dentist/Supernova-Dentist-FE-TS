4ccaeda0cf031b08e1a97879b3720baab44ec136
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-08-15 13:25:21 +0100

    update types for services data

--- types/Service.d.ts
+++ types/Service.d.ts
@@ -25,3 +25,49 @@ type ServiceBoxProps = {
   benefitText: string;
   stats: ServiceStatProps[];
 };
+
+type TextColors = {
+  header: string;
+  text: string;
+};
+
+type ServiceGridListItemProps = {
+  id?: number;
+  heading: string;
+  text: string;
+  textColors?: TextColors;
+};
+
+type ListItem = {
+  id?: number;
+  heading: string;
+  text: string;
+};
+
+type ListItems = ListItem[];
+
+type ServiceGridListProps = {
+  listItems: ListItems;
+  textColors?: TextColors;
+};
+
+type ServiceBenefitBox = {
+  icon: ReactElement;
+  header: string;
+  text: string;
+};
+
+type ServiceProcessBoxProps = {
+  src: string;
+  header: string;
+  text: string;
+};
+
+type ServiceTestimonialBoxProps = {
+  name: string;
+  text: string;
+  beforeSrc: string;
+  afterSrc: string;
+  beforeAlt: string;
+  afterAlt: string;
+};
