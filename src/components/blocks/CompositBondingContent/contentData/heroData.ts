08111c45d2ce794f1beec8e31ded21903c1a8b34
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-08-20 19:10:15 +0100

    move hero data to file closer to component its used in

---
+++ src/components/blocks/CompositBondingContent/contentData/heroData.ts
@@ -0,0 +1,17 @@
+export default {
+  header: { title: 'Composite Bonding', subTitle: 'Transform your smile with our expert composite bonding services.' },
+  mainText:
+    "Composite bonding is a cosmetic dental procedure that enhances the appearance of your teeth. Our skilled dentists use tooth-colored resin to fill in gaps, chips, or discoloration, creating a natural-looking, seamless smile. This treatment improves aesthetics and helps restore your teeth's function and beauty.",
+  benefitText:
+    "Boost your confidence and achieve the smile you've always wanted with our composite bonding services. Book your appointment today!",
+  serviceStats: [
+    {
+      title: '30 - 60 minutes',
+      text: 'per tooth',
+    },
+    {
+      title: '1',
+      text: 'treatments required',
+    },
+  ],
+};
