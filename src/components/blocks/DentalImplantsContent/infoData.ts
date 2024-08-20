24ab337d0e3ec2ccb31a9396a2754adedd4bae90
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-08-20 18:40:14 +0100

    rename data files to stay consistent

---
+++ src/components/blocks/DentalImplantsContent/infoData.ts
@@ -0,0 +1,9 @@
+export default {
+  title: 'What are Dental Implants?',
+  descriptions: [
+    'Dental implants are a modern solution for replacing missing teeth. They consist of a titanium post that is surgically inserted into the jawbone, which acts as a replacement root for a new tooth. This procedure provides a stable and durable foundation for attaching crowns, bridges, or dentures.',
+    'Implants offer a natural look and feel, effectively restoring both the function and aesthetics of your smile. They can be used to replace a single tooth, multiple teeth, or even support full dentures. Our expert team ensures a precise and comfortable procedure with long-lasting results.',
+  ],
+  advantagesTitle: 'Advantages of Dental Implants:',
+  useCaseTitle: 'Use Cases of Dental Implants',
+};
