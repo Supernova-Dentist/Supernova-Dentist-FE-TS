fdb6c3225cb9c5c24063dbd64f36589015d6c16f
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-08-24 16:31:34 +0100

    add chevron to links and improve styles

--- src/components/DesktopNav/DesktopNav.tsx
+++ src/components/DesktopNav/DesktopNav.tsx
@@ -1,9 +1,11 @@
 'use client';
 
+import Link from 'next/link';
 import React, { useState } from 'react';
+import { ChevronDownIcon } from '@radix-ui/react-icons';
 import { AnimatePresence, motion } from 'framer-motion';
 import { navLinks } from '@/lib/constants';
-import Link from 'next/link';
+import { cn } from '@/lib/utils';
 import Button from '../Button/Button';
 
 export default function DesktopNav() {
@@ -38,13 +40,18 @@ const FlyoutLink = ({ children, href, flyoutItems }: FlyoutLinkProps) => {
 
   return (
     <div onMouseEnter={() => setOpen(true)} onMouseLeave={() => setOpen(false)} className='relative w-fit h-fit z-50'>
-      <a href={href} className='relative text-white'>
+      <a href={href} className='relative text-white transition-all duration-300 ease-out flex items-center'>
         {children}
+        {flyoutItems.length > 0 && (
+          <ChevronDownIcon
+            className={cn(`ml-1 transform transition-transform duration-300 ${showFlyout ? 'rotate-180' : ''}`)}
+          />
+        )}
         <span
           style={{
             transform: showFlyout ? 'scaleX(1)' : 'scaleX(0)',
           }}
-          className='absolute -bottom-2 -left-2 -right-2 h-1 origin-left scale-x-0 rounded-full bg-indigo-300 transition-transform duration-300 ease-out'
+          className='absolute -bottom-2 -left-2 -right-2 h-1 origin-left scale-x-0 rounded-full bg-gold transition-transform duration-300 ease-out'
         />
       </a>
       <AnimatePresence>
@@ -58,10 +65,14 @@ const FlyoutLink = ({ children, href, flyoutItems }: FlyoutLinkProps) => {
             className='absolute left-1/2 top-12 bg-white text-black shadow-lg'
           >
             <div className='absolute -top-6 left-0 right-0 h-6 bg-transparent' />
-            <div className='absolute left-1/2 top-0 h-4 w-4 -translate-x-1/2 -translate-y-1/2 rotate-45 bg-white' />
-            <div className='w-64 bg-white p-6 shadow-xl z-50'>
+            <div className='absolute left-1/2 top-0 h-4 w-4 -translate-x-1/2 -translate-y-1/2 rotate-45 bg-cream' />
+            <div className='flex flex-col gap-4 w-64 bg-cream p-6 shadow-xl z-50'>
               {flyoutItems.map(({ name, link }) => (
-                <a key={name} href={link} className='block text-sm hover:underline'>
+                <a
+                  key={name}
+                  href={link}
+                  className='block text-md text-gold transition-all duration-300 ease-out hover:text-amber-700 hover:translate-x-1'
+                >
                   {name}
                 </a>
               ))}
