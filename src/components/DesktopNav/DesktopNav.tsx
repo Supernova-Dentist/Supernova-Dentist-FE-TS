bf02664ba4f87d6e718ec6636db33435a9657336
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-08-24 17:17:10 +0100

    add more semantic elements in markup

--- src/components/DesktopNav/DesktopNav.tsx
+++ src/components/DesktopNav/DesktopNav.tsx
@@ -8,19 +8,23 @@ import FlyoutLink from './FlyoutLink/FlyoutLink';
 
 export default function DesktopNav() {
   return (
-    <div className='flex justify-end gap-8 items-center bg-grey p-6'>
-      <FlyoutLink href='/' flyoutItems={[]}>
-        Home
-      </FlyoutLink>
-      {navLinks.map(({ name, flyout }) => (
-        <FlyoutLink key={name} href='#' flyoutItems={flyout}>
-          {name}
+    <nav className='bg-grey p-6'>
+      <ul className='flex justify-end gap-8 items-center '>
+        <FlyoutLink href='/' flyoutItems={[]}>
+          Home
         </FlyoutLink>
-      ))}
+        {navLinks.map(({ name, flyout }) => (
+          <FlyoutLink key={name} href='#' flyoutItems={flyout}>
+            {name}
+          </FlyoutLink>
+        ))}
 
-      <Link href='/'>
-        <Button className='text-md bg-gold hover:bg-amber-700 text-cream px-4 py-2 rounded'>Book Now</Button>
-      </Link>
-    </div>
+        <li>
+          <Link href='/'>
+            <Button className='text-md bg-gold hover:bg-amber-700 text-cream px-4 py-2 rounded'>Book Now</Button>
+          </Link>
+        </li>
+      </ul>
+    </nav>
   );
 }
--- src/components/DesktopNav/FlyoutLink/FlyoutLink.tsx
+++ src/components/DesktopNav/FlyoutLink/FlyoutLink.tsx
@@ -1,8 +1,7 @@
-import Link from 'next/link';
 import { useMemo, useState } from 'react';
-import { ChevronDownIcon } from '@radix-ui/react-icons';
 import { AnimatePresence, motion } from 'framer-motion';
-import { cn } from '@/lib/utils';
+import FlyoutList from '../FlyoutList/FlyoutList';
+import MainLink from '../MainLink/MainLink';
 
 export default function FlyoutLink({ children, href, flyoutItems }: FlyoutLinkProps) {
   const [open, setOpen] = useState(false);
@@ -10,27 +9,10 @@ export default function FlyoutLink({ children, href, flyoutItems }: FlyoutLinkPr
   const showFlyout = useMemo(() => Array.isArray(flyoutItems) && flyoutItems.length > 0 && open, [flyoutItems, open]);
 
   return (
-    <div onMouseEnter={() => setOpen(true)} onMouseLeave={() => setOpen(false)} className='relative w-fit h-fit z-50'>
-      <Link
-        href={href}
-        aria-expanded={showFlyout}
-        className='relative text-white transition-all duration-300 ease-out flex items-center'
-      >
+    <li onMouseEnter={() => setOpen(true)} onMouseLeave={() => setOpen(false)} className='relative w-fit h-fit z-50'>
+      <MainLink href={href} flyoutItems={flyoutItems} open={open} showFlyout={showFlyout}>
         {children}
-        {flyoutItems.length > 0 && (
-          <ChevronDownIcon
-            className={cn('ml-1 transform transition-transform duration-300', {
-              'rotate-180': showFlyout,
-            })}
-          />
-        )}
-        <span
-          style={{
-            transform: open ? 'scaleX(1)' : 'scaleX(0)',
-          }}
-          className='absolute -bottom-2 -left-2 -right-2 h-1 origin-left scale-x-0 rounded-full bg-gold transition-transform duration-300 ease-out'
-        />
-      </Link>
+      </MainLink>
       <AnimatePresence>
         {showFlyout && (
           <motion.div
@@ -43,20 +25,12 @@ export default function FlyoutLink({ children, href, flyoutItems }: FlyoutLinkPr
           >
             <div className='absolute -top-6 left-0 right-0 h-6 bg-transparent' />
             <div className='absolute left-1/2 top-0 h-4 w-4 -translate-x-1/2 -translate-y-1/2 rotate-45 bg-cream' />
-            <div className='flex flex-col gap-4 w-64 bg-cream p-6 shadow-xl z-50'>
-              {flyoutItems.map(({ name, link }) => (
-                <Link
-                  key={name}
-                  href={link}
-                  className='block text-md text-gold transition-all duration-300 ease-out hover:text-amber-700 hover:translate-x-1'
-                >
-                  {name}
-                </Link>
-              ))}
-            </div>
+            <nav className='w-64 bg-cream p-6 shadow-xl z-50'>
+              <FlyoutList flyoutItems={flyoutItems} />
+            </nav>
           </motion.div>
         )}
       </AnimatePresence>
-    </div>
+    </li>
   );
 }
