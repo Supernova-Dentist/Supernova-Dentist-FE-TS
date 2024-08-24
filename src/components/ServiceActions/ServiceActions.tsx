2cf27a89a51d00fe6cbfd3f8b750ddee25e07f48
Adam Young <adamyoungy678@gmail.com>
2024-08-24 23:14:54 +0100

    refactor: Update ServiceActions component to open Dentally portal in a new tab
    This commit updates the ServiceActions component to open the Dentally portal in a new tab when the "Book Now" button is clicked. It imports the `DentallyPortal` constant from the `@/lib/constants` module and sets the `href` prop of the `Link` component to `${DentallyPortal}`. This change improves the user experience by providing a seamless way to access the Dentally portal without leaving the current page.
    
    Refactor the ServiceActions component to open Dentally portal in a new tab.

--- src/components/ServiceActions/ServiceActions.tsx
+++ src/components/ServiceActions/ServiceActions.tsx
@@ -1,11 +1,13 @@
 import Link from 'next/link';
 import LearnMoreButton from '../LearnMoreButton/LearnMoreButton';
+import { DentallyPortal } from '@/lib/constants';
 
 export default function ServiceActions() {
   return (
     <div className='lg:mt-6 mt-10 flex flex-col gap-4 sm:flex-row md:justify-start sm:justify-center'>
       <Link
-        href='#'
+        target='_blank'
+        href={`${DentallyPortal}`}
         className='inline-flex h-10 items-center justify-center rounded-md bg-gold px-6 text-sm font-medium text-white shadow transition-colors hover:bg-cream hover:text-gold focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50'
         prefetch={false}
       >
