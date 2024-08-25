d123d2ae8a30525f2aeffa692616e328e07e955c
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-08-25 15:29:08 +0100

    center content on mobile

--- src/components/PromotionForm/PromotionForm.tsx
+++ src/components/PromotionForm/PromotionForm.tsx
@@ -6,11 +6,11 @@ import { Button } from '@/components/ui/button';
 export default function PromotionForm() {
   return (
     <section className='w-full py-12 md:py-24 lg:py-32 bg-muted'>
-      <div className='container grid items-center gap-8 px-4 md:px-6 lg:grid-cols-2 lg:gap-12 mx-auto max-w-[1250px]'>
-        <div className='space-y-4'>
+      <div className='container grid items-center lg:justify-start justify-center gap-8 px-4 md:px-6 lg:grid-cols-2 lg:gap-12 mx-auto max-w-[1250px]'>
+        <div className='space-y-4 text-center lg:text-left'>
           <div className='inline-block rounded-lg bg-grey px-3 py-1 text-sm text-gray-50'>Limited Time Offer</div>
           <h2 className='text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl'>50% Off First Visit</h2>
-          <p className='max-w-[600px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed text-lightGrey'>
+          <p className='max-w-[600px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed text-lightGrey tracking-tight'>
             For the first 50 people who sign up, get 50% off your first visit to our dental practice. Don&apos;t miss
             out on this exclusive offer!
           </p>
