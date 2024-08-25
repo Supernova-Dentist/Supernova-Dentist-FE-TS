e1d6a5926d3084fdb19ece3637ace79d6673b1b3
Adam Young <adamyoungy678@gmail.com>
2024-08-25 12:18:22 +0100

    ```text refactor: Update EnquiryButton referringPage URLs for cosmetic dentistry pages
    Update the `referringPage` prop values in the `EnquiryButton` component for the cosmetic dentistry pages. This change ensures that the correct URLs are used when submitting an enquiry from the Invisalign, Composite Bonding, Dental Implants, and Tooth Whitening pages.

--- src/app/cosmetic-dentistry/composite-bonding/page.tsx
+++ src/app/cosmetic-dentistry/composite-bonding/page.tsx
@@ -13,7 +13,7 @@ export default function CompositeBonding() {
     <div className='flex flex-col min-h-[100dvh]'>
       <CompositBondingContent />
       <div className='fixed inset-x-0 bottom-5 md:fixed md:bottom-10 md:right-40 flex justify-center md:justify-end'>
-        <EnquiryButton referringPage={'composite-bonding'} />
+        <EnquiryButton referringPage={'/cosmetic-dentistry/composite-bonding'} />
       </div>
     </div>
   );
--- src/app/cosmetic-dentistry/tooth-whitening/page.tsx
+++ src/app/cosmetic-dentistry/tooth-whitening/page.tsx
@@ -13,7 +13,7 @@ export default function ToothWhitening() {
     <div className='flex flex-col min-h-[100dvh]'>
       <ToothWhiteningContent />
       <div className='fixed inset-x-0 bottom-5 md:fixed md:bottom-10 md:right-40 flex justify-center md:justify-end'>
-        <EnquiryButton referringPage={'tooth-whitening'} />
+        <EnquiryButton referringPage={'cosmetic-dentistry/tooth-whitening'} />
       </div>
     </div>
   );
