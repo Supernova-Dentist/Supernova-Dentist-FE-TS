2328fd900d782978e4f072412ec498c8009f6f89
Adam Young <adamyoungy678@gmail.com>
2024-08-25 12:21:58 +0100

    refactor: Update EnquiryButton referringPage URLs for general checkup

--- src/app/general-dentistry/general-checkup/page.tsx
+++ src/app/general-dentistry/general-checkup/page.tsx
@@ -13,7 +13,7 @@ export default function GeneralCheckup() {
     <div className='flex flex-col min-h-[100dvh]'>
       <GeneralCheckupContent />
       <div className='fixed inset-x-0 bottom-5 md:fixed md:bottom-10 md:right-40 flex justify-center md:justify-end'>
-        <EnquiryButton referringPage={'cosmetic-dentistry/tooth-whitening'} />
+        <EnquiryButton referringPage={'general-dentistry/general-checkup'} />
       </div>
     </div>
   );
