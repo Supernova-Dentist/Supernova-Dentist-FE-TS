9cf627dbf43b2a35c232abe05bd0cc04be57db76
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-08-25 22:46:37 +0100

    add zod schema and type to types file

---
+++ types/PromotionForm.ts
@@ -0,0 +1,14 @@
+import validator from 'validator';
+import { z } from 'zod';
+
+// For some reason zod not giving correct errors for when input are empty. Should get the default field is required message, so added min(1) for that
+export const promotionSignupSchema = z.object({
+  fullname: z.string().min(1, 'Full name is required').max(99, 'Name too long'),
+  email: z.string().min(1, 'Email is required').email(),
+  phone: z
+    .string()
+    .min(1, 'Phone number is required')
+    .refine((data) => validator.isMobilePhone(data), { message: 'Invalid phone number' }),
+});
+
+export type PromotionFormData = z.infer<typeof promotionSignupSchema>;
