73f533047c153de49e51c00d223fa1b6b9e5c302
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-08-25 15:25:14 +0100

    comment out unused sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,26 @@
-import dynamic from 'next/dynamic';
-import Services from '@/components/Services/Services';
-import GalleryCarousel from '@/components/blocks/GalleryCarousel/GalleryCarousel';
-import GoogleReviews from '@/components/blocks/GoogleReviews/GoogleReviews';
-import Reviews from '@/components/blocks/Reviews/Review';
-import Steps from '@/components/blocks/Steps/Steps';
+// import dynamic from 'next/dynamic';
+// import Services from '@/components/Services/Services';
+// import GalleryCarousel from '@/components/blocks/GalleryCarousel/GalleryCarousel';
+// import GoogleReviews from '@/components/blocks/GoogleReviews/GoogleReviews';
+// import Reviews from '@/components/blocks/Reviews/Review';
+// import Steps from '@/components/blocks/Steps/Steps';
 import Welcome from '@/components/blocks/Welcome/Welcome';
-const GoogleMap = dynamic(async () => await import('@/components/blocks/GoogleMap/GoogleMap'), {
-  ssr: false,
-});
+import PromotionForm from '@/components/PromotionForm/PromotionForm';
+// const GoogleMap = dynamic(async () => await import('@/components/blocks/GoogleMap/GoogleMap'), {
+//   ssr: false,
+// });
 
 export default function Home() {
   return (
     <main>
       <Welcome />
-      <Services />
+      <PromotionForm />
+      {/* <Services />
       <Steps />
       <Reviews />
       <GalleryCarousel />
       <GoogleReviews />
-      <GoogleMap />
+      <GoogleMap /> */}
     </main>
   );
 }
