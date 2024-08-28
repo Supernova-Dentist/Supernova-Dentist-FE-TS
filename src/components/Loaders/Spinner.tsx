fa623bd33eb9cb31f158d246b499e8acc82f8ad7
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-08-28 12:07:48 +0100

    add loading spinner component and map loading skeleton with spinner

---
+++ src/components/Loaders/Spinner.tsx
@@ -0,0 +1,5 @@
+export default function Spinner() {
+  return (
+    <div className='w-12 h-12 border-4 border-dotted border-grey rounded-full inline-block box-border animate-rotation'></div>
+  );
+}
