989e43a75c9c0b0618f5e53b9d9ef4a075131840
Adam <adamyoungy678@gmail.com>
2024-09-06 21:59:10 +0100

    refactor: Update ServiceTestimonial component layout for better readability

--- src/components/ServiceTestimonial/ServiceTestimonial.tsx
+++ src/components/ServiceTestimonial/ServiceTestimonial.tsx
@@ -9,10 +9,12 @@ export default function ServiceTestimonial({
   afterAlt,
 }: ServiceTestimonial) {
   return (
-    <div className='bg-cream p-6 rounded-lg shadow-lg'>
-      <p className='text-lightGrey'>{text}</p>
-      <p className='my-4 font-medium text-gold'>{name}</p>
-      <div className='flex justify-center p-4 border rounded-3xl bg-neutral-100  border-neutral-200 px-4'>
+    <div className='bg-cream p-6 rounded-lg shadow-lg flex flex-col justify-between h-full'>
+      <div>
+        <p className='text-lightGrey'>{text}</p>
+        <p className='my-4 font-medium text-gold'>{name}</p>
+      </div>
+      <div className='flex justify-center p-4 border rounded-3xl bg-neutral-100 border-neutral-200 mt-auto'>
         <Compare
           firstImage={beforeSrc}
           secondImage={afterSrc}
