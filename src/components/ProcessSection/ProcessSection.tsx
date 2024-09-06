8d93d2eb42fddf7af8d8f3a3aa2699b7191d279f
Adam <adamyoungy678@gmail.com>
2024-09-06 22:12:29 +0100

    refactor: Update AftercareSection and BenefitSection components to center align description text

--- src/components/ProcessSection/ProcessSection.tsx
+++ src/components/ProcessSection/ProcessSection.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
 import SectionTitle from '@/components/SectionTitle/SectionTitle';
 import ServiceProcess from '@/components/ServiceProcess/ServiceProcess';
+import React from 'react';
 
 export default function ProcessSection({ data }: ProcessSection) {
   const { title, description, content } = data;
@@ -9,7 +9,9 @@ export default function ProcessSection({ data }: ProcessSection) {
     <section className='py-12 px-4 md:py-20 lg:py-28 bg-lightGrey'>
       <div className='container mx-auto px-4 md:px-6'>
         <SectionTitle title={title} />
-        <p className='mt-4 text-cream max-w-[700px]'>{description}</p>
+        <div className='flex justify-center'>
+          <p className='mt-4 mb-6 text-cream max-w-[720px]'>{description}</p>
+        </div>
         <div className='mt-6 grid gap-6 md:grid-cols-2 lg:grid-cols-3'>
           {content.map(({ id, src, header, text }) => (
             <React.Fragment key={id}>
