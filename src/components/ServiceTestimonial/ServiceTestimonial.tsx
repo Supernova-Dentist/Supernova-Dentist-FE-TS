beba83c391e275320fcfe339012c212330a9597d
Adam <adamyoungy678@gmail.com>
2024-09-06 21:55:00 +0100

    chore: Update ServiceTestimonial component to use Compare component for image comparison

--- src/components/ServiceTestimonial/ServiceTestimonial.tsx
+++ src/components/ServiceTestimonial/ServiceTestimonial.tsx
@@ -1,4 +1,4 @@
-import { SliderWithImageComparison } from '../SliderWithImageComparison/SliderWithImageComparison';
+import { Compare } from '../ui/compare';
 
 export default function ServiceTestimonial({
   name,
@@ -12,14 +12,16 @@ export default function ServiceTestimonial({
     <div className='bg-cream p-6 rounded-lg shadow-lg'>
       <p className='text-lightGrey'>{text}</p>
       <p className='my-4 font-medium text-gold'>{name}</p>
-      <SliderWithImageComparison
-        beforeImageSrc={beforeSrc}
-        beforeImageAlt={beforeAlt}
-        afterImageSrc={afterSrc}
-        afterImageAlt={afterAlt}
-        imageWidth={400}
-        imageHeight={300}
-      />
+      <div className='flex justify-center p-4 border rounded-3xl bg-neutral-100  border-neutral-200 px-4'>
+        <Compare
+          firstImage={beforeSrc}
+          secondImage={afterSrc}
+          firstImageClassName='object-cover object-left-top'
+          secondImageClassname='object-cover object-left-top'
+          className='h-64 w-64'
+          slideMode='hover'
+        />
+      </div>
     </div>
   );
 }
