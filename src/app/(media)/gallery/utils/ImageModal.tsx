3197d04cb1ea4a12a8cb9d1ee449cc25ca3b1cce
Adam Young <adamyoungy678@gmail.com>
2024-09-07 11:51:42 +0100

    refactor: Update ImageModal component to include StaggeredDropDown for additional functionality

--- src/app/(media)/gallery/utils/ImageModal.tsx
+++ src/app/(media)/gallery/utils/ImageModal.tsx
@@ -1,7 +1,6 @@
 import React, { useRef } from 'react';
-import Button from '@/components/Button/Button';
-import { FaShareAlt } from 'react-icons/fa';
 import { IoClose } from 'react-icons/io5'; // Importing close icon
+import StaggeredDropDown from '@/components/StaggeredDropDown/StaggeredDropDown';
 
 const ImageModal = ({ open, onClose, selectedImage }: { open: boolean; onClose: () => void; selectedImage: any }) => {
   const modalRef = useRef<HTMLDivElement>(null);
@@ -27,6 +26,9 @@ const ImageModal = ({ open, onClose, selectedImage }: { open: boolean; onClose: 
         ref={modalRef}
         className='relative bg-white z-50 p-8 rounded-lg shadow-lg max-w-[80vw] max-h-[80vh] overflow-auto'
       >
+        <div className='relative flex justify-center p-4 space-x-4'>
+          <StaggeredDropDown />
+        </div>
         <button
           onClick={onClose}
           className='absolute top-1 right-1 p-2 bg-gray-200 rounded-full text-black hover:bg-gray-300'
@@ -34,6 +36,7 @@ const ImageModal = ({ open, onClose, selectedImage }: { open: boolean; onClose: 
         >
           <IoClose size={24} />
         </button>
+
         <div className='flex justify-center mt-1 mb-4'>
           <img
             src={selectedImage.jetpack_featured_media_url}
@@ -46,12 +49,6 @@ const ImageModal = ({ open, onClose, selectedImage }: { open: boolean; onClose: 
         <div className='p-4 bg-background'>
           <h2 className='text-center text-2xl font-bold mb-4'>{selectedImage.title.rendered}</h2>
           <ImageContent content={selectedImage.content?.rendered} />
-        </div>
-        <div className='flex justify-end p-4'>
-          <Button type='button' variant='secondary' className='flex items-center space-x-2'>
-            <FaShareAlt size={16} /> {/* Share icon */}
-            {/* <span>Share</span> */}
-          </Button>
         </div>
       </div>
     </div>
