0977a565f520fcf8e3c4d1a91de6b24a94f20efb
Adam <adamyoungy678@gmail.com>
2024-09-11 20:06:37 +0100

    feat: Add 'meet the team' link to CornerNav and desktop nav

--- src/lib/constants.tsx
+++ src/lib/constants.tsx
@@ -16,7 +16,7 @@ export const cosmeticServices = [
 export const practiceInfo = [
   { name: 'Our Clinic', link: '/clinic' },
   // { name: 'Meet Dr. Young', link: '/meet-dr-young' },
-  // { name: 'Meet The Team', link: '/team' },
+  { name: 'Meet The Team', link: '/team' },
   { name: 'Find Us', link: '/find-us' },
   { name: 'Pricing', link: '/pricing' },
   { name: 'Enquiry', link: '/enquiry' },
