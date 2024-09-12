f3e0a2fa5fa6d4c4b8427fa96e61c9fa812afe08
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-09-12 08:55:36 +0100

    add function to update token in db

--- src/data-access/instagram.ts
+++ src/data-access/instagram.ts
@@ -8,3 +8,16 @@ export async function getCurrentInstagramAccessToken() {
 
   return tokenRecord;
 }
+export async function updateInstagramToken(newToken: string, newExpiry: Date) {
+  const updatedToken = await prisma.instagramToken.update({
+    where: {
+      id: 1,
+    },
+    data: {
+      access_token: newToken,
+      expires_in: newExpiry,
+    },
+  });
+
+  return updatedToken;
+}
