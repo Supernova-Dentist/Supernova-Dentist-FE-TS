9a40060650f1d673eb3dafd20ecf84df6d3241e0
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-09-12 08:56:06 +0100

    add function to refresh the long life token and update in db

--- src/data-access/instagram.ts
+++ src/data-access/instagram.ts
@@ -21,3 +21,29 @@ export async function updateInstagramToken(newToken: string, newExpiry: Date) {
 
   return updatedToken;
 }
+export async function refreshAccessToken(currentToken: string) {
+  console.log({ currentToken });
+
+  const refreshUrl = `${INSTAGRAM_API_BASE_URL}/refresh_access_token?grant_type=ig_refresh_token&access_token=${currentToken}`;
+
+  try {
+    const response = await fetch(refreshUrl);
+    const data = await response.json();
+
+    console.log({ response, data });
+
+    if (!response.ok) {
+      throw new Error('Failed to refresh access token');
+    }
+
+    const newExpiresIn = new Date(Date.now() + data.expires_in * 1000);
+    const updatedToken = await updateInstagramToken(data.access_token, newExpiresIn);
+
+    console.log({ updatedToken, newExpiresIn });
+
+    return updatedToken.access_token;
+  } catch (error) {
+    console.log({ error });
+    throw new Error('Something went wrong');
+  }
+}
