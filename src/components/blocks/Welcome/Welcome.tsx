4b5b72640a0c9da5a4a142c8a8989b09d9aeea40
Adam <adamyoungy678@gmail.com>
2024-09-15 00:27:57 +0100

    refactor: Improve Welcome component layout and styling

--- src/components/blocks/Welcome/Welcome.tsx
+++ src/components/blocks/Welcome/Welcome.tsx
@@ -1,30 +1,38 @@
+import AnimatedSVGLogo from '@/components/AnimatedSVGLogo/AnimatedSVGLogo';
 import CtaButtons from '../Cta/CtaButtons';
 import { SlideTabsExample } from '../SlideTabs/SlideTabs';
 import { BubbleText } from './Utils/BubbleText';
 
 export default function Welcome() {
   return (
-    <div className='flex flex-col min-h-screen-minus-nav'>
+    <div className='flex flex-col min-h-screen'>
       <section
-        className='relative w-full h-screen-minus-nav bg-cover bg-center bg-no-repeat'
+        className='relative w-full h-screen bg-cover bg-center bg-no-repeat'
         style={{ backgroundImage: 'url("/hero-image.jpg")' }}
       >
         <div className='absolute inset-0 bg-gradient-to-b from-grey to-lightGrey' />
-        <div className='relative h-full flex flex-col items-center justify-center px-4 sm:px-6 text-center text-white'>
-          <h1 className='text-4xl md:text-5xl font-bold mb-2'>
-            Welcome to <span className='lg:hidden text-gold'>Supernova</span>{' '}
-            <span className='hidden lg:inline-block'>
-              <BubbleText />{' '}
-            </span>{' '}
-            Dental <img src='/favicon.ico' alt='Supernova Dental Logo' className='h-16 md:h-24 w-auto inline' />
-          </h1>
-          <p className='text-xl mb-16'>Experience exceptional dental care with our state-of-the-art clinic.</p>
-
+        <div className='relative flex flex-col items-center justify-center h-full px-4 sm:px-6 text-center text-white'>
+          <div className='flex flex-col items-center justify-center h-auto'>
+            <h1 className='text-3xl sm:text-4xl md:text-5xl font-bold mb-2 flex flex-col lg:flex-row items-center justify-center'>
+              Welcome to
+              <span className='lg:hidden text-gold my-2'>Supernova</span>{' '}
+              <span className='hidden lg:inline-flex items-center px-4 my-4 pb-2'>
+                <BubbleText />
+              </span>{' '}
+              Dental
+              <span className='w-20 mt-2 lg:mt-0 inline-flex items-center justify-center lg:ml-4'>
+                <AnimatedSVGLogo />
+              </span>
+            </h1>
+            <p className='text-lg sm:text-xl mt-6 lg:mt-2 mb-12'>
+              Experience exceptional dental care with our state-of-the-art clinic.
+            </p>
+          </div>
           <CtaButtons />
-          <ul className='hidden lg:grid grid-cols-1 sm:grid-cols-2 gap-4 text-lg mt-16'>
+          <ul className='hidden lg:grid grid-cols-1 sm:grid-cols-2 gap-4 text-base sm:text-lg mt-12'>
             <li className='flex items-center'>
               <svg
-                className='h-6 w-6 text-green-500 mr-2'
+                className='h-5 w-5 sm:h-6 sm:w-6 text-green-500 mr-2'
                 xmlns='http://www.w3.org/2000/svg'
                 fill='none'
                 viewBox='0 0 24 24'
@@ -36,7 +44,7 @@ export default function Welcome() {
             </li>
             <li className='flex items-center'>
               <svg
-                className='h-6 w-6 text-green-500 mr-2'
+                className='h-5 w-5 sm:h-6 sm:w-6 text-green-500 mr-2'
                 xmlns='http://www.w3.org/2000/svg'
                 fill='none'
                 viewBox='0 0 24 24'
@@ -48,7 +56,7 @@ export default function Welcome() {
             </li>
             <li className='flex items-center'>
               <svg
-                className='h-6 w-6 text-green-500 mr-2'
+                className='h-5 w-5 sm:h-6 sm:w-6 text-green-500 mr-2'
                 xmlns='http://www.w3.org/2000/svg'
                 fill='none'
                 viewBox='0 0 24 24'
@@ -60,7 +68,7 @@ export default function Welcome() {
             </li>
             <li className='flex items-center'>
               <svg
-                className='h-6 w-6 text-green-500 mr-2'
+                className='h-5 w-5 sm:h-6 sm:w-6 text-green-500 mr-2'
                 xmlns='http://www.w3.org/2000/svg'
                 fill='none'
                 viewBox='0 0 24 24'
