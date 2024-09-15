9d967ba12b1d6c4fc5af39a68f9d0e6e0f1fe416
Adam <adamyoungy678@gmail.com>
2024-09-15 17:25:52 +0100

    refactor: Refactor CornerNav component to use a div instead of a nav element as there is a nav element already used in MainNav

--- src/components/CornerNav/CornerNav.tsx
+++ src/components/CornerNav/CornerNav.tsx
@@ -16,11 +16,11 @@ export const CornerNav = () => {
 
 const LinksOverlay = () => {
   return (
-    <nav className='fixed right-4 top-4 z-40 h-[calc(100vh_-_32px)] w-[calc(100%_-_32px)] overflow-hidden'>
+    <div className='fixed right-4 top-4 z-40 h-[calc(100vh_-_32px)] w-[calc(100%_-_32px)] overflow-hidden'>
       <Logo />
       <LinksContainer />
       <FooterCTAs />
-    </nav>
+    </div>
   );
 };
 
