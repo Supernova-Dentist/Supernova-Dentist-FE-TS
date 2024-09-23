0580e5f698dcb8bbce9e9de6e9944a5565d43271
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-09-23 20:46:14 +0100

    Add card list component

---
+++ src/components/Pricing/PricingCards.tsx
@@ -0,0 +1,15 @@
+import { pricing } from '@/lib/constants';
+import React from 'react';
+import PricingCard from './PricingCard';
+
+export default function PricingCards() {
+  return (
+    <div className='flex mt-16 gap-8 max-w-[1100px] w-full'>
+      {pricing.map((plan, index) => (
+        <React.Fragment key={plan.id}>
+          <PricingCard plan={plan.plan} price={plan.price} features={plan.features} isMiddle={index === 1} />
+        </React.Fragment>
+      ))}
+    </div>
+  );
+}
