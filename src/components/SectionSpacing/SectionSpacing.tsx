202d929bc80da46766130243d6de7a5ee4a0961f
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-09-23 06:23:29 +0100

    add spacing component

--- src/components/SectionSpacing/SectionSpacing.tsx
+++ src/components/SectionSpacing/SectionSpacing.tsx
@@ -1,3 +1,5 @@
-export default function SectionSpacing({ children }: { children: React.ReactNode }) {
-  return <section className='mb-4 text-gray-800 leading-relaxed'>{children}</section>;
+import { cn } from '@/lib/utils';
+
+export default function SectionSpacing({ children, className }: { children: React.ReactNode; className?: string }) {
+  return <section className={cn('mb-4 text-gray-800 leading-relaxed', className)}>{children}</section>;
 }
