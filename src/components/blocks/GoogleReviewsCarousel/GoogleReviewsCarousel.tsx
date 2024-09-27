e8baa370a6d9046c89a3fdc8af7182f8e6dcaee2
Adam <adamyoungy678@gmail.com>
2024-09-27 20:42:49 +0100

    Refactor GoogleReviewsCarousel component to update layout and add review section

--- src/components/blocks/GoogleReviewsCarousel/GoogleReviewsCarousel.tsx
+++ src/components/blocks/GoogleReviewsCarousel/GoogleReviewsCarousel.tsx
@@ -20,7 +20,7 @@ export default function GoogleReviewsCarousel() {
   };
 
   return (
-    <div className='w-full overflow-hidden mx-auto md:px-24 px-12'>
+    <div className='max-w-[80%] overflow-hidden mx-auto md:px-24 px-12'>
       <Carousel
         opts={{ align: 'start', loop: true }}
         plugins={[Autoplay({ delay: 8000 })]}
@@ -31,10 +31,10 @@ export default function GoogleReviewsCarousel() {
             <CarouselItem
               key={id}
               onClick={() => toggleModal({ id, name, review, rating, date })}
-              className='w-full sm:basis-1/2 lg:basis-1/3 2xl:basis-1/6'
+              className='w-full mt-4 sm:basis-1/2 lg:basis-1/3 2xl:basis-1/5'
             >
-              <Card className='shadow-none'>
-                <CardContent className='bg-cream py-4 px-0'>
+              <Card className='shadow-none bg-transparent'>
+                <CardContent>
                   <GoogleReview name={name} review={review} rating={rating} date={date} />
                 </CardContent>
               </Card>
@@ -44,9 +44,9 @@ export default function GoogleReviewsCarousel() {
         <CarouselPrevious />
         <CarouselNext />
       </Carousel>
-      <div className='flex justify-center p-4 mt-4'>
+      {/* <div className='flex justify-center p-4 mt-4'>
         <ReviewLink />
-      </div>
+      </div> */}
 
       {isModalOpen && selectedReview != null && (
         <Modal isOpen={isModalOpen} onClose={() => toggleModal(null)}>
