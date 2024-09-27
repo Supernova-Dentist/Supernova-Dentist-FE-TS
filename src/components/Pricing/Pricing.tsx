220f1ad71d08009a85e8c718a60150aebbfdec4f
Adam <adamyoungy678@gmail.com>
2024-09-27 19:57:24 +0100

    Refactor Pricing component to update opening offers section

--- src/components/Pricing/Pricing.tsx
+++ src/components/Pricing/Pricing.tsx
@@ -11,10 +11,12 @@ export default function Pricing() {
   return (
     <section className='py-12 md:py-24 lg:py-32 mx-auto text-gray-900 my-20 bg-gradient-to-b from-white to-cream'>
       <div className='flex justify-center flex-col items-center'>
-        <h1 className='text-2xl md:text-3xl lg:text-5xl font-semibold text-center max-w-[500px] mb-8'>
-          Opening Offers
+      <div className='rounded-xl bg-grey px-4 py-1 text-sm text-gray-50'>Opening Offers</div>
+        <h1 className='text-4xl lg:text-5xl font-semibold text-center max-w-[500px] mt-4 mb-8'>
+          Don&apos;t miss out on huge savings!
         </h1>
-        <PricingTabs setMonthly={setMonthly} monthly={monthly} />
+        <hr className='border-t-2 border-grey w-20 mx-auto mt-2 mb-8' />
+        {/* <PricingTabs setMonthly={setMonthly} monthly={monthly} /> */}
         
       </div>
       <div className='flex justify-center w-full max-xl:overflow-auto pb-10'>
