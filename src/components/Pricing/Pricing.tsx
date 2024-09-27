5e0933354a2b9da2f5ce8ee1052228c04a225fbf
Adam <adamyoungy678@gmail.com>
2024-09-27 19:41:52 +0100

    Refactor Pricing component to update opening offers section

--- src/components/Pricing/Pricing.tsx
+++ src/components/Pricing/Pricing.tsx
@@ -9,21 +9,13 @@ export default function Pricing() {
   const [monthly, setMonthly] = useState(true);
 
   return (
-    <section className='px-4 sm:px-8 md:px-16 lg:px-24 mx-auto text-gray-900 my-20'>
+    <section className='py-12 md:py-24 lg:py-32 mx-auto text-gray-900 my-20 bg-gradient-to-b from-white to-cream'>
       <div className='flex justify-center flex-col items-center'>
         <h1 className='text-2xl md:text-3xl lg:text-5xl font-semibold text-center max-w-[500px] mb-8'>
-          Pick a plan thats right for you
+          Opening Offers
         </h1>
         <PricingTabs setMonthly={setMonthly} monthly={monthly} />
-        <div className='absolute -z-10'>
-          <Image
-            className='relative -z-10 opacity-40 top-[-40px]'
-            src='/assets/svgs/bg-outlines.svg'
-            alt='outline'
-            width={960}
-            height={380}
-          />
-        </div>
+        
       </div>
       <div className='flex justify-center w-full max-xl:overflow-auto pb-10'>
         <PricingCards monthly={monthly} />
