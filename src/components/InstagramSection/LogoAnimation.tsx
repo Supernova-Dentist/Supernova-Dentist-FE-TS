1f8a2f85d281571ec50923c908f71fc36f3639de
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-09-27 15:32:44 +0100

    add animation to own component

---
+++ src/components/InstagramSection/LogoAnimation.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import { DotLottieReact } from '@lottiefiles/dotlottie-react';
+
+export default function LogoAnimation() {
+  return (
+    <DotLottieReact
+      className='w-20 h-20'
+      autoplay
+      src='https://lottie.host/1b49c8d3-68ca-42f8-97ae-3d0cd9bbb85b/1CgOz9gN9t.json'
+    />
+  );
+}
