b4756eb744a281bb01d9db44242c74b9e8c96c70
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-10-01 11:58:17 +0100

    add basic promotion footer

---
+++ src/components/PromotionFooter/PromotionFooter.tsx
@@ -0,0 +1,9 @@
+export default function PromotionFooter() {
+  return (
+    <footer className='w-full px-10 py-4 bg-lightGold'>
+      <div>
+        <span className='text-gray-100'>&copy; Supernova Dental 2024</span>
+      </div>
+    </footer>
+  );
+}
