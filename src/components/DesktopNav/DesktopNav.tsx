6f7ed25e52f95820a83a414842d936b271162e38
Adam <adamyoungy678@gmail.com>
2024-10-06 13:11:26 +0100

    refactor: replace home link with supernova + logo and add layout changes to desktop nav

--- src/components/DesktopNav/DesktopNav.tsx
+++ src/components/DesktopNav/DesktopNav.tsx
@@ -1,29 +1,33 @@
 'use client';
 
+import { DentallyPortal, navLinks } from '@/lib/constants';
 import Link from 'next/link';
 import React from 'react';
-import { DentallyPortal, navLinks } from '@/lib/constants';
 import Button from '../Button/Button';
 import FlyoutLink from './FlyoutLink/FlyoutLink';
 
 export default function DesktopNav() {
   return (
     <nav className='bg-grey p-6'>
-      <ul className='flex justify-center gap-8 items-center '>
-        <FlyoutLink href='/' flyoutItems={[]}>
-          Home
-        </FlyoutLink>
-        {navLinks.map(({ name, flyout, url }) => (
-          <FlyoutLink key={name} href={url} flyoutItems={flyout}>
-            {name}
+      <ul className='flex gap-8 items-center justify-between'>
+        <div>
+          <FlyoutLink href='/' flyoutItems={[]}>
+            Supernova Dental <img src='/favicon.ico' alt='Supernova Dental Logo' className='ml-2 h-10 w-auto inline ' />
           </FlyoutLink>
-        ))}
+        </div>
 
-        {/* <li>
+        <div className='flex-1 flex gap-8 items-center justify-center'>
+          {navLinks.map(({ name, flyout, url }) => (
+            <FlyoutLink key={name} href={url} flyoutItems={flyout}>
+              {name}
+            </FlyoutLink>
+          ))}
+        </div>
+        <li>
           <Link target='_blank' href={`${DentallyPortal}`}>
-            <Button className='text-md bg-gold hover:bg-amber-700 text-cream px-4 py-2 rounded'>Book Now</Button>
+            <Button className='text-md bg-gold hover:bg-amber-700 text-cream px-4 py-2 rounded'>Sign Up!</Button>
           </Link>
-        </li> */}
+        </li>
       </ul>
     </nav>
   );
