3f73556049323712eaa493933a82ac7f69dd9711
Adam <adamyoungy678@gmail.com>
2024-10-12 01:52:57 +0100

    Refactor Welcome component: add id to welcome section

--- src/components/blocks/Welcome/Welcome.tsx
+++ src/components/blocks/Welcome/Welcome.tsx
@@ -1,16 +1,16 @@
+import ScrollVideoPlayer from '@/components/ScrollVideoPlayer/ScrollVideoPlayer';
 import { SparklesPreview } from '@/components/SparklesPreview/SparklesPreview';
+import dynamic from 'next/dynamic';
 import CtaButtons from '../Cta/CtaButtons';
 import { SlideTabsExample } from '../SlideTabs/SlideTabs';
 import { BubbleText } from './Utils/BubbleText';
-import ScrollVideoPlayer from '@/components/ScrollVideoPlayer/ScrollVideoPlayer';
-import dynamic from 'next/dynamic';
 const AuroraHero = dynamic(async () => await import('../AuroraHero/AuroraHero'), {
   ssr: false,
 });
 
 export default function Welcome() {
   return (
-    <div className='flex flex-col min-h-[100vh]'>
+    <div id='welcome' className='flex flex-col min-h-[100vh]'>
       <AuroraHero />
     </div>
   );
