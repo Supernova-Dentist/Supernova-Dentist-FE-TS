a44721750364ba6cf8a26742b1a7a350f6646a02
Adam <adamyoungy678@gmail.com>
2024-10-18 18:31:51 +0100

    feat: update google review data with new url fields

--- src/components/GoogleReview/googleReviewMockData.ts
+++ src/components/GoogleReview/googleReviewMockData.ts
@@ -6,6 +6,8 @@ export default [
     rating: 5,
     review:
       'I had quite a lot of work on my teeth done by Scott. He put me totally at ease and I was really pleased with the work he did. I’ve been back for check ups since and he is always so friendly and welcoming. Would highly recommend!',
+    url: 'https://g.co/kgs/dx1FLCj',
+    previewUrl: '/assets/images/maria-review.png',
   },
   {
     id: 2,
@@ -14,6 +16,8 @@ export default [
     rating: 5,
     review:
       "Being a very nervous patient I can't praise Scott and the nursing team highly enough. I never thought I would say it but I no longer dread a trip to the dentist. The appointments have always been pain free and I am made aware of what is being done. Well done to you all.",
+    url: 'https://g.co/kgs/31z8iQs',
+    previewUrl: '/assets/images/wendy-review.png',
   },
   {
     id: 3,
@@ -22,6 +26,8 @@ export default [
     rating: 5,
     review:
       'Absolutely amazing .. terrified of any dentist but Dr Scott Young made me feel relaxed and explained everything..  every step of the way .. and on my second appointment I had no anxiety no worry’s about anything especially any discomfort with treatment Thankyou',
+    url: 'https://g.co/kgs/NiDdSST',
+    previewUrl: '/assets/images/karen-review.png',
   },
   {
     id: 4,
@@ -30,6 +36,7 @@ export default [
     rating: 5,
     review:
       "Always had a professional and excellent experience with Lisa & Scott who've looked after my teeth for 2 years now and so much healthier.",
+    url: 'https://g.co/kgs/RRJBRYx',
+    previewUrl: '/assets/images/nicola-review.png',
   },
-  
 ];
