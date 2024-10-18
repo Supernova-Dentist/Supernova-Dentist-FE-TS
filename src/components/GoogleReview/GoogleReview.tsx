52cf0098067724cceff441dee5456db8a30de74a
Adam <adamyoungy678@gmail.com>
2024-10-18 18:33:02 +0100

    Refactor GoogleReview component: Update review item layout, add view on Google button, and handle click events

--- src/components/GoogleReview/GoogleReview.tsx
+++ src/components/GoogleReview/GoogleReview.tsx
@@ -2,9 +2,23 @@
 
 import Image from 'next/image';
 import { IoStar } from 'react-icons/io5';
+import { LinkPreview } from '../ui/link-preview';
 
-export default function GoogleReview({ name, date, review, rating }: GoogleReviewProps) {
+export default function GoogleReview({
+  name,
+  date,
+  review,
+  rating,
+  url,
+  previewUrl,
+  onClickReview,
+}: GoogleReviewProps) {
   const reviewStars = Array(rating).fill(0);
+
+  const handleViewOnGoogleClick = (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+    window.open(url, '_blank');
+  };
 
   return (
     <div className='min-w-[180px] flex flex-col justify-between'>
@@ -30,10 +44,30 @@ export default function GoogleReview({ name, date, review, rating }: GoogleRevie
         <div>
           <p className='text-[15px] max-h-[87px] overflow-hidden'>{review}</p>
         </div>
-        <div>
-          <span className='text-xs text-gray-500 leading-none cursor-pointer hover:underline hover:text-gray-800'>
-            Read more
-          </span>
+        <div className='flex flex-row w-full justify-between mt-2'>
+          <div>
+            <button
+              onClick={() => (onClickReview != null) && onClickReview({ name, date, review, rating, url, previewUrl })}
+              className='text-xs text-gray-500 bg-transparent border-none leading-none cursor-pointer hover:underline hover:text-gray-800'
+            >
+              Read more
+            </button>
+          </div>
+          <div>
+            <LinkPreview
+              isStatic
+              imageSrc={previewUrl}
+              url={url}
+              className='font-bold bg-clip-text text-transparent bg-gradient-to-br from-purple-500 to-pink-500'
+            >
+              <button
+                onClick={handleViewOnGoogleClick}
+                className='text-xs text-gray-500 leading-none cursor-pointer hover:underline hover:text-gray-800'
+              >
+                View on Google
+              </button>
+            </LinkPreview>
+          </div>
         </div>
       </div>
     </div>
