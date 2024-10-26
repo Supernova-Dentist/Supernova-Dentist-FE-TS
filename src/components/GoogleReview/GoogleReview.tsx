030d890e1a78e1c71f2c17049d001d8a5d57535f
Adam Young <adamyoungy678@gmail.com>
2024-10-26 21:25:42 +0100

    style: update GoogleReview component layout and styles

--- src/components/GoogleReview/GoogleReview.tsx
+++ src/components/GoogleReview/GoogleReview.tsx
@@ -21,9 +21,9 @@ export default function GoogleReview({
   };
 
   return (
-    <div className='min-w-[180px] flex flex-col justify-between'>
-      <div className='p-5 bg-white shadow-md cursor-pointer rounded-sm transform transition-transform duration-300 ease-in-out hover:-translate-y-2'>
-        <div className='flex flex-col'>
+    <div className='min-w-[180px] flex flex-col justify-between h-full'>
+      <div className='p-5 bg-white min-h-[240px] shadow-md cursor-pointer rounded-sm transform transition-transform duration-300 ease-in-out hover:-translate-y-2 flex flex-col h-full'>
+        <div className='flex flex-col flex-grow'>
           <div className='flex justify-between gap-2'>
             <span className='text-sm leading-none'>{name}</span>
             <Image
@@ -35,39 +35,35 @@ export default function GoogleReview({
             />
           </div>
           <span className='text-gray-500 text-xs leading-none'>{date}</span>
-        </div>
-        <div className='flex mt-4 mb-2'>
+        <div className='flex mt-2 mb-2'>
           {reviewStars.map((_, i) => (
             <IoStar color='#F6BB06' key={i} />
           ))}
         </div>
-        <div>
+        </div>
+        <div className='flex-grow'>
           <p className='text-[15px] max-h-[87px] overflow-hidden'>{review}</p>
         </div>
-        <div className='flex flex-row w-full justify-between mt-4 md:mt-2'>
-          <div>
+        <div className='flex flex-row w-full justify-center mt-4 space-x-6'>
+          <button
+            onClick={() => onClickReview && onClickReview({ name, date, review, rating, url, previewUrl })}
+            className='text-xs text-gray-500 bg-transparent border-none leading-none cursor-pointer hover:underline hover:text-gray-800'
+          >
+            Read more
+          </button>
+          <LinkPreview
+            isStatic
+            imageSrc={previewUrl}
+            url={url}
+            className='font-bold bg-clip-text text-transparent bg-gradient-to-br from-purple-500 to-pink-500'
+          >
             <button
-              onClick={() => onClickReview != null && onClickReview({ name, date, review, rating, url, previewUrl })}
-              className='text-xs text-gray-500 bg-transparent border-none leading-none cursor-pointer hover:underline hover:text-gray-800'
+              onClick={handleViewOnGoogleClick}
+              className='text-xs text-gray-500 leading-none cursor-pointer hover:underline hover:text-gray-800'
             >
-              Read more
+              View on Google
             </button>
-          </div>
-          <div>
-            <LinkPreview
-              isStatic
-              imageSrc={previewUrl}
-              url={url}
-              className='font-bold bg-clip-text text-transparent bg-gradient-to-br from-purple-500 to-pink-500'
-            >
-              <button
-                onClick={handleViewOnGoogleClick}
-                className='text-xs text-gray-500 leading-none cursor-pointer hover:underline hover:text-gray-800'
-              >
-                View on Google
-              </button>
-            </LinkPreview>
-          </div>
+          </LinkPreview>
         </div>
       </div>
     </div>
