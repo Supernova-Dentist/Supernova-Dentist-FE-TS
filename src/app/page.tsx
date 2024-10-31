15e9a9ef1cc487f604c500086f5b7c3951682389
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-10-31 14:33:21 +0000

    move pricing up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,8 +29,8 @@ export default function Home() {
       <AboutSection />
       <SmileTransformations />
       <GoogleReviews />
-      <Journey />
       <Pricing />
+      <Journey />
       <VideoAnimationSection />
       {/* <InstagramSection /> */}
       <Location />
