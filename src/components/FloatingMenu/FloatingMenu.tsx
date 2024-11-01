1bd015e3591a83f44507e9a25ae92df4af27150c
Adam <adamyoungy678@gmail.com>
2024-11-01 18:36:28 +0000

    feat: framer motion for floating menu

--- src/components/FloatingMenu/FloatingMenu.tsx
+++ src/components/FloatingMenu/FloatingMenu.tsx
@@ -1,10 +1,11 @@
 'use client';
 
-import { MdAdd, MdEmail } from 'react-icons/md';
+import { motion } from 'framer-motion';
 import { FaFacebookMessenger, FaInstagramSquare } from 'react-icons/fa';
-import { Fab, Action } from 'react-tiny-fab';
-import { useWindowSize } from '../blocks/MainNav/MainNav';
+import { MdAdd, MdEmail } from 'react-icons/md';
+import { Action, Fab } from 'react-tiny-fab';
 import 'react-tiny-fab/dist/styles.css';
+import { useWindowSize } from '../blocks/MainNav/MainNav';
 
 export default function FloatingMenu() {
   const { width } = useWindowSize();
@@ -25,22 +26,28 @@ export default function FloatingMenu() {
   }
 
   return (
-    <Fab
-      mainButtonStyles={{ backgroundColor: '#0f172a' }}
-      style={{ bottom: 0, right: 0 }}
-      icon={<MdAdd size={26} />}
-      event={event}
-      alwaysShowTitle={isMobile}
+    <motion.div
+      initial={{ opacity: 0, y: 20 }} // Initial state for the animation
+      animate={{ opacity: 1, y: 0 }} // Animate in
+      transition={{ duration: 1.5 }} // Duration of the animation
     >
-      <Action style={{ backgroundColor: '#0f172a' }} text='Email' onClick={handleEmailClick}>
-        <MdEmail size={25} />
-      </Action>
-      <Action style={{ backgroundColor: '#0f172a' }} text='Messenger' onClick={handleMessengerClick}>
-        <FaFacebookMessenger size={25} />
-      </Action>
-      <Action style={{ backgroundColor: '#0f172a' }} text='Instagram' onClick={handleInstagramClick}>
-        <FaInstagramSquare size={25} />
-      </Action>
-    </Fab>
+      <Fab
+        mainButtonStyles={{ backgroundColor: '#0f172a' }}
+        style={{ bottom: 0, right: 0 }}
+        icon={<MdAdd size={26} />}
+        event={event}
+        alwaysShowTitle={isMobile}
+      >
+        <Action style={{ backgroundColor: '#0f172a' }} text='Email' onClick={handleEmailClick}>
+          <MdEmail size={25} />
+        </Action>
+        <Action style={{ backgroundColor: '#0f172a' }} text='Messenger' onClick={handleMessengerClick}>
+          <FaFacebookMessenger size={25} />
+        </Action>
+        <Action style={{ backgroundColor: '#0f172a' }} text='Instagram' onClick={handleInstagramClick}>
+          <FaInstagramSquare size={25} />
+        </Action>
+      </Fab>
+    </motion.div>
   );
 }
