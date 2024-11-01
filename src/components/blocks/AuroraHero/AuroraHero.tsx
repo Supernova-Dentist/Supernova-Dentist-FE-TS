50acd0f148e9c5305e7b708cad514158bcb7a126
Adam <adamyoungy678@gmail.com>
2024-11-01 13:14:57 +0000

    feat: add framer motion for hero

--- src/components/blocks/AuroraHero/AuroraHero.tsx
+++ src/components/blocks/AuroraHero/AuroraHero.tsx
@@ -19,6 +19,7 @@ const COLORS_TOP = [
 const AuroraHero = () => {
   const color = useMotionValue(COLORS_TOP[0]);
 
+  // Animate the color value to cycle through COLORS_TOP
   useEffect(() => {
     void animate(color, COLORS_TOP, {
       ease: 'easeInOut',
@@ -34,15 +35,28 @@ const AuroraHero = () => {
 
   return (
     <motion.section
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 1.5 }}
       style={{
         backgroundImage,
       }}
       className='relative grid min-h-screen place-content-center overflow-hidden bg-gray-950 px-4 py-24 text-gray-200'
     >
-      <div className='relative z-10 flex flex-col items-center'>
-        <span className='mb-1.5 lg:mb-0 inline-block rounded-full bg-gray-600/50 px-3 py-1.5 text-sm'>
+      <motion.div
+        initial={{ y: -20, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ duration: 1, delay: 0.5 }}
+        className='relative z-10 flex flex-col items-center'
+      >
+        <motion.span
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1.2, delay: 0.7 }}
+          className='mb-2 inline-block rounded-full bg-gray-600/50 px-3 py-1.5 text-sm'
+        >
           Limited Time Offer Now Live!
-        </span>
+        </motion.span>
         <SparklesPreview />
 
         <motion.button
@@ -57,18 +71,23 @@ const AuroraHero = () => {
           whileTap={{
             scale: 0.985,
           }}
-          className='group relative flex w-fit items-center gap-1.5 lg:mt-4 rounded-full text-md md:text-lg lg:text-xl bg-gray-950/10 px-4 py-2 text-gray-50 transition-colors hover:bg-gray-950/50'
+          className='group relative flex w-fit items-center gap-1.5 lg:mt-4 rounded-full text-md md:text-lg lg:text-xl bg-gray-950/10 px-4 py-1.5 text-gray-50 transition-colors hover:bg-gray-950/50'
         >
           Sign Up Now
           <FiArrowRight className='transition-transform rotate-90 lg:rotate-0 lg:group-hover:rotate-90 group-active:-rotate-12' />
         </motion.button>
-      </div>
+      </motion.div>
 
-      <div className='absolute inset-0 z-0'>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 2, delay: 0.3 }}
+        className='absolute inset-0 z-0'
+      >
         <Canvas>
           <Stars radius={50} count={2500} factor={4} fade speed={2} />
         </Canvas>
-      </div>
+      </motion.div>
     </motion.section>
   );
 };
