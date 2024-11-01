4790e536950e59d1be878da6f0f9d83384af28ee
Adam <adamyoungy678@gmail.com>
2024-11-01 17:55:14 +0000

    fix: fix timeline height bug

--- src/components/Journey/Journey.tsx
+++ src/components/Journey/Journey.tsx
@@ -1,18 +1,27 @@
+'use client';
+
 import { Timeline } from '@/components/ui/timeline';
 import Image from 'next/image';
-import React from 'react';
+import React, { useState } from 'react';
 import InvisalignImage from '../../../public/assets/images/invisalign_aligners.png';
 import WhiteTeeth from '../../../public/assets/images/teeth-whitening.jpeg';
 import Veneers from '../../../public/assets/images/veneers.jpeg';
 
 export function Journey() {
+  const [imagesLoaded, setImagesLoaded] = useState(0); // Track number of loaded images
+
+  // This function is called when an image loads
+  const handleImageLoad = () => {
+    setImagesLoaded((prev) => prev + 1);
+  };
+
   const data = [
     {
       title: 'A - Align',
       content: (
         <div className='px-12 py-8'>
           <p className='mb-8 text-md md:text-lg lg:text-xl md:text-left text-center text-gray-800'>
-            Potentially less than than 6 months to straighten teeth to correct alignment
+            Potentially less than 6 months to straighten teeth to correct alignment
           </p>
           <Image
             src={InvisalignImage}
@@ -20,7 +29,8 @@ export function Journey() {
             width={100}
             height={800}
             layout='responsive'
-            className=' max-w-[30rem] mx-auto py-4'
+            className='max-w-[30rem] mx-auto py-4'
+            onLoad={handleImageLoad} // Pass handleImageLoad to onLoad
           />
         </div>
       ),
@@ -33,14 +43,14 @@ export function Journey() {
             We use exclusive whitening products that can only be given by a dentist that really bring out the natural
             white smile to give you the Supernova shine.
           </p>
-
           <Image
             src={WhiteTeeth}
             alt='White teeth'
             width={100}
             height={800}
             layout='responsive'
-            className=' max-w-[30rem] mx-auto py-4'
+            className='max-w-[30rem] mx-auto py-4'
+            onLoad={handleImageLoad} // Pass handleImageLoad to onLoad
           />
         </div>
       ),
@@ -49,7 +59,7 @@ export function Journey() {
       title: 'C - Contour',
       content: (
         <div className='px-12 py-8'>
-          <p className='mb-8 text-md md:text-lg lg:text-xl md:text-left  text-center text-gray-800'>
+          <p className='mb-8 text-md md:text-lg lg:text-xl md:text-left text-center text-gray-800'>
             Not always required and treatment is dependant on a case by case basis. With use of composite bonding or
             veneers we can really elevate cases when we have the teeth in the right position and colour.
           </p>
@@ -59,15 +69,17 @@ export function Journey() {
             width={100}
             height={800}
             layout='responsive'
-            className=' max-w-[30rem] mx-auto py-4'
+            className='max-w-[30rem] mx-auto py-4'
+            onLoad={handleImageLoad} // Pass handleImageLoad to onLoad
           />
         </div>
       ),
     },
   ];
+
   return (
     <div id='journey' className='w-full'>
-      <Timeline data={data} />
+      <Timeline data={data} imagesLoaded={imagesLoaded} totalImages={data.length} />
     </div>
   );
 }
--- src/components/ui/timeline.tsx
+++ src/components/ui/timeline.tsx
@@ -8,22 +8,23 @@ interface TimelineEntry {
   content: React.ReactNode;
 }
 
-export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
+export const Timeline = ({
+  data,
+  imagesLoaded,
+  totalImages,
+}: {
+  data: TimelineEntry[];
+  imagesLoaded: number;
+  totalImages: number;
+}) => {
   const ref = useRef<HTMLDivElement>(null);
   const containerRef = useRef<HTMLDivElement>(null);
   const [height, setHeight] = useState(0);
-  const [imagesLoaded, setImagesLoaded] = useState(0); // Track number of loaded images
 
-  // This function is called when an image loads
-  const handleImageLoad = () => {
-    setImagesLoaded((prev) => prev + 1);
-  };
-
-  // Use effect to update height after all images have loaded
-  // Use effect to update height after all images have loaded and on window resize
+  // Update height after all images have loaded and on window resize
   useEffect(() => {
     const updateHeight = () => {
-      if (imagesLoaded === data.length && ref.current) {
+      if (imagesLoaded === totalImages && ref.current) {
         const rect = ref.current.getBoundingClientRect();
         setHeight(rect.height);
       }
@@ -35,7 +36,7 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
     return () => {
       window.removeEventListener('resize', updateHeight); // Clean up listener on unmount
     };
-  }, [imagesLoaded, data.length]); // Dependencies: images loaded and data length
+  }, [imagesLoaded, totalImages]); // Dependencies: images loaded and total images
 
   const { scrollYProgress } = useScroll({
     target: containerRef,
@@ -51,63 +52,56 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
   const opacityTransform = useTransform(scrollYProgress, [0, 0.1], [0, 1]);
 
   return (
-    <motion.div
-      ref={refInView}
-      initial={{ opacity: 0, y: 20 }} // Start hidden and slightly lower
-      animate={inView ? { opacity: 1, y: 0 } : {}} // Animate when in view
-      transition={{ duration: 0.5 }} // Animation duration
-    >
-      <div className='w-full bg-white dark:bg-neutral-950 font-sans md:px-10' ref={containerRef}>
-        <div className='max-w-7xl mx-auto py-20 px-4 md:px-8 lg:px-10'>
-          <div className='flex justify-center mb-4'>
-            <div className='rounded-xl bg-gold px-4 py-1 text-sm text-gray-50'>Your Journey</div>
-          </div>
-          <h2 className='text-3xl md:text-4xl lg:text-5xl font-bold text-center mb-2 text-gold'>
-            Your Smile Transformation Journey
-          </h2>
-          <hr className='border-t-2 border-gold w-20 mx-auto mt-6 mb-8' />
-          <p className='text-xl md:text-2xl lg:text-2xl text-center text-gray-800'>
-            Improving your smile can be as easy as A B C. Here&apos;s how we can help you achieve the smile you&apos;ve
-            always wanted.
-          </p>
+    <div className='w-full bg-white dark:bg-neutral-950 font-sans md:px-10' ref={containerRef}>
+      <div className='max-w-7xl mx-auto py-20 px-4 md:px-8 lg:px-10'>
+        <div className='flex justify-center mb-4'>
+          <div className='rounded-xl bg-gold px-4 py-1 text-sm text-gray-50'>Your Journey</div>
         </div>
+        <h2 className='text-3xl md:text-4xl lg:text-5xl font-bold text-center mb-2 text-gold'>
+          Your Smile Transformation Journey
+        </h2>
+        <hr className='border-t-2 border-gold w-20 mx-auto mt-6 mb-8' />
+        <p className='text-xl md:text-2xl lg:text-2xl text-center text-gray-800'>
+          Improving your smile can be as easy as A B C. Here&apos;s how we can help you achieve the smile you&apos;ve
+          always wanted.
+        </p>
+      </div>
 
-        <div ref={ref} className='relative max-w-7xl mx-auto pb-20'>
-          {data.map((item, index) => (
-            <div key={index} className='flex justify-start pt-10 md:pt-40 md:gap-10'>
-              <div className='sticky flex flex-col md:flex-row z-40 items-center top-40 self-start max-w-xs lg:max-w-sm md:w-full'>
-                <div className='h-10 absolute left-3 md:left-3 w-10 rounded-full bg-white dark:bg-black flex items-center justify-center'>
-                  <div className='h-4 w-4 rounded-full bg-neutral-200 dark:bg-neutral-800 border border-neutral-300 dark:border-neutral-700 p-2' />
-                </div>
-                <h3 className='hidden md:block text-xl md:pl-20 md:text-5xl font-bold text-neutral-500 dark:text-neutral-500 '>
-                  {item.title}
-                </h3>
+      <div ref={ref} className='relative max-w-7xl mx-auto pb-20'>
+        {data.map((item, index) => (
+          <div key={index} className='flex justify-start pt-10 md:pt-40 md:gap-10'>
+            <div className='sticky flex flex-col md:flex-row z-40 items-center top-40 self-start max-w-xs lg:max-w-sm md:w-full'>
+              <div className='h-10 absolute left-3 md:left-3 w-10 rounded-full bg-white dark:bg-black flex items-center justify-center'>
+                <div className='h-4 w-4 rounded-full bg-neutral-200 dark:bg-neutral-800 border border-neutral-300 dark:border-neutral-700 p-2' />
               </div>
-
-              <div className='relative pl-20 pr-4 md:pl-4 w-full'>
-                <h3 className='md:hidden block text-2xl mb-4 text-left font-bold text-neutral-500 dark:text-neutral-500'>
-                  {item.title}
-                </h3>
-                {React.cloneElement(item.content as React.ReactElement, { onLoad: handleImageLoad })}
-              </div>
+              <h3 className='hidden md:block text-xl md:pl-20 md:text-5xl font-bold text-neutral-500 dark:text-neutral-500'>
+                {item.title}
+              </h3>
             </div>
-          ))}
-          <div
-            style={{
-              height: height + 'px',
-            }}
-            className='absolute md:left-8 left-8 top-0 overflow-hidden w-[2px] bg-[linear-gradient(to_bottom,var(--tw-gradient-stops))] from-transparent from-[0%] via-neutral-200 dark:via-neutral-700 to-transparent to-[99%] [mask-image:linear-gradient(to_bottom,transparent_0%,black_10%,black_90%,transparent_100%)] '
-          >
-            <motion.div
-              style={{
-                height: heightTransform,
-                opacity: opacityTransform,
-              }}
-              className='absolute inset-x-0 top-0 w-[2px] bg-gradient-to-t from-purple-500 via-blue-500 to-transparent from-[0%] via-[10%] rounded-full'
-            />
+
+            <div className='relative pl-20 pr-4 md:pl-4 w-full'>
+              <h3 className='md:hidden block text-2xl mb-4 text-left font-bold text-neutral-500 dark:text-neutral-500'>
+                {item.title}
+              </h3>
+              {item.content}
+            </div>
           </div>
+        ))}
+        <div
+          style={{
+            height: height + 'px',
+          }}
+          className='absolute md:left-8 left-8 top-0 overflow-hidden w-[2px] bg-[linear-gradient(to_bottom,var(--tw-gradient-stops))] from-transparent from-[0%] via-neutral-200 dark:via-neutral-700 to-transparent to-[99%] [mask-image:linear-gradient(to_bottom,transparent_0%,black_10%,black_90%,transparent_100%)] '
+        >
+          <motion.div
+            style={{
+              height: heightTransform,
+              opacity: opacityTransform,
+            }}
+            className='absolute inset-x-0 top-0 w-[2px] bg-gradient-to-t from-purple-500 via-blue-500 to-transparent from-[0%] via-[10%] rounded-full'
+          />
         </div>
       </div>
-    </motion.div>
+    </div>
   );
 };
