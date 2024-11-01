591427ca032d701214568afd76f3d69b30f9b2eb
Adam <adamyoungy678@gmail.com>
2024-11-01 19:47:33 +0000

    refactor: Update Timeline component to pass imagesLoaded prop
    - Update the Timeline component in the ui folder to include the imagesLoaded prop in the props interface.
    - Pass the imagesLoaded prop to the Timeline component in the Journey component.
    - Remove the onLoad event handler from the div element in the Timeline component.

--- src/components/Journey/Journey.tsx
+++ src/components/Journey/Journey.tsx
@@ -79,7 +79,7 @@ export function Journey() {
 
   return (
     <div id='journey' className='w-full'>
-      <Timeline data={data} setImagesLoaded={setImagesLoaded} />
+      <Timeline data={data} setImagesLoaded={setImagesLoaded} imagesLoaded={imagesLoaded} />
     </div>
   );
 }
