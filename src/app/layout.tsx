de83e760ac583aee53d01bb451ef05660b304993
Adam <adamyoungy678@gmail.com>
2024-11-02 22:31:53 +0000

    Refactor metadata in layout.tsx and update Supernova Dental website metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,8 +13,26 @@ import './reset.css';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Supernova Dental',
-  description: 'Description here',
+  title: 'Supernova Dental Bridgwater - Smile Brighter This November!',
+  description:
+    'Join Supernova Dental Bridgwater this November and enjoy 50% off your first exam and hygiene appointment! Sign up by Dec 31st and start your journey to a healthier, brighter smile. Invisalign opening offers available too!',
+  keywords:
+    'Supernova Dental, dental exam offer, dental hygiene offer, Invisalign Bridgwater, dental practice Bridgwater, teeth whitening, healthy smile, dentist Bridgwater',
+  openGraph: {
+    title: 'Supernova Dental Bridgwater - Smile Brighter This November!',
+    description:
+      'We’re opening in Bridgwater with an exclusive offer: 50% off your first exam and hygiene appointment if you sign up by Dec 31st! Discover our Invisalign opening offers and book your spot today.',
+    url: 'https://www.supernovadental.co.uk/',
+    type: 'website',
+    images: [
+      {
+        url: 'https://firebasestorage.googleapis.com/v0/b/supernova-dental.appspot.com/o/Supernova_Opening_Post.jpeg?alt=media&token=06c6642d-57dc-48a2-820d-0a332febe798',
+        width: 1080,
+        height: 1080,
+        alt: 'Supernova Dental - Smile Brighter Promotion',
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
