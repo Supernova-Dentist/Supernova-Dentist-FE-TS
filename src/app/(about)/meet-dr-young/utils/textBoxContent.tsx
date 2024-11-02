008c732ba50e01fd86ca000dcbba238dc2521d86
Adam <adamyoungy678@gmail.com>
2024-11-02 14:36:09 +0000

    remove unneeded directories

--- src/app/(about)/meet-dr-young/utils/textBoxContent.tsx
+++
@@ -1,6 +0,0 @@
-export default {
-  textOne:
-    "Beyond his work at the office, Dr. Raymond is passionate about his involvement in dental outreach programs, both within the United States and abroad. During his most recent mission trip abroad, Dr. Raymond spent 10 days in the Dominican Republic, providing free dental care to underserved children. He also serves on the board of the Arizona Dental Foundation and spearheaded the dental charity TDC Cares (Tucson Dental Community Cares) providing services to veterans and job-seeking homeless citizens of Tucson. Learn more about Skyline Dental's charity and community efforts here.",
-  textTwo:
-    'On the weekends, you will find Dr. Raymond either trail running in the Catalinas or sitting atop Mt. Lemmon, pondering the existence of pine trees in the desert with his wife, Jessie, and dog, Rudy. Apart from spending time with his family, Dr. Raymond loves traveling, yoga, surfing, and golf.',
-};
--- src/app/(about)/team/textBoxContent.tsx
+++
@@ -1,5 +0,0 @@
-export default {
-  title: 'Dr. Young',
-  textOne:
-    '“I&apos;m so proud of my team here at Supernova Dental. We do such great work together serving our patients and our community!”',
-};
