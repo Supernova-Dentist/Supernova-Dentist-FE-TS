f8b53370fb0eed7f40f13721fcd73e68fcdd4ee0
Adam Young <adamyoungy678@gmail.com>
2024-11-20 19:36:42 +0000

    feat: bring over component for sign up form from promo page@

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,11 +6,13 @@ import Reviews from '@/components/blocks/Reviews/Review';
 import Steps from '@/components/blocks/Steps/Steps';
 import Welcome from '@/components/blocks/Welcome/Welcome';
 import IntroHome from '@/components/IntroHome/IntroHome';
+import PromotionForm from '@/components/PromotionForm/PromotionForm';
 
 export default function Home() {
   return (
     <main>
       <Welcome />
+      <PromotionForm />
       <IntroHome />
       <Steps />
       <Reviews />
