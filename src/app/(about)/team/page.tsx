a8ceeb4d96ef57ff3cb8c6217317986c7ae05d91
Adam Young <adamyoungy678@gmail.com>
2024-12-07 14:51:45 +0000

    refactor: update TeamPage layout and improve styling

--- src/app/(about)/team/page.tsx
+++ src/app/(about)/team/page.tsx
@@ -9,22 +9,17 @@ export const metadata: Metadata = {
 
 export default function TeamPage() {
   return (
-    <div className='flex flex-col min-h-screen '>
-      {/* Hero Section */}
-      <section className="flex flex-col justify-center items-center h-120 w-full bg-[url('/assets/images/aiTeamImage.jpg')] bg-cover bg-center bg-no-repeat py-20 md:py-32 lg:py-40">
-        <h1 className='text-3xl font-extrabold text-center text-gold sm:text-4xl md:text-5xl'>Meet Our Dental Team</h1>
-        <p className='mx-auto mt-4 max-w-2xl text-lg text-center text-white'>
-          Discover the dedicated professionals who make our dental practice exceptional.
-        </p>
-      </section>
-      <section className='w-full py-12 md:py-24 lg:py-32 bg-cream'>
-        {/* Breadcrumb */}
-        <div className='mx-auto px-4 md:px-6 container pt-8 pb-4'>
-          <BreadCrumb />
-        </div>
+    <div className='flex flex-col min-h-screen justify-center align-center bg-gradient-to-b from-white to-cream'>
+      <h1 className='text-3xl font-extrabold text-center text-gold md:text-5xl lg:text-7xl'>Meet Our Dental Team</h1>
+      <p className='mx-auto mt-4 max-w-2xl text-md md:text-xl lg:text-2xl text-center'>
+        Discover the dedicated professionals who make our dental practice exceptional.
+      </p>
+      {/* Breadcrumb */}
+      <div className='mx-auto px-4 md:px-6 container pt-8 pb-4'>
+        <BreadCrumb />
+      </div>
 
-        <TeamMemberGrid />
-      </section>
+      <TeamMemberGrid />
     </div>
   );
 }
