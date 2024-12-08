1d90dc880826736131ca64f03c0168020188c26b
Adam <adamyoungy678@gmail.com>
2024-12-08 17:05:29 +0000

    refactor: reorganize imports and replace ProcessSection with InvisalignJourney in InvisalignContent

--- src/components/blocks/InvisalignContent/InvisalignContent.tsx
+++ src/components/blocks/InvisalignContent/InvisalignContent.tsx
@@ -1,26 +1,28 @@
-import HeroSection from '@/components/HeroSection/HeroSection';
-import BreadCrumb from '@/components/BreadCrumb/BreadCrumb';
-import InfoSection from '@/components/InfoSection/InfoSection';
-import TestimonialsSection from '@/components/TestimonialsSection/TestimonialsSection';
-import ComparisonSection from '@/components/ComparisonSection/ComparisonSection';
-import BenefitSection from '@/components/BenefitSection/BenefitSection';
-import ProcessSection from '@/components/ProcessSection/ProcessSection';
 import AftercareSection from '@/components/AftercareSection/AftercareSection';
+import BenefitSection from '@/components/BenefitSection/BenefitSection';
+import BreadCrumb from '@/components/BreadCrumb/BreadCrumb';
+import ComparisonSection from '@/components/ComparisonSection/ComparisonSection';
 import FaqSection from '@/components/FaqSection/FaqSection';
-import invisalignHeroData from './contentData/heroData';
-import benefitsData from './contentData/benefitsData';
-import processData from './contentData/processData';
-import testimonialsData from './contentData/testimonialsData';
-import aftercareData from './contentData/aftercareData';
-import { tableData } from './contentData/tableData';
-import { faqItems } from './contentData/faqData';
-import infoData from './contentData/infoData';
+import HeroSection from '@/components/HeroSection/HeroSection';
+import InfoSection from '@/components/InfoSection/InfoSection';
+import ProcessSection from '@/components/ProcessSection/ProcessSection';
+import TestimonialsSection from '@/components/TestimonialsSection/TestimonialsSection';
+import ServicePageWrapper from '@/hoc/ServicePageWrapper';
 import { SlideTabsExample } from '../SlideTabs/SlideTabs';
+import aftercareData from './contentData/aftercareData';
+import benefitsData from './contentData/benefitsData';
+import { faqItems } from './contentData/faqData';
+import invisalignHeroData from './contentData/heroData';
+import infoData from './contentData/infoData';
+import processData, { InvisalignJourney } from './contentData/processData';
+import { tableData } from './contentData/tableData';
+import testimonialsData from './contentData/testimonialsData';
 
 export default function InvisalignContent() {
   return (
     <>
       <HeroSection data={invisalignHeroData} />
+
       <div className='mx-auto px-4 md:px-6 container pt-10 pb-6 md:py-12'>
         <BreadCrumb />
       </div>
@@ -29,7 +31,7 @@ export default function InvisalignContent() {
       </div>
       <InfoSection id='info' data={infoData} />
       <BenefitSection id='benefits' data={benefitsData} />
-      <ProcessSection id='process' data={processData} />
+      <InvisalignJourney id='process' data={processData} />
       <TestimonialsSection id='testimonials' data={testimonialsData} />
       <AftercareSection data={aftercareData} />
       <ComparisonSection id='comparison' data={tableData} />
