e1c3f9b43b6b0048315fc330b8ae9cd6a20d000f
Adam Young <adamyoungy678@gmail.com>
2024-12-08 21:32:04 +0000

    fix: update background color in ProcessSection and adjust paragraph spacing for consistency

--- src/components/ProcessSection/ProcessSection.tsx
+++ src/components/ProcessSection/ProcessSection.tsx
@@ -13,21 +13,18 @@ interface ProcessData {
 
 export default function ProcessSection({ data, id }: { data: ProcessData; id: string }) {
   const { title, description, stepContent, timelineData } = data;
-  
 
   return (
-    <section id={id} className='py-12 px-4 md:py-20 lg:py-28 bg-cream'>
+    <section id={id} className='py-12 px-4 md:py-20 lg:py-28 bg-grey'>
       <div className='container mx-auto px-4 md:px-6'>
         <SectionTitle title={title} />
         <hr className='border-t-2 border-gold w-20 mx-auto mt-4 mb-8' />
         <div className='flex justify-center'>
-          <p className='mt-4 mb-6 max-w-[720px]'>{description}</p>
+          <p className='mt-4 mb-8 max-w-[720px] text-cream text-lg'>{description}</p>
         </div>
         {/* Conditionally render Timeline or SteppedProgress based on the presence of timelineData */}
         {timelineData && timelineData.length > 0 ? (
-          <InvisalignTimeline
-            data={timelineData}
-          />
+          <InvisalignTimeline data={timelineData} />
         ) : (
           <div className='mt-6 grid gap-6 md:grid-cols-2 lg:grid-cols-3'>
             {stepContent.map(
