578b0a909d8f13786c75a6b9ba23d1d11e60045c
Adam Young <adamyoungy678@gmail.com>
2024-12-08 12:14:01 +0000

    feat: update Invisalign content images and remove unnecessary classes

--- src/components/blocks/InvisalignContent/contentData/processData.tsx
+++ src/components/blocks/InvisalignContent/contentData/processData.tsx
@@ -30,16 +30,17 @@ export default {
       title: 'Initial Consultation',
       content: (
         <div>
-          <p className='text-cream text-xs md:text-sm font-normal mb-8'>
+          <p className='text-xs md:text-sm font-normal mb-8'>
             Your journey begins with a consultation to discuss your goals and assess your dental health. The dentist
             will take digital scans and X-rays to create a customized treatment plan.
           </p>
           <div className='grid grid-cols-2 gap-4'>
             <Image
-              src='/assets/images/consultation.jpg'
+              src='/assets/images/Invisalign/Supernova-33.jpg'
               alt='Initial Consultation'
               width={500}
               height={500}
+              layout={'responsive'}
               className='rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]'
             />
           </div>
@@ -50,16 +51,17 @@ export default {
       title: 'Receiving Your Aligners',
       content: (
         <div>
-          <p className='text-cream text-xs md:text-sm font-normal mb-8'>
+          <p className=' text-xs md:text-sm font-normal mb-8'>
             Once your aligners are ready, you will receive them along with instructions on how to wear and care for
             them. Your dentist will check the fit and make adjustments if necessary.
           </p>
           <div className='grid grid-cols-2 gap-4'>
             <Image
-              src='/assets/images/aligners.jpg'
+              src='/assets/images/Invisalign/Supernova-16.jpg'
               alt='Receiving Aligners'
               width={500}
               height={500}
+              layout={'responsive'}
               className='rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]'
             />
           </div>
@@ -70,16 +72,17 @@ export default {
       title: 'Progress Check-ups',
       content: (
         <div>
-          <p className='text-cream text-xs md:text-sm font-normal mb-8'>
+          <p className=' text-xs md:text-sm font-normal mb-8'>
             Regular check-ups are scheduled to monitor your progress. Your dentist will ensure that your teeth are
             moving according to the treatment plan and make adjustments as needed.
           </p>
           <div className='grid grid-cols-2 gap-4'>
             <Image
-              src='/assets/images/checkup.jpg'
+              src='/assets/images/Invisalign/Supernova-30.jpg'
               alt='Progress Check-ups'
               width={500}
               height={500}
+              layout={'responsive'}
               className='rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]'
             />
           </div>
@@ -90,16 +93,17 @@ export default {
       title: 'Final Stages and Retainers',
       content: (
         <div>
-          <p className='text-cream text-xs md:text-sm font-normal mb-8'>
+          <p className=' text-xs md:text-sm font-normal mb-8'>
             As you near the end of your treatment, you will receive your final set of aligners. After completing the
             treatment, you will be given retainers to maintain the new position of your teeth.
           </p>
           <div className='grid grid-cols-2 gap-4'>
             <Image
-              src='/assets/images/retainers.jpg'
+              src='/assets/images/Invisalign/Supernova-14.jpg'
               alt='Final Stages'
               width={500}
               height={500}
+              layout={'responsive'}
               className='rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]'
             />
           </div>
