c3af35c7d74808a5a786ecb0ba49e20d4d1d3fe6
Adam Young <adamyoungy678@gmail.com>
2024-12-08 21:13:37 +0000

    fix: enhance styling in ServiceDescription component for better responsiveness

--- src/components/ServiceDescription/ServiceDescription.tsx
+++ src/components/ServiceDescription/ServiceDescription.tsx
@@ -1,5 +1,7 @@
 import { cn } from '@/lib/utils';
 
 export default function ServiceDescription({ description, className }: ServiceDescription) {
-  return <p className={cn('text-lightGrey text-left text-lg', className)}>{description}</p>;
+  return (
+    <p className={cn('text-lightGrey text-left text-lg lg:text-xl max-w-4xl mx-auto', className)}>{description}</p>
+  );
 }
