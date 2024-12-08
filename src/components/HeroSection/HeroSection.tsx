e73af5f5dd003f7ee9e4ecea15cac0af9ec9248e
Adam <adamyoungy678@gmail.com>
2024-12-08 17:05:40 +0000

    fix: update HeroSection layout for improved alignment and text centering

--- src/components/HeroSection/HeroSection.tsx
+++ src/components/HeroSection/HeroSection.tsx
@@ -2,12 +2,11 @@ import ServiceBox from '../blocks/ServiceBox/ServiceBox';
 
 export default function HeroSection({ data }: HeroSection) {
   return (
-    <section className="relative max-w-[100%] mx-auto bg-[url('/assets/images/Supernova-cover.jpg')] bg-cover bg-center bg-no-repeat py-20 md:py-32 lg:py-40">
-      <div className='container mx-auto px-4 md:px-6'>
+    <section className="relative flex items-center justify-center max-w-[100%] mx-auto bg-[url('/assets/images/Supernova-cover.jpg')] bg-cover bg-center bg-no-repeat py-20 md:py-32 lg:py-40">
+      <div className=' mx-auto px-6 text-center'>
         <ServiceBox
           title={data.header.title}
           subTitle={data.header.subTitle}
-          mainText={data.mainText}
           benefitText={data.benefitText}
           stats={data.serviceStats}
         />
