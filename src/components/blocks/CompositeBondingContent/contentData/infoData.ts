04f613138b4c7db80b49ddd402b256acc5ffc018
Adam Young <adamyoungy678@gmail.com>
2024-12-08 19:03:55 +0000

    fix: rename directory

--- src/components/blocks/CompositeBondingContent/contentData/infoData.ts
+++ src/components/blocks/CompositeBondingContent/contentData/infoData.ts
@@ -4,6 +4,7 @@ export default {
     'Composite bonding is a highly versatile and popular cosmetic dental procedure used to enhance the appearance of teeth. It involves applying a tooth-colored resin material to the surface of the teeth, which is then shaped and polished to match the surrounding teeth for a natural look.',
     'This treatment has been in use for several decades and has a well-established safety record. Advances in dental materials and techniques have made this procedure more effective and longer-lasting than ever before.',
   ],
+  videoSrc: '/assets/videos/compositeBondingVideo.mp4',
   advantagesTitle: 'Advantages of Composite Bonding',
   useCaseTitle: 'Use Cases of Composite Bonding',
   advantagesData: [
