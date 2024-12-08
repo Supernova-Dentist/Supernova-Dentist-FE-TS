f339f5f0d8e6090e7c6c34ae4e0412baa0f10b6b
Adam Young <adamyoungy678@gmail.com>
2024-12-08 21:13:31 +0000

    fix: add spacing in InfoSection component for improved layout

--- src/components/InfoSection/InfoSection.tsx
+++ src/components/InfoSection/InfoSection.tsx
@@ -19,6 +19,7 @@ export default function InfoSection({ data, id }: { data: any; id?: string }) {
           <div>
             <ServiceDescription className='mt-6 mb-4' description={descriptions[0]} />
             <ServiceDescription description={descriptions[1]} />
+            
           </div>
           {/* Video */}
           <ServiceVideo videoSrc={videoSrc} />
