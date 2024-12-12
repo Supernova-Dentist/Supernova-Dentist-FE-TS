a40b1867c7c80f35513a3d52d10bf334738ce5ab
Adam Young <adamyoungy678@gmail.com>
2024-12-12 19:55:32 +0000

    refactor: Update metadata in layout.tsx with enhanced descriptions and new image for openGraph

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,27 +15,26 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata: Metadata = {
   title: 'Supernova Dental - Expert Dental Care in Bridgwater | 50% Off First Exam & Hygiene Appointment',
   description:
-    'Join Supernova Dental in Bridgwater for exclusive offers: 50% off your first dental exam and hygiene appointment, plus special Invisalign deals. Book today and start your journey to a healthier smile!',
+    'Join Supernova Dental in Bridgwater for exclusive offers: 50% off your first dental exam and hygiene appointment, plus special Invisalign deals if you sign up by December 31st 2024. Book today and start your journey to a healthier smile!',
   keywords:
     'Supernova Dental, dental exam offer, dental hygiene offer, Invisalign Bridgwater, dental practice Bridgwater, teeth whitening Bridgwater, healthy smile, dentist Bridgwater, best dentist in Bridgwater, cosmetic dentistry Bridgwater, affordable Invisalign',
-    openGraph: {
-      title: 'Supernova Dental - Exclusive Offers for New Patients',
-      description:
-        'Join Supernova Dental in Bridgwater with exclusive offers: 50% off your first dental exam and hygiene appointment and special Invisalign deals. Book today!',
-      url: 'https://www.supernovadental.co.uk/',
-      type: 'website',
-      locale: 'en_GB',
-      siteName: 'Supernova Dental',
-      images: [
-        {
-          url: 'https://firebasestorage.googleapis.com/v0/b/supernova-dental.appspot.com/o/Supernova_Opening_Post.jpeg?alt=media&token=06c6642d-57dc-48a2-820d-0a332febe798',
-          width: 1080,
-          height: 1080,
-          alt: 'Supernova Dental - Smile Brighter Promotion',
-        },
-      ],
-    }
-    
+  openGraph: {
+    title: 'Supernova Dental - Exclusive Offers for New Patients',
+    description:
+      'Join Supernova Dental in Bridgwater with exclusive offers: 50% off your first dental exam and hygiene appointment and special Invisalign deals if you sign up by December 31st 2024. Book today!',
+    url: 'https://www.supernovadental.co.uk/',
+    type: 'website',
+    locale: 'en_GB',
+    siteName: 'Supernova Dental',
+    images: [
+      {
+        url: 'https://firebasestorage.googleapis.com/v0/b/supernova-dental.appspot.com/o/Supernova_cover.png?alt=media&token=fce9e004-b06b-4348-a65f-ce6371631b15',
+        width: 1080,
+        height: 1080,
+        alt: 'Supernova Dental - Smile Brighter Promotion',
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
