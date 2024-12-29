05e798547d8f3329f0c3ab9b8650879b56239c8d
Adam Young <adamyoungy678@gmail.com>
2024-12-29 00:57:15 +0000

    refactor: remove ComingSoonSection from Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -51,7 +51,6 @@ export default function Home() {
       {/* <InstagramSection /> */}
       <FindUs />
       <PromotionFAQ />
-      <ComingSoonSection />
       {/* <FloatingMenu /> */}
 
       {/* <Reviews /> */}
