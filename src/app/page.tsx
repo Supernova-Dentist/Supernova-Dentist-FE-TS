3391fcc77d20abb4b4a6e31be57bc45cd86806a5
Adam Young <adamyoungy678@gmail.com>
2024-12-30 18:34:08 +0000

    feat: add metadata for Home page to enhance SEO and user engagement

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,10 @@ import AccessibilityWidget from '@/components/blocks/AccessibilityWidget/Accessi
 import GalleryCarousel from '@/components/blocks/GalleryCarousel/GalleryCarousel';
 import DynamicGoogleMap from '@/components/blocks/GoogleMap/DynamicGoogleMap';
 import dynamic from 'next/dynamic';
-// import Services from '@/components/Services/Services';
-// import GalleryCarousel from '@/components/blocks/GalleryCarousel/GalleryCarousel';
-// import GoogleReviews from '@/components/blocks/GoogleReviews/GoogleReviews';
-// import Reviews from '@/components/blocks/Reviews/Review';
-// import Steps from '@/components/blocks/Steps/Steps';
 import AboutSection from '@/components/AboutSection/AboutSection';
 import GoogleReviews from '@/components/blocks/GoogleReviews/GoogleReviews';
 import Reviews from '@/components/blocks/Reviews/Review';
 import Welcome from '@/components/blocks/Welcome/Welcome';
-import IntroHome from '@/components/IntroHome/IntroHome';
 import PromotionForm from '@/components/PromotionForm/PromotionForm';
 import Steps from '@/components/blocks/Steps/Steps';
 import Pricing from './(about)/pricing/page';
@@ -20,44 +14,49 @@ import { Journey } from '@/components/Journey/Journey';
 import { VideoAnimationSection } from '@/components/VideoAnimationSection/VideoAnimationSection';
 import FindUs from '@/components/FindUs/FindUs';
 import PromotionFAQ from '@/components/PromotionFAQ/PromotionFAQ';
-import ComingSoonSection from '@/components/ComingSoon/ComingSoon';
-import InstagramSection from '@/components/InstagramSection/InstagramSection';
 import MeetTeamSection from '@/components/MeetTeamSection/MeetTeamSection';
 
 const FloatingMenu = dynamic(async () => await import('@/components/FloatingMenu/FloatingMenu'), {
   ssr: false,
 });
 
+export const metadata = {
+  title: 'Supernova Dental - 50% Off Your First Exam & Hygiene Appointment in Bridgwater',
+  description:
+    "Take advantage of Supernova Dental's 50% off offer on your first exam and hygiene appointment in Bridgwater. Book now and start your journey to a healthier smile!",
+  keywords:
+    'dental exam offer, dental hygiene offer, Invisalign, Bridgwater dentist, teeth whitening, healthy smile, dentist promotions',
+  openGraph: {
+    title: 'Supernova Dental - Exclusive Offers for New Patients',
+    description:
+      'Join Supernova Dental in Bridgwater with exclusive offers: 50% off your first dental exam and hygiene appointment. Book today!',
+    url: 'https://www.supernovadental.co.uk',
+    type: 'website',
+    images: [
+      {
+        url: '/assets/images/logo.png',
+        width: 500,
+        height: 500,
+        alt: 'Supernova Dental Logo',
+      },
+    ],
+  },
+};
+
 export default function Home() {
   return (
     <main>
       <Welcome />
       <PromotionForm />
-      {/* <IntroHome /> */}
-      {/* <Steps /> */}
-      {/* <Reviews />
-      <GalleryCarousel />
-      <GoogleReviews />
-      <DynamicGoogleMap /> */}
       <AccessibilityWidget />
-
       <AboutSection />
       <MeetTeamSection />
-      {/* <Pricing /> */}
       <SmileTransformations />
       <GoogleReviews />
       <Journey />
       <VideoAnimationSection />
-      {/* <InstagramSection /> */}
       <FindUs />
       <PromotionFAQ />
-      
-
-      {/* <Reviews /> */}
-      {/* <Services />
-      <Steps />
-      <GalleryCarousel />
-      <GoogleMap /> */}
     </main>
   );
 }
