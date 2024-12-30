56229b0dc0ee2904f843636544e7b708becf8553
Adam Young <adamyoungy678@gmail.com>
2024-12-30 00:39:08 +0000

    refactor: remove FloatingMenu component from Home page for code cleanup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -51,7 +51,7 @@ export default function Home() {
       {/* <InstagramSection /> */}
       <FindUs />
       <PromotionFAQ />
-      <FloatingMenu />
+      
 
       {/* <Reviews /> */}
       {/* <Services />
