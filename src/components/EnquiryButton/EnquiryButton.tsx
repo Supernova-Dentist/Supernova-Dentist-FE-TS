65288d9163c7887e3c0c51138286f87e93b8db90
Adam Young <adamyoungy678@gmail.com>
2024-12-30 15:01:44 +0000

    fix: update enquiry button styling and positioning for better visibility

--- src/components/EnquiryButton/EnquiryButton.tsx
+++ src/components/EnquiryButton/EnquiryButton.tsx
@@ -8,10 +8,10 @@ interface Props {
 
 const EnquiryButton = ({ referringPage }: Props) => {
   return (
-    <div className='fixed inset-x-0 bottom-5 md:fixed md:right-28 flex justify-center md:justify-end'>
+    <div className='flex justify-center mx-auto'>
       <Link
         href={`/enquiry?ref=${encodeURIComponent(referringPage)}`}
-        className='bg-blue-500 text-white py-3 px-6 rounded-full shadow-lg transition-transform duration-300 ease-in-out hover:scale-110 hover:shadow-xl'
+        className='bg-gold text-white py-3 px-6 rounded-full shadow-lg transition-transform duration-300 ease-in-out hover:scale-110 hover:shadow-xl'
       >
         Enquiry
       </Link>
