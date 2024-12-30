b879dd60cea0e52e3f29d9d5af9a710d3bb565c4
Adam Young <adamyoungy678@gmail.com>
2024-12-30 17:06:49 +0000

    fix: enhance ServiceVideo component with border and rounded corners for improved aesthetics

--- src/components/ServiceVideo/ServiceVideo.tsx
+++ src/components/ServiceVideo/ServiceVideo.tsx
@@ -4,14 +4,15 @@ interface ServiceVideoProps {
 
 export default function ServiceVideo({ videoSrc }: ServiceVideoProps) {
   return (
-    <div className='w-full relative pb-[56.25%]'>
-      {' '}
+    <div className='w-full relative pb-[56.25%] overflow-hidden border-4 border-gold rounded-lg'>
       {/* 16:9 Aspect Ratio */}
       <iframe
         src={videoSrc}
         title='video'
-        className='absolute top-0 left-0 w-full h-full'
-        frameBorder='0'
+        className='absolute top-0 left-0 w-full h-full object-cover rounded-lg' 
+        style={{
+          border: 'none',
+        }}
         allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
         allowFullScreen
       />
