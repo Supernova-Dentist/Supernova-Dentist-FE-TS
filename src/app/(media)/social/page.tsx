4cfd4981343516da4e5e197b984b0771342a38b0
Adam Young <adamyoungy678@gmail.com>
2024-12-30 19:24:18 +0000

    feat: add HighlightCard component to SocialMedia page for enhanced user engagement

--- src/app/(media)/social/page.tsx
+++ src/app/(media)/social/page.tsx
@@ -5,6 +5,7 @@ import InstagramPostGrid from './utils/InstagramPostGrid';
 import PageHero from '@/components/blocks/PageHero/PageHero';
 import GalleryCarousel from '@/components/blocks/GalleryCarousel/GalleryCarousel';
 import DynamicGoogleMap from '@/components/blocks/GoogleMap/DynamicGoogleMap';
+import { HighlightCard } from '@/components/HighlightCard/HighlightCard';
 
 export default async function SocialMedia() {
   const instagramPosts = await fetchInstagramPosts();
@@ -13,16 +14,18 @@ export default async function SocialMedia() {
 
   return (
     <>
-      <PageHero bgImage='bg-blog' title='Social' showButtons={false} className='bg-center' />
-      <div className='flex flex-col items-center justify-start py-12 bg-cream'>
+      <HighlightCard
+        title='Social - Supernova Dental'
+        description='Follow us on Instagram for the latest updates and dental tips'
+        logoSrc='/assets/images/logo.png'
+        className='mt-16'
+      />
+      <div className='flex flex-col items-center justify-start py-12'>
         <div className='w-full max-w-7xl px-6 sm:px-8 lg:px-12'>
           <BreadCrumb />
           <div className='flex flex-col sm:flex-row sm:items-center justify-between'>
             <div>
               <h1 className='text-3xl font-bold break-words text-grey'>{username}</h1>
-              <p className='text-muted-foreground text-lightGrey'>
-                Follow us on Instagram for the latest updates and dental tips.
-              </p>
             </div>
             <a
               href={instagramProfileUrl}
@@ -42,8 +45,6 @@ export default async function SocialMedia() {
           )}
         </div>
       </div>
-      <GalleryCarousel />
-      <DynamicGoogleMap />
     </>
   );
 }
