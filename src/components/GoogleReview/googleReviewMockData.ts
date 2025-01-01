4271f252cc77e5edddc52f94e11c4f8e92b4c175
Adam <adamyoungy678@gmail.com>
2025-01-01 14:25:20 +0000

    feat: update google reviews

--- src/components/GoogleReview/googleReviewMockData.ts
+++ src/components/GoogleReview/googleReviewMockData.ts
@@ -1,26 +1,45 @@
 export default [
   {
     id: 1,
-    name: 'Maria Heasell',
-    date: '2 years ago',
+    name: 'Anthony Hales',
+    date: 'a week ago',
     rating: 5,
     review:
-      'I had quite a lot of work on my teeth done by Scott. He put me totally at ease and I was really pleased with the work he did. I’ve been back for check ups since and he is always so friendly and welcoming. Would highly recommend!',
-    url: 'https://g.co/kgs/dx1FLCj',
-    previewUrl: '/assets/images/maria-review.png',
+      'I was so nervous about having dental treatment as I have had bad experiences before but when I signed up with supernova dental they was very understanding I had my first treatment on the 16th December they was amazing going back on the 19th for more treatment the nerves are gone I would highly recommend supernova dental ANTHONY BURNELL.',
+    url: 'https://g.co/kgs/vfJmXQt',
+    previewUrl: '/assets/images/anthony-review.png',
   },
   {
     id: 2,
-    name: 'Wendy Gregory',
-    date: '2 years ago',
+    name: 'Nigel White',
+    date: 'a week ago',
     rating: 5,
     review:
-      "Being a very nervous patient I can't praise Scott and the nursing team highly enough. I never thought I would say it but I no longer dread a trip to the dentist. The appointments have always been pain free and I am made aware of what is being done. Well done to you all.",
-    url: 'https://g.co/kgs/31z8iQs',
-    previewUrl: '/assets/images/wendy-review.png',
+      'Absolutely first class emergency same day modern technology pain-free fantastic staff I would highly recommend this Dentist to my family and friends',
+    url: 'https://g.co/kgs/hr2p2ee',
+    previewUrl: '/assets/images/nigel-review.png',
   },
   {
     id: 3,
+    name: 'Dean Whitaker',
+    date: 'a week ago',
+    rating: 5,
+    review:
+      'I am not a great patient when it comes to the dentist. But I have found my experience at Supernova so far to be fabulous. The team there were very friendly and professional putting me at ease straight away. Scott has made me feel much more confident in visiting the dentist, and has been fantastic.',
+    url: 'https://g.co/kgs/qiUhyye',
+    previewUrl: '/assets/images/dean-review.png',
+  },
+  {
+    id: 4,
+    name: 'Ray Cox',
+    date: '2 weeks ago',
+    rating: 5,
+    review: "Great staff and service - Best experience I've had and it was very informative.",
+    url: 'https://g.co/kgs/2fek9h5',
+    previewUrl: '/assets/images/ray-review.png',
+  },
+  {
+    id: 5,
     name: 'Karen Bligh',
     date: 'a year ago',
     rating: 5,
@@ -30,7 +49,7 @@ export default [
     previewUrl: '/assets/images/karen-review.png',
   },
   {
-    id: 4,
+    id: 6,
     name: 'Nicola Valentine',
     date: 'a year ago',
     rating: 5,
@@ -38,5 +57,25 @@ export default [
       "Always had a professional and excellent experience with Lisa & Scott who've looked after my teeth for 2 years now and so much healthier.",
     url: 'https://g.co/kgs/RRJBRYx',
     previewUrl: '/assets/images/nicola-review.png',
+  },
+  {
+    id: 7,
+    name: 'Maria Heasell',
+    date: '2 years ago',
+    rating: 5,
+    review:
+      'I had quite a lot of work on my teeth done by Scott. He put me totally at ease and I was really pleased with the work he did. I’ve been back for check ups since and he is always so friendly and welcoming. Would highly recommend!',
+    url: 'https://g.co/kgs/dx1FLCj',
+    previewUrl: '/assets/images/maria-review.png',
+  },
+  {
+    id: 8,
+    name: 'Wendy Gregory',
+    date: '2 years ago',
+    rating: 5,
+    review:
+      "Being a very nervous patient I can't praise Scott and the nursing team highly enough. I never thought I would say it but I no longer dread a trip to the dentist. The appointments have always been pain free and I am made aware of what is being done. Well done to you all.",
+    url: 'https://g.co/kgs/31z8iQs',
+    previewUrl: '/assets/images/wendy-review.png',
   },
 ];
