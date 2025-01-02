63746a38c3a67cc7b703d95282b620042bbf5234
Adam Young <adamyoungy678@gmail.com>
2025-01-02 12:08:39 +0000

    feat: update Home page metadata and replace FindUs component with FindUsSection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,6 @@ import dynamic from 'next/dynamic';
 // import Steps from '@/components/blocks/Steps/Steps';
 import AboutSection from '@/components/AboutSection/AboutSection';
 import GoogleReviews from '@/components/blocks/GoogleReviews/GoogleReviews';
-import Reviews from '@/components/blocks/Reviews/Review';
 import Welcome from '@/components/blocks/Welcome/Welcome';
 import ComingSoonSection from '@/components/ComingSoon/ComingSoon';
 import FindUs from '@/components/FindUs/FindUs';
@@ -17,10 +16,31 @@ import PromotionFAQ from '@/components/PromotionFAQ/PromotionFAQ';
 import PromotionForm from '@/components/PromotionForm/PromotionForm';
 import SmileTransformations from '@/components/SmileTransformations/SmileTransformations';
 import { VideoAnimationSection } from '@/components/VideoAnimationSection/VideoAnimationSection';
+import FindUsSection from '@/components/FindUsSection/FindUsSection';
 import DentallyTutorial from '@/components/DentallyTutorial/DentallyTutorial';
-const FloatingMenu = dynamic(async () => await import('@/components/FloatingMenu/FloatingMenu'), {
-  ssr: false,
-});
+
+export const metadata = {
+  title: 'Supernova Dental - 50% Off Your First Exam & Hygiene Appointment in Bridgwater',
+  description:
+    "Take advantage of Supernova Dental's 50% off offer on your first exam and hygiene appointment in Bridgwater. Book now and start your journey to a healthier smile!",
+  keywords:
+    'dental exam offer, dental hygiene offer, Invisalign, Bridgwater dentist, teeth whitening, healthy smile, dentist promotions',
+  openGraph: {
+    title: 'Supernova Dental - Exclusive Offers for New Patients',
+    description:
+      'Join Supernova Dental in Bridgwater with exclusive offers: 50% off your first dental exam and hygiene appointment. Book today!',
+    url: 'https://www.supernovadental.co.uk',
+    type: 'website',
+    images: [
+      {
+        url: '/assets/images/logo.png',
+        width: 500,
+        height: 500,
+        alt: 'Supernova Dental Logo',
+      },
+    ],
+  },
+};
 
 export default function Home() {
   return (
@@ -34,11 +54,9 @@ export default function Home() {
       <GoogleReviews />
       <Journey />
       <VideoAnimationSection />
-      {/* <InstagramSection /> */}
-      <FindUs />
+      <FindUsSection />
       <PromotionFAQ />
       <ComingSoonSection />
-      
 
       {/* <Reviews /> */}
       {/* <Services />
