e806e74f25e5971f8d80ca75ee41a9ef79c23d43
Adam Young <adamyoungy678@gmail.com>
2025-01-02 12:38:30 +0000

    fix: update copyright year and last updated date in PromotionFooter component

--- src/components/PromotionFooter/PromotionFooter.tsx
+++ src/components/PromotionFooter/PromotionFooter.tsx
@@ -4,9 +4,9 @@ export default function PromotionFooter() {
       <div className='flex justify-between flex-col lg:flex-row items-center gap-1'>
         <span className='text-center lg:text-left'>This practice is fully private with no NHS services available.</span>
         <div className='flex items-center gap-2 flex-col-reverse sm:flex-row'>
-          <span>&copy; Supernova Dental 2024</span>
+          <span>&copy; Supernova Dental 2025</span>
           <span className='hidden sm:block'>|</span>
-          <span>Website last updated: December 2024</span>
+          <span>Website last updated: January 2025</span>
         </div>
       </div>
     </footer>
