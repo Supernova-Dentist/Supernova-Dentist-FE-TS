697fe1a0bce4c2db146dd07d2593874bc9892ca8
Adam Young <adamyoungy678@gmail.com>
2025-01-02 16:03:52 +0000

    feat: adjust padding in PromotionFooter for improved layout

--- src/components/PromotionFooter/PromotionFooter.tsx
+++ src/components/PromotionFooter/PromotionFooter.tsx
@@ -1,6 +1,6 @@
 export default function PromotionFooter() {
   return (
-    <footer className='w-full p-6 sm:px-10 bg-grey text-cream text-sm'>
+    <footer className='w-full p-4 sm:px-10 bg-grey text-cream text-sm'>
       <div className='flex justify-between flex-col lg:flex-row items-center gap-1'>
         <span className='text-center lg:text-left'>This practice is fully private with no NHS services available.</span>
         <div className='flex items-center gap-2 flex-col-reverse sm:flex-row'>
