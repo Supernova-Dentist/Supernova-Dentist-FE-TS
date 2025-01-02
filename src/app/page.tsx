2bf13057325b202081491c9a76c070ab00260438
Adam Young <adamyoungy678@gmail.com>
2025-01-02 12:14:25 +0000

    refactor: remove unused imports and commented-out components in Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,7 @@
-import dynamic from 'next/dynamic';
-// import Services from '@/components/Services/Services';
-// import GalleryCarousel from '@/components/blocks/GalleryCarousel/GalleryCarousel';
-// import GoogleReviews from '@/components/blocks/GoogleReviews/GoogleReviews';
-// import Reviews from '@/components/blocks/Reviews/Review';
-// import Steps from '@/components/blocks/Steps/Steps';
 import AboutSection from '@/components/AboutSection/AboutSection';
 import GoogleReviews from '@/components/blocks/GoogleReviews/GoogleReviews';
 import Welcome from '@/components/blocks/Welcome/Welcome';
 import ComingSoonSection from '@/components/ComingSoon/ComingSoon';
-import FindUs from '@/components/FindUs/FindUs';
-import InstagramSection from '@/components/InstagramSection/InstagramSection';
 import { Journey } from '@/components/Journey/Journey';
 import Pricing from '@/components/Pricing/Pricing';
 import PromotionFAQ from '@/components/PromotionFAQ/PromotionFAQ';
@@ -57,12 +49,6 @@ export default function Home() {
       <FindUsSection />
       <PromotionFAQ />
       <ComingSoonSection />
-
-      {/* <Reviews /> */}
-      {/* <Services />
-      <Steps />
-      <GalleryCarousel />
-      <GoogleMap /> */}
     </main>
   );
 }
