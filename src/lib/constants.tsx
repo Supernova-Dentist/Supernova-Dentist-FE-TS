c079341838be7e28ba2914d664cab3bf870e0893
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2025-01-08 17:40:15 +0000

    add mapper obj to map account ids to pages

--- src/lib/constants.tsx
+++ src/lib/constants.tsx
@@ -120,3 +120,9 @@ export const pricing = [
   //   features: ['50% off new patient assessment', '50% off 1st routine hygiene appointment', 'Includes air polish'],
   // },
 ];
+
+// This could have potential risks exposing the app ids though
+export const instagramAccountsConfig = {
+  social: '17841470126685683',
+  'scott-young': '17841468241198700',
+};
