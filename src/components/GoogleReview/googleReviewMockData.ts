bb363bcd5fd673bf3570faf9ee02e57c52b260d6
Adam <adamyoungy678@gmail.com>
2025-02-16 21:28:21 +0000

    fix: update sitemap lastmod date and enhance Google review mock data with new reviews

--- src/components/GoogleReview/googleReviewMockData.ts
+++ src/components/GoogleReview/googleReviewMockData.ts
@@ -1,81 +1,142 @@
 export default [
   {
-    id: 1,
-    name: 'Anthony Hales',
+    id: 7,
+    name: 'Hollie Tudor-Smith',
+    date: 'yesterday',
+    rating: 5,
+    review: `I had a fantastic experience with Scott and the team at Supernova. He is extremely knowledgeable and experienced, and I felt confident in his care throughout my visit. The team is always accommodating, ensuring they cater to my availability and make the process as smooth as possible. Highly recommend!`,
+    url: 'https://g.co/kgs/DHd4rJi',
+    previewUrl: '/assets/images/wendy-review.png',
+  },
+  {
+    id: 8,
+    name: 'Patricia Boyce',
     date: 'a week ago',
     rating: 5,
-    review:
-      'I was so nervous about having dental treatment as I have had bad experiences before but when I signed up with supernova dental they was very understanding I had my first treatment on the 16th December they was amazing going back on the 19th for more treatment the nerves are gone I would highly recommend supernova dental ANTHONY BURNELL.',
-    url: 'https://g.co/kgs/vfJmXQt',
-    previewUrl: '/assets/images/anthony-review.png',
+    review: `This was my first visit and Scott the dentist was great, I am not one for going to the dentist , (bad experiences) he was very caring, plain speaking, told me what was needed but left it in my hands if I wanted it done. The practice is very relaxing, loved it. Although this was my first visit I would recommend Scott. Thank you Scott and your team, see you soon.`,
+    url: 'https://g.co/kgs/B5HdTA2',
+    previewUrl: '/assets/images/wendy-review.png',
+  },
+  {
+    id: 8,
+    name: 'Suzanne Radford',
+    date: 'a week ago',
+    rating: 5,
+    review: `I had an emergency appointment today with Scott due to having a broken tooth. Scott & his team greeted me with care and kindness. The repair was done efficiently & was pain free. I would recommend Supernova to anyone. A dentist you can trust.`,
+    url: 'https://g.co/kgs/G7x44rD',
+    previewUrl: '/assets/images/wendy-review.png',
+  },
+  {
+    id: 9,
+    name: 'Danny Shadbolt',
+    date: '2 weeks ago',
+    rating: 5,
+    review: `Got me in straight away and did the job I asked very clean and very clear instructions`,
+    url: 'https://g.co/kgs/gCiHVVT',
+    previewUrl: '/assets/images/wendy-review.png',
+  },
+  {
+    id: 9,
+    name: 'Dan Prowse',
+    date: '3 weeks ago',
+    rating: 5,
+    review: `Excellent experience; friendly and knowledgeable staff. Thank you!`,
+    url: 'https://g.co/kgs/UZ9W9gB',
+    previewUrl: '/assets/images/wendy-review.png',
+  },
+  {
+    id: 9,
+    name: 'Vanessa Felstead',
+    date: '3 weeks ago',
+    rating: 5,
+    review: `Extremely helpful & understanding.`,
+    url: 'https://g.co/kgs/KGWb6X7',
+    previewUrl: '/assets/images/wendy-review.png',
+  },
+  {
+    id: 7,
+    name: 'Jay M',
+    date: '4 weeks ago',
+    rating: 5,
+    review: `Highly recommend Supernova Dental. I experienced a friendly and professional welcome from Mia, professional and comforting experience with my appointment with Scott and a fantastic professional experience with Kayleigh for my Hygienist appointment, can honestly say it was the most thorough and pleasant hygienist appointment I've experienced. Fantastic team in a lovely/stylish practice. Thanks to all. Josh`,
+    url: 'https://g.co/kgs/dNE52b8',
+    previewUrl: '/assets/images/maria-review.png',
+  },
+  {
+    id: 8,
+    name: 'Dan Lathall',
+    date: 'a month ago',
+    rating: 5,
+    review: `I’m a new patient here, I saw an ad on online and wanted to take my dental care seriously now. What an experience so far, Loving every second of my journey.
+
+Professional curtesy call, text reminders and updates. Earlier appointment provided. nothing is too much for this team.
+
+I was seen by Kay and her nurse Tori.
+So professional. Walked me through everything she was doing.
+Any questions I had, any little issue I have been living with for years was no problem for her.
+Tori her nurse was amazing so friendly and removed all my excess dribble and I dribble a lot.
+
+I had my teeth all cleaned up and now they are exceptionally smooth.
+How did they make the water taste like cola too, this place just works magic ✨
+
+The Practice was so clean and well maintained, did I mention I love gold? The place is full of GOLD ! Love it ! Parking was super easy, all sign posted too.
+
+Well done Kay you got yourself a lifetime patient here. I even recommended my dad while I was there.`,
+    url: 'https://g.co/kgs/6mCqfem',
+    previewUrl: '/assets/images/wendy-review.png',
+  },
+  {
+    id: 6,
+    name: 'Matthew Vowles',
+    date: '5 weeks ago',
+    rating: 5,
+    review: `Great service with very friendly staff. The dental practice is very modern and spotless. Would definitely recommend.`,
+    url: 'https://g.co/kgs/qMxx5nf',
+    previewUrl: '/assets/images/nicola-review.png',
+  },
+  {
+    id: 5,
+    name: 'George Talbot',
+    date: '6 weeks ago',
+    rating: 5,
+    review: `Scott has been so helpful with sorting out my teeth. The practice is very nice and modern and all the staff are fantastic`,
+    url: 'https://g.co/kgs/qkPQuC1',
+    previewUrl: '/assets/images/karen-review.png',
   },
   {
     id: 2,
     name: 'Nigel White',
-    date: 'a week ago',
+    date: '8 weeks ago',
     rating: 5,
-    review:
-      'Absolutely first class emergency same day modern technology pain-free fantastic staff I would highly recommend this Dentist to my family and friends',
+    review: `Absolutely first class emergency same day modern technology pain-free fantastic staff I would highly recommend this Dentist to my family and friends`,
     url: 'https://g.co/kgs/hr2p2ee',
     previewUrl: '/assets/images/nigel-review.png',
   },
   {
+    id: 1,
+    name: 'Anthony Hales',
+    date: '9 weeks ago',
+    rating: 5,
+    review: `I was so nervous about having dental treatment as I have had bad experiences before but when I signed up with supernova dental they was very understanding I had my first treatment on the 16th December they was amazing going back on the 19th for more treatment the nerves are gone I would highly recommend Supernova dental ANTHONY BURNELL.`,
+    url: 'https://g.co/kgs/vfJmXQt',
+    previewUrl: '/assets/images/anthony-review.png',
+  },
+  {
     id: 3,
     name: 'Dean Whitaker',
-    date: 'a week ago',
+    date: '9 weeks ago',
     rating: 5,
-    review:
-      'I am not a great patient when it comes to the dentist. But I have found my experience at Supernova so far to be fabulous. The team there were very friendly and professional putting me at ease straight away. Scott has made me feel much more confident in visiting the dentist, and has been fantastic.',
+    review: `I am not a great patient when it comes to the dentist. But I have found my experience at Supernova so far to be fabulous. The team there were very friendly and professional putting me at ease straight away. Scott has made me feel much more confident in visiting the dentist, and has been fantastic.`,
     url: 'https://g.co/kgs/qiUhyye',
     previewUrl: '/assets/images/dean-review.png',
   },
   {
     id: 4,
     name: 'Ray Cox',
-    date: '2 weeks ago',
+    date: '9 weeks ago',
     rating: 5,
-    review: "Great staff and service - Best experience I've had and it was very informative.",
+    review: `Great staff and service - Best experience I've had and it was very informative.`,
     url: 'https://g.co/kgs/2fek9h5',
     previewUrl: '/assets/images/ray-review.png',
-  },
-  {
-    id: 5,
-    name: 'Karen Bligh',
-    date: 'a year ago',
-    rating: 5,
-    review:
-      'Absolutely amazing .. terrified of any dentist but Dr Scott Young made me feel relaxed and explained everything..  every step of the way .. and on my second appointment I had no anxiety no worry’s about anything especially any discomfort with treatment Thankyou',
-    url: 'https://g.co/kgs/NiDdSST',
-    previewUrl: '/assets/images/karen-review.png',
-  },
-  {
-    id: 6,
-    name: 'Nicola Valentine',
-    date: 'a year ago',
-    rating: 5,
-    review:
-      "Always had a professional and excellent experience with Lisa & Scott who've looked after my teeth for 2 years now and so much healthier.",
-    url: 'https://g.co/kgs/RRJBRYx',
-    previewUrl: '/assets/images/nicola-review.png',
-  },
-  {
-    id: 7,
-    name: 'Maria Heasell',
-    date: '2 years ago',
-    rating: 5,
-    review:
-      'I had quite a lot of work on my teeth done by Scott. He put me totally at ease and I was really pleased with the work he did. I’ve been back for check ups since and he is always so friendly and welcoming. Would highly recommend!',
-    url: 'https://g.co/kgs/dx1FLCj',
-    previewUrl: '/assets/images/maria-review.png',
-  },
-  {
-    id: 8,
-    name: 'Wendy Gregory',
-    date: '2 years ago',
-    rating: 5,
-    review:
-      "Being a very nervous patient I can't praise Scott and the nursing team highly enough. I never thought I would say it but I no longer dread a trip to the dentist. The appointments have always been pain free and I am made aware of what is being done. Well done to you all.",
-    url: 'https://g.co/kgs/31z8iQs',
-    previewUrl: '/assets/images/wendy-review.png',
   },
 ];
