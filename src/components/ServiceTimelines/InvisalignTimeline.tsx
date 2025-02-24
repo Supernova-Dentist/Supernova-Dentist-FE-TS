99ecef7d4d365e6bb2d0a95ef7c5946faa607d2c
Adam <adamyoungy678@gmail.com>
2025-02-24 21:49:46 +0000

    fix: update font sizes for Invisalign timeline titles for better readability

--- src/components/ServiceTimelines/InvisalignTimeline.tsx
+++ src/components/ServiceTimelines/InvisalignTimeline.tsx
@@ -66,13 +66,13 @@ export const InvisalignTimeline = ({ data, setImagesLoaded, imagesLoaded }: Time
               <div className='h-10 absolute left-3 md:left-3 w-10 rounded-full bg-white dark:bg-black flex items-center justify-center'>
                 <div className='h-4 w-4 rounded-full bg-neutral-200 dark:bg-neutral-800 border border-neutral-300 dark:border-neutral-700 p-2' />
               </div>
-              <h3 className='hidden md:block text-lg lg:text-2xl md:pl-20 md:text-5xl font-bold text-neutral-500 dark:text-neutral-500'>
+              <h3 className='hidden md:block text-3xl lg:text-3xl md:pl-20 md:text-5xl font-bold text-neutral-500 dark:text-neutral-500'>
                 {item.title}
               </h3>
             </div>
 
             <div className='relative pl-20 pr-4 md:pl-4 w-full'>
-              <h3 className='md:hidden block text-2xl mb-4 text-left font-bold text-neutral-500 dark:text-neutral-500'>
+              <h3 className='md:hidden block text-3xl md:text-5xl lg:text-3xl mb-4 text-left font-bold text-neutral-500 dark:text-neutral-500'>
                 {item.title}
               </h3>
               <div>{React.cloneElement(item.content as React.ReactElement, { onLoad: handleImageLoad })}</div>
