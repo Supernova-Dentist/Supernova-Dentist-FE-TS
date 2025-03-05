008e056a8623205cb4fd797175a3d9baefbe1a12
Adam Young <adamyoungy678@gmail.com>
2025-03-05 16:46:24 +0000

    feat: add ServiceForm component for Practice Plan sign-up on OurPractice page

--- src/app/practice/page.tsx
+++ src/app/practice/page.tsx
@@ -10,12 +10,14 @@ import { FeaturesSectionDemo } from '@/components/ui/bento-grid';
 import Link from 'next/link';
 import DentallyTutorial from '@/components/DentallyTutorial/DentallyTutorial';
 import { AboutPractice } from '@/components/AboutPractice/AboutPractice';
+import ServiceForm from '@/components/ServiceForm/ServiceForm';
 
 export const metadata: Metadata = {
   title: 'Supernova Dental | Your Patient Journey',
   description:
     'Visit Supernova Dental in Bridgwater for top-tier dental care with state-of-the-art technology. We prioritise your oral health and well-being every step of your patient journey.',
-    keywords: 'dentist in Bridgwater, emergency dentist Bridgwater, Supernova Dental, invisalign bridgwater, invisalign, cosmetic dentistry, dental care Bridgwater, emergency dental care, oral health, teeth whitening Bridgwater, dental practice Bridgwater',
+  keywords:
+    'dentist in Bridgwater, emergency dentist Bridgwater, Supernova Dental, invisalign bridgwater, invisalign, cosmetic dentistry, dental care Bridgwater, emergency dental care, oral health, teeth whitening Bridgwater, dental practice Bridgwater',
 };
 
 export default function OurPractice() {
@@ -26,6 +28,16 @@ export default function OurPractice() {
         title='Welcome to Supernova Dental'
         description='Exceptional dental care in a modern, welcoming environment.'
       /> */}
+      <div className='mt-4'>
+        <ServiceForm
+          serviceName='Practice Plan'
+          serviceChip='Join Our Practice Plan'
+          serviceDescription='Enjoy affordable, ongoing dental care with our Practice Plan, designed to keep your oral health in top condition.'
+          formTitle='Sign Up for Our Practice Plan'
+          formDescription='Join our Practice Plan from £5.99/month for children or £24.99/month for adults (16+). Enjoy routine check-ups, hygiene visits, emergency cover, and more. Sign up today for expert dental care and peace of mind.'
+        />
+      </div>
+
       {/* About Section */}
       <section>
         <div className='container mx-auto px-4 md:px-6 lg:px-8'>
@@ -43,8 +55,6 @@ export default function OurPractice() {
       <DentallyTutorial />
 
       <StickyCards />
-
-      <PromotionForm />
     </div>
   );
 }
