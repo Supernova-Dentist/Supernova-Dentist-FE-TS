b57209484168be4b1d633bb0a4563378f306ce27
Adam <adamyoungy678@gmail.com>
2025-03-10 21:33:36 +0000

    feat: add invis open day page

---
+++ src/components/blocks/InvisalignOpenDayContent/InvisalignOpenDayCard/InvisalignOpenDayCard.tsx
@@ -0,0 +1,75 @@
+import { cn } from '@/lib/utils';
+import { scrollToPromotionForm } from '@/utils/scrollToPromotionForm';
+import { motion } from 'framer-motion';
+import CountUp from 'react-countup';
+import { IoCheckmarkCircle } from 'react-icons/io5';
+import Button from '../../../Button/Button';
+
+type PricingCardProps = {
+  plan: string;
+  price: { number: number; text?: string };
+  offerPrice: number;
+  features: string[];
+  monthly: boolean;
+  isMiddle?: boolean;
+};
+
+export default function InvisalignOpenDayCard({
+  plan,
+  price,
+  offerPrice,
+  features,
+  isMiddle,
+  monthly,
+}: PricingCardProps) {
+  return (
+    <motion.div
+      initial={{ opacity: 0, y: 20 }} // Initial state for animation
+      whileInView={{ opacity: 1, y: 0 }} // Animate when in view
+      transition={{ duration: 0.5, ease: 'easeInOut' }} // Transition settings
+      className={cn(
+        'p-12 rounded-lg shadow-xl border border-solid border-black/10 relative overflow-hidden w-full sm:w-[500px] flex flex-col', // Increased width and padding
+        isMiddle ? 'bg-gray-900' : 'bg-gray-50',
+        'min-h-[600px]' // Increased height for more room
+      )}
+    >
+      <img src='/favicon.ico' alt='Supernova Dental Logo' className='absolute top-4 right-4 h-16 w-auto' /> {/* Slightly larger logo */}
+      <div className='flex flex-col flex-grow'>
+        <div className={cn('rounded-sm px-6 py-2 w-fit mb-6', isMiddle ? 'bg-gray-700 text-gray-50' : 'bg-gray-200')}>
+          <span className='text-lg'>{plan}</span> {/* Slightly larger text */}
+        </div>
+        <span className='text-lightGold'>{price?.text}</span>
+        <span className={cn('text-6xl mb-2 font-semibold', isMiddle ? 'text-gray-50' : null)}> {/* Increased font size */}
+          £
+          <CountUp
+            className=''
+            start={price.number}
+            end={monthly ? price.number : offerPrice}
+            duration={0.4}
+            useEasing={false}
+            preserveValue
+          />
+        </span>
+        <span className='text-gray-500 text-sm'>SAVING</span>
+        <div className={cn('bg-gray-200 w-full h-[1px] my-10', isMiddle ? 'bg-gray-700' : 'bg-gray-200')}></div> {/* Increased spacing */}
+
+        <ul className='flex flex-col gap-4'> {/* Increased gap */}
+          {features.map((feature) => (
+            <li className={cn('flex gap-2 items-center', isMiddle ? 'text-gray-50' : null)} key={feature}> {/* Increased gap */}
+              <IoCheckmarkCircle color={isMiddle ? 'var(--lightGold)' : ''} size={24} /> {/* Larger icon */}
+              {feature}
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className='mt-auto'>
+        {/* Added a div to contain the button */}
+        <Button
+          onClick={scrollToPromotionForm}
+          className={cn('w-full text-gray-50 rounded-sm mt-8 px-6 py-3', isMiddle ? 'bg-lightGold' : 'bg-gray-900')} 
+          text='Claim offer'
+        />
+      </div>
+    </motion.div>
+  );
+}
---
+++ src/components/blocks/InvisalignOpenDayContent/InvisalignOpenDayContent.tsx
@@ -0,0 +1,224 @@
+'use client';
+
+import PricingCards from '@/components/Pricing/PricingCards';
+import PromotionForm from '@/components/PromotionForm/PromotionForm';
+import ServiceForm from '@/components/ServiceForm/ServiceForm';
+import SmileTransformations from '@/components/SmileTransformations/SmileTransformations';
+import { VideoAnimationSection } from '@/components/VideoAnimationSection/VideoAnimationSection';
+import { motion, useScroll, useTransform } from 'framer-motion';
+import React, { type ReactNode, useRef } from 'react';
+import { FiArrowUpRight } from 'react-icons/fi';
+import InvisalignOpenDayCard from './InvisalignOpenDayCard/InvisalignOpenDayCard';
+import InvisalignOpenDayForm from './InvisalignOpenDayForm/InvisalignOpenDayForm';
+import InvisalignOpenDayJourney from './InvisalignOpenDayJourney/InvisalignOpenDayJourney';
+import Results from './Results/Results';
+
+const pricing = [
+  {
+    plan: 'Invisalign Deluxe',
+    price: { number: 1000, text: 'Over' },
+    offerPrice: 49,
+    features: [
+      'Free Invisalign assessment',
+      'Includes Dual arch Invisalign',
+      '£250 off Treatment cost',
+      'Free whitening worth £425',
+      'Free vivera retainers worth £399',
+    ],
+  },
+];
+
+export const InvisalignOpenDayContent = () => {
+  return (
+    <div className='bg-cream'>
+      <div className='p-4 md:p-12 min-h-screen flex flex-col justify-center items-center'>
+        <InvisalignOpenDayForm
+          serviceName='Invisalign'
+          serviceChip='Invisalign® Open Day'
+          serviceDescription='Exclusive open day bonuses and discounts worth over £1000 if you go ahead at the event'
+          formTitle='Sign Up for a Free Consultation'
+          formDescription='Meet with our experts, ask questions, and learn how Invisalign can transform your smile.'
+        />
+      </div>
+
+      <TextParallaxContent
+        imgUrl='/assets/images/invisalign_1.jpeg'
+        subheading='Huge Savings'
+        heading="Don't miss out."
+      >
+        <div className='flex items-center justify-center pt-8 pb-16'>
+          {pricing.map((planData, index) => (
+            <InvisalignOpenDayCard
+              key={index}
+              plan={planData.plan}
+              price={planData.price}
+              offerPrice={planData.offerPrice}
+              features={planData.features}
+              monthly={false}
+              isMiddle
+            />
+          ))}
+        </div>
+      </TextParallaxContent>
+      <TextParallaxContent
+        imgUrl='/assets/images/invisalign_2.jpg'
+        subheading='Treatment Cases'
+        heading='See how it can help.'
+      >
+        <div className='relative w-full max-w-[360px] mx-auto aspect-[9/16]'>
+          <video
+            className='w-full h-full object-cover rounded-lg lg:mt-8'
+            controls
+            preload='metadata'
+            src='/assets/videos/invisalign_treatment_cases.mp4'
+          />
+        </div>
+      </TextParallaxContent>
+      <TextParallaxContent imgUrl='/assets/images/invisalign_3.jpg' subheading='Modern' heading='See the journey.'>
+        <InvisalignOpenDayJourney />
+      </TextParallaxContent>
+      <TextParallaxContent
+        imgUrl='/assets/images/invisalign_4.jpg'
+        subheading='Results'
+        heading='See the difference we can make.'
+      >
+        <Results />
+        <VideoAnimationSection />
+      </TextParallaxContent>
+    </div>
+  );
+};
+
+const IMG_PADDING = 12;
+
+const TextParallaxContent = ({
+  imgUrl,
+  subheading,
+  heading,
+  children,
+  logoSrc,
+}: {
+  imgUrl: string;
+  subheading: string;
+  heading: string;
+  children: ReactNode;
+  logoSrc?: string;
+}) => {
+  return (
+    <div
+      style={{
+        paddingLeft: IMG_PADDING,
+        paddingRight: IMG_PADDING,
+      }}
+    >
+      <div className='relative h-[150vh]'>
+        <StickyImage imgUrl={imgUrl} />
+        <OverlayCopy heading={heading} subheading={subheading} logoSrc={logoSrc} />
+      </div>
+      {children}
+    </div>
+  );
+};
+
+const TextParallaxContentForm = ({
+  imgUrl,
+  subheading,
+  heading,
+  children,
+  logoSrc,
+}: {
+  imgUrl: string;
+  subheading: string;
+  heading: string;
+  children: ReactNode;
+  logoSrc?: string;
+}) => {
+  return (
+    <div
+      style={{
+        paddingLeft: IMG_PADDING,
+        paddingRight: IMG_PADDING,
+      }}
+    >
+      <div className='relative h-[150vh]'>
+        <InvisalignOpenDayForm
+          serviceName='Invisalign'
+          serviceChip='Invisalign® Open Day'
+          serviceDescription='Get a personalised Invisalign treatment plan and discover how clear aligners can transform your smile.'
+          formTitle='Sign up for your Free Invisalign Consultation'
+          formDescription='Sign up now for a no-obligation consultation with our expert team, and take the first step towards your perfect smile with Invisalign clear aligners.'
+        />
+      </div>
+      {children}
+    </div>
+  );
+};
+
+const StickyImage = ({ imgUrl }: { imgUrl: string }) => {
+  const targetRef = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: targetRef,
+    offset: ['end end', 'end start'],
+  });
+
+  const scale = useTransform(scrollYProgress, [0, 1], [1, 0.85]);
+  const opacity = useTransform(scrollYProgress, [0, 1], [1, 0]);
+
+  return (
+    <motion.div
+      style={{
+        backgroundImage: `url(${imgUrl})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        height: `calc(100vh - ${IMG_PADDING * 2}px)`,
+        top: IMG_PADDING,
+        scale,
+      }}
+      ref={targetRef}
+      className='sticky z-0 overflow-hidden rounded-3xl'
+    >
+      <motion.div
+        className='absolute inset-0 bg-neutral-950/70'
+        style={{
+          opacity,
+        }}
+      />
+    </motion.div>
+  );
+};
+
+const OverlayCopy = ({ subheading, heading, logoSrc }: { subheading: string; heading: string; logoSrc?: string }) => {
+  const targetRef = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: targetRef,
+    offset: ['start end', 'end start'],
+  });
+
+  const y = useTransform(scrollYProgress, [0, 1], [250, -250]);
+  const opacity = useTransform(scrollYProgress, [0.25, 0.5, 0.75], [0, 1, 0]);
+
+  return (
+    <motion.div
+      style={{
+        y,
+        opacity,
+      }}
+      ref={targetRef}
+      className='absolute left-0 top-0 flex h-screen w-full flex-col items-center justify-center text-white'
+    >
+      <p className='mb-2 text-center text-xl md:mb-4 md:text-3xl'>{subheading}</p>
+      <p className='text-center text-4xl font-bold md:text-7xl'>{heading}</p>
+      {logoSrc && <img src={logoSrc} className='w-auto h-auto max-w-full mt-4 md:w-32 md:h-32' alt='Logo' />}
+    </motion.div>
+  );
+};
+
+const ExampleContent = () => (
+  <InvisalignOpenDayForm
+    serviceName='Invisalign'
+    serviceChip='Invisalign® Open Day'
+    serviceDescription='Get a personalised Invisalign treatment plan and discover how clear aligners can transform your smile.'
+    formTitle='Sign up for your Free Invisalign Consultation'
+    formDescription='Sign up now for a no-obligation consultation with our expert team, and take the first step towards your perfect smile with Invisalign clear aligners.'
+  />
+);
