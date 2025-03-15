40501abe46e780a31478cf74c3cf1e3277af0eca
Adam Young <adamyoungy678@gmail.com>
2025-03-15 12:54:02 +0000

    feat: update InvisalignOpenDayForm to use new poster image for improved visuals

--- src/components/blocks/InvisalignOpenDayContent/InvisalignOpenDayForm/InvisalignOpenDayForm.tsx
+++ src/components/blocks/InvisalignOpenDayContent/InvisalignOpenDayForm/InvisalignOpenDayForm.tsx
@@ -147,7 +147,7 @@ export default function InvisalignOpenDayForm({
               </h2>
               <p className='text-muted-foreground md:text-2xl'>{serviceDescription}</p>
               <div className='hidden lg:flex items-center justify-center mx-auto gap-4 pt-4 pb-8'>
-                <img src='/assets/images/invisalign_1.jpg' alt='Invisalign Logo' className=' w-80' />
+                <img src='/assets/images/invisalign_open_Day_poster.jpg' alt='Invisalign Logo' className=' w-96' />
               </div>
             </div>
             <Card className=' max-h-[50rem] md:max-h-[40rem] mt-6 mx-auto lg:m-auto w-full max-w-lg bg-gray-50 shadow-2xl p-2 md:p-6 flex items-center justify-center'>
@@ -227,7 +227,11 @@ export default function InvisalignOpenDayForm({
             </Card>
 
             <div className='flex flex-col lg:hidden py-12 mt-4'>
-              <img src='/assets/images/invisalign_1.jpg' alt='Invisalign Logo' className=' w-80 mx-auto pb-4' />
+              <img
+                src='/assets/images/invisalign_open_Day_poster.jpg'
+                alt='Invisalign Logo'
+                className=' w-96 mx-auto pb-4'
+              />
               <img
                 src='/assets/images/supernova_dental_cover.png'
                 alt='Invisalign Logo'
