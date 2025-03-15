d91ea289b9615b85c820b87ca5eaef66993f6c35
Adam Young <adamyoungy678@gmail.com>
2025-03-15 12:54:07 +0000

    feat: add InvisalignOpenDayHero component for enhanced visual presentation

---
+++ src/components/blocks/InvisalignOpenDayContent/InvisalignOpenDayHero/InvisalignOpenDayHero.tsx
@@ -0,0 +1,58 @@
+'use client';
+import { motion, useMotionTemplate } from 'framer-motion';
+import React from 'react';
+import { ImagesSlider } from '../../../ui/images-slider';
+import { scrollToPromotionForm } from '@/utils/scrollToPromotionForm';
+import { FiArrowRight } from 'react-icons/fi';
+
+export function InvisalignOpenDayHero() {
+  const images = [
+    '/assets/images/invisalign_1.jpg',
+    '/assets/images/invisalign_2.jpg',
+    '/assets/images/invisalign_3.jpg',
+    '/assets/images/outerBuilding.jpg',
+  ];
+
+  const border = useMotionTemplate`1px solid #a4693d`;
+  const boxShadow = useMotionTemplate`0px 4px 24px #a4693d`;
+
+  return (
+    <ImagesSlider className='h-[100vh]' images={images}>
+      <motion.div
+        initial={{
+          opacity: 0,
+          y: -80,
+        }}
+        animate={{
+          opacity: 1,
+          y: 0,
+        }}
+        transition={{
+          duration: 0.6,
+        }}
+        className='z-50 flex flex-col justify-center items-center'
+      >
+        <motion.p className='font-bold text-xl md:text-6xl text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 py-4'>
+          Supernova Dental <br /> Invisalign Open Day <br /> April 2025
+        </motion.p>
+        <motion.button
+          onClick={scrollToPromotionForm}
+          style={{
+            border,
+            boxShadow,
+          }}
+          whileHover={{
+            scale: 1.015,
+          }}
+          whileTap={{
+            scale: 0.985,
+          }}
+          className='group relative flex w-fit items-center gap-1.5 lg:mt-4 rounded-full text-md md:text-lg lg:text-xl bg-gray-950/10 px-4 py-1.5 text-gray-50 transition-colors hover:bg-gray-950/50'
+        >
+          Sign Up Now
+          <FiArrowRight className='transition-transform rotate-90 lg:rotate-0 lg:group-hover:rotate-90 group-active:-rotate-12' />
+        </motion.button>
+      </motion.div>
+    </ImagesSlider>
+  );
+}
