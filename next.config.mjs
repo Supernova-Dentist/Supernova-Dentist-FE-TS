21d8abcf3e53d3b75bfd39dd5d1cb0941ff41aac
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2025-05-13 13:21:34 +0100

    feat: add 'cdninstagram.com' to image domains in Next.js config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    domains: ['cdninstagram.com', 'scontent-man2-1.cdninstagram.com'],
     remotePatterns: [
       {
         hostname: '*.cdninstagram.com',
