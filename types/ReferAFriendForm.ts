bf2a5dddeea25ea26949d8dc80d7892ae64278d4
Adam <adamyoungy678@gmail.com>
2025-05-22 08:21:57 +0100

    feat: update referrer name validation to handle empty input gracefully

--- types/ReferAFriendForm.ts
+++ types/ReferAFriendForm.ts
@@ -3,7 +3,11 @@ import { z } from 'zod';
 
 // For some reason zod not giving correct errors for when input are empty. Should get the default field is required message, so added min(1) for that
 export const referAFriendSignupSchema = z.object({
-  referrerName: z.string().min(1, 'Referrer name is required').max(99, 'Name too long'),
+  referrerName: z
+  .string()
+  .max(99, 'Name too long')
+  .default('') // so it doesn't fail on undefined
+  .transform((val) => val.trim() === '' ? 'NoFriendReferral' : val),
 
   fullname: z.string().min(1, 'Full name is required').max(99, 'Name too long'),
   email: z.string().min(1, 'Email is required').email(),
