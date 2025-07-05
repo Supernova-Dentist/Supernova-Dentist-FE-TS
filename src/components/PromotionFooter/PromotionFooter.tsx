95ba9545253305ff833cf962738e79268ea206ed
Adam Young <adamyoungy678@gmail.com>
2025-07-05 20:24:25 +0100

    Update website last updated date in PromotionFooter component

--- src/components/PromotionFooter/PromotionFooter.tsx
+++ src/components/PromotionFooter/PromotionFooter.tsx
@@ -20,7 +20,7 @@ export default function PromotionFooter() {
         <div className='flex items-center gap-2 flex-col-reverse sm:flex-row'>
           <span>&copy; 2025 Supernova Dental All Rights Reserved</span>
           <span className='hidden sm:block'>|</span>
-          <span>Website last updated: May 2025</span>
+          <span>Website last updated: June 2025</span>
         </div>
       </div>
     </div>
