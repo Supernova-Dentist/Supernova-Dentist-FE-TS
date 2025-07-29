b42acb38f4ded242b801e20c23e67d3a444db0e6
Adam <adamyoungy678@gmail.com>
2025-07-29 23:16:19 +0100

    feat: add Google Tag Manager integration to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -59,6 +59,17 @@ export default function RootLayout({
             gtag('config', 'AW-16737398524'); // Google Ads ID
           `}
         </Script>
+
+        <Script id='gtm-script' strategy='beforeInteractive'>
+          {`
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-5Q5NWLHG');
+  `}
+        </Script>
+
         <meta name='google-site-verification' content='6AoMb9jPZjKrBtnIYhIpHOb96jJ_QaDRMAIqUffMCMw' />
         <Script id='meta-pixel' strategy='beforeInteractive'>
           {`
@@ -100,6 +111,15 @@ export default function RootLayout({
         </noscript>
       </head>
       <body className={inter.className}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src='https://www.googletagmanager.com/ns.html?id=GTM-5Q5NWLHG'
+            height='0'
+            width='0'
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
         <MainNav />
         {children}
         <FloatingMenu />
