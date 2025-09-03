257a2dcd3d8648f2f45a39b5467b0a2c3fa71ff8
Adam <adamyoungy678@gmail.com>
2025-09-03 16:06:10 +0100

    feat: update cone-beam CT scan pricing and add referring dentists section

--- src/app/pricing/utils/pricingItems.ts
+++ src/app/pricing/utils/pricingItems.ts
@@ -18,8 +18,7 @@ export const pricingItems = [
 
       { description: 'Small X-Rays', price: 12, priceDescription: 'each' },
       { description: 'Large full mouth X-ray (OPG)', price: 50 },
-      { description: 'Cone-beam CT scan (without report)', price: 180 },
-      { description: 'Cone-beam CT scan (with report)', price: 270 },
+      { description: 'Cone-beam CT scan ', price: 225 },
     ],
   },
   {
@@ -238,6 +237,13 @@ discomfort.`,
     products: [
       { description: 'Dental Implant', priceDescriptionBefore: 'From', price: 1400 },
       { description: 'Dental Implant Crown', priceDescriptionBefore: 'From', price: 1350 },
+    ],
+  },
+  {
+    title: 'Referring Dentists',
+    products: [
+      { description: 'Cone-beam CT scan (without report)', price: 180 },
+      { description: 'Cone-beam CT scan (with report)', price: 270 },
     ],
   },
 ];
