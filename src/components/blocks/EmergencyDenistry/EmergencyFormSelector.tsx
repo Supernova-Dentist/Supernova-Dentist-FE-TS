2a744a3347c38ee830146778a81e949b58267dcf
Adam <adamyoungy678@gmail.com>
2025-09-20 23:42:50 +0100

    fix: add missing id attribute to EmergencyFormSelector component

--- src/components/blocks/EmergencyDenistry/EmergencyFormSelector.tsx
+++ src/components/blocks/EmergencyDenistry/EmergencyFormSelector.tsx
@@ -2,14 +2,14 @@
 
 import React, { useState } from 'react';
 import EmergencyToggleSection from './utils/emergency-form-toggle';
-import { NewEmergencyFormContent } from './utils/new-emergency-form-content';
 import { ExisitingEmergencyFormContent } from './utils/exisiting-emergency-form-content';
+import { NewEmergencyFormContent } from './utils/new-emergency-form-content';
 
 function EmergencyFormSelector() {
   const [isExistingPatient, setIsExistingPatient] = useState(false);
 
   return (
-    <div className='py-12 bg-cream'>
+    <div id='form' className='py-12 bg-cream'>
       <p className='text-md md:text-xl lg:text-2xl leading-tight text-center tracking-tight font-bold text-gold mt-4 mb-2'>
         Please fill in the Emergency Dentistry Triage Form below:
       </p>
