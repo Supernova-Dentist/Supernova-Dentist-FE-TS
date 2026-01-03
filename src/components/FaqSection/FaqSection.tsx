9689b26961e08a9d072e1daf80daa341e0b8a6f3
Adam <adamyoungy678@gmail.com>
2026-01-03 23:04:13 +0000

    feat: update background gradient in FaqSection component for improved aesthetics

--- src/components/FaqSection/FaqSection.tsx
+++ src/components/FaqSection/FaqSection.tsx
@@ -11,7 +11,7 @@ export default function  FaqSection({ faqItems, id }: { faqItems: any; id: strin
   });
 
   return (
-    <section id={id} ref={ref} className='w-full text-gray-900 bg-gradient-to-b from-white to-cream pb-4 md:pb-8'>
+    <section id={id} ref={ref} className='w-full text-gray-900 bg-white pb-4 md:pb-8'>
       <div className='px-4 md:px-6 w-full'>
         <motion.div
           className='flex flex-col items-center space-y-6 text-center'
