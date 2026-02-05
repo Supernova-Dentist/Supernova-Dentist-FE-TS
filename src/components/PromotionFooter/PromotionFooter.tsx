1c0b4fc5222956f121653aacb34b49251cf68e53
Adam <adamyoungy678@gmail.com>
2026-02-05 16:34:42 +0000

    fix: update website last updated date in PromotionFooter component

--- src/components/PromotionFooter/PromotionFooter.tsx
+++ src/components/PromotionFooter/PromotionFooter.tsx
@@ -40,7 +40,7 @@ export default function PromotionFooter() {
         <div className='flex items-center gap-2 flex-col-reverse sm:flex-row'>
           <span>&copy; 2025 Supernova Dental All Rights Reserved</span>
           <span className='hidden sm:block'>|</span>
-          <span>Website last updated: January 2026</span>
+          <span>Website last updated: February 2026</span>
         </div>
       </div>
     </div>
