5fd94bee326f2e0296e959434c2dd5ca0c97f9a6
Adam <adamyoungy678@gmail.com>
2026-02-13 16:19:59 +0000

    feat: update TeamMemberGrid with new image imports and enhance image handling

--- src/app/team/utils/TeamMemberGrid.tsx
+++ src/app/team/utils/TeamMemberGrid.tsx
@@ -4,74 +4,36 @@ import { Button } from '@/components/ui/button';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 
+// Import images to allow Next.js to generate blurDataURL automatically
+import adamImage from '../../../../public/assets/images/Headshots/adam-young-operations-director-supernova-dental-bridgwater.webp';
+import amyImage from '../../../../public/assets/images/Headshots/amy-skinner-front-of-house-supernova-dental-bridgwater.webp';
+import jacquelineImage from '../../../../public/assets/images/Headshots/dr-jacqueline-amarin-dentist-supernova-dental-bridgwater.webp';
+import saraImage from '../../../../public/assets/images/Headshots/dr-sara-banuri-implantologist-supernova-dental-bridgwater.webp';
+import souadImage from '../../../../public/assets/images/Headshots/dr-souad-maddi-dentist-supernova-dental-bridgwater.webp';
+import kayleighImage from '../../../../public/assets/images/Headshots/kayleigh_headshot.jpeg';
+import mariaImage from '../../../../public/assets/images/Headshots/maria-mihaela-morar-dental-nurse-supernova-dental-bridgwater.webp';
+import miaImage from '../../../../public/assets/images/Headshots/mia-coakley-lead-front-house-supernova-dental-bridgwater.webp';
+import vanessaImage from '../../../../public/assets/images/Headshots/vanessa-speier-trainee-dental-nurse-supernova-dental-bridgwater.webp';
+import victoriaImage from '../../../../public/assets/images/Headshots/victoria-hands-lead-dental-nurse-supernova-dental-bridgwater.webp';
+import scottImage from '../../../../public/assets/images/Headshots/dr-scott-young-principal-dentist-supernova-dental-bridgwater.webp';
+
 const teamMembers = [
-  {
-    name: 'Dr. Scott Young',
-    title: 'Principal Dentist/Owner',
-    image: '/assets/images/Headshots/dr-scott-young-principal-dentist-supernova-dental-bridgwater.webp',
-    slug: 'dr-scott-young',
-  },
-  {
-    name: 'Dr. Sara D Banuri',
-    title: 'Implantologist/Oral Surgeon',
-    image: '/assets/images/Headshots/dr-sara-banuri-implantologist-supernova-dental-bridgwater.webp',
-    slug: 'dr-sara-d-banuri',
-  },
-  {
-    name: 'Dr. Souad Maddi',
-    title: 'General and Restorative Dentist',
-    image: '/assets/images/Headshots/dr-souad-maddi-dentist-supernova-dental-bridgwater.webp',
-    slug: 'dr-souad-maddi',
-  },
+  { name: 'Dr. Scott Young', title: 'Principal Dentist/Owner', image: scottImage, slug: 'dr-scott-young' },
+  { name: 'Dr. Sara D Banuri', title: 'Implantologist/Oral Surgeon', image: saraImage, slug: 'dr-sara-d-banuri' },
+  { name: 'Dr. Souad Maddi', title: 'General and Restorative Dentist', image: souadImage, slug: 'dr-souad-maddi' },
   {
     name: 'Dr. Jacqueline Amarin',
     title: 'General and Restorative Dentist',
-    image: '/assets/images/Headshots/dr-jacqueline-amarin-dentist-supernova-dental-bridgwater.webp',
+    image: jacquelineImage,
     slug: 'dr-jacqueline-amarin',
   },
-
-  {
-    name: 'Kayleigh Dowdle',
-    title: 'Dental Therapist',
-    image: '/assets/images/Headshots/kayleigh-dowdle-supernova-dental-bridgwater.webp',
-    slug: 'kayleigh-dowdle',
-  },
-  {
-    name: 'Adam Young',
-    title: 'Operations Director',
-    image: '/assets/images/Headshots/adam-young-operations-director-supernova-dental-bridgwater.webp',
-    slug: 'adam-young',
-  },
-  {
-    name: 'Victoria Hands',
-    title: 'Lead Dental Nurse',
-    image: '/assets/images/Headshots/victoria-hands-lead-dental-nurse-supernova-dental-bridgwater.webp',
-    slug: 'victoria-hands',
-  },
-  {
-    name: 'Maria-Mihaela Morar',
-    title: 'Dental Nurse',
-    image: '/assets/images/Headshots/maria-mihaela-morar-dental-nurse-supernova-dental-bridgwater.webp',
-    slug: 'maria-mihaela-morar',
-  },
-  {
-    name: 'Mia Coakley',
-    title: 'Lead Front of House',
-    image: '/assets/images/Headshots/mia-coakley-lead-front-house-supernova-dental-bridgwater.webp',
-    slug: 'mia-coakley',
-  },
-  {
-    name: 'Amy Skinner',
-    title: 'Receptionist',
-    image: '/assets/images/Headshots/amy-skinner-front-of-house-supernova-dental-bridgwater.webp',
-    slug: 'amy-skinner',
-  },
-  {
-    name: 'Vanessa Speier',
-    title: 'Trainee Dental Nurse',
-    image: '/assets/images/Headshots/vanessa-speier-trainee-dental-nurse-supernova-dental-bridgwater.webp',
-    slug: 'vanessa-speier',
-  },
+  { name: 'Kayleigh Dowdle', title: 'Dental Therapist', image: kayleighImage, slug: 'kayleigh-dowdle' },
+  { name: 'Adam Young', title: 'Operations Director', image: adamImage, slug: 'adam-young' },
+  { name: 'Victoria Hands', title: 'Lead Dental Nurse', image: victoriaImage, slug: 'victoria-hands' },
+  { name: 'Maria-Mihaela Morar', title: 'Dental Nurse', image: mariaImage, slug: 'maria-mihaela-morar' },
+  { name: 'Mia Coakley', title: 'Lead Front of House', image: miaImage, slug: 'mia-coakley' },
+  { name: 'Amy Skinner', title: 'Receptionist', image: amyImage, slug: 'amy-skinner' },
+  { name: 'Vanessa Speier', title: 'Trainee Dental Nurse', image: vanessaImage, slug: 'vanessa-speier' },
 ];
 
 export default function TeamMemberGrid() {
@@ -84,20 +46,21 @@ export default function TeamMemberGrid() {
   return (
     <section className='container mx-auto py-12 md:py-20'>
       <div className='grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
-        {teamMembers.map((member) => (
+        {teamMembers.map((member, index) => (
           <div
             key={member.name}
             className='group relative overflow-hidden rounded-lg bg-white shadow-lg transition-transform transform hover:scale-105'
           >
-            <div className='overflow-hidden'>
+            <div className='overflow-hidden rounded-lg'>
               <Image
                 src={member.image}
                 alt={`${member.name}, ${member.title} at Supernova Dental in Bridgwater`}
                 width={1800}
                 height={2700}
-                layout={'responsive'}
                 quality={75}
-                className='object-cover h-auto w-auto rounded-lg p-4'
+                placeholder='blur'
+                className='object-cover w-full h-full rounded-lg'
+                priority={index < 4} // first row priority
               />
             </div>
             <div className='p-6'>
