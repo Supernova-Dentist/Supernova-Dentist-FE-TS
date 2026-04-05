bd6ee49378ed516f7caab5520a20c86ac01a02c6
Adam <adamyoungy678@gmail.com>
2026-04-05 16:56:48 +0100

    feat: update service description and form description in DentalImplantsContent component to include '6'

--- src/components/blocks/DentalImplantsContent/DentalImplantsContent.tsx
+++ src/components/blocks/DentalImplantsContent/DentalImplantsContent.tsx
@@ -23,9 +23,9 @@ export default function DentalImplantsContent() {
         <ServiceForm
           serviceName='Dental Implants Journey – Full-Arch (All-on-4/6) in Somerset'
           serviceChip='Dental Implant Consultation'
-          serviceDescription='Discover single-tooth implants and advanced full-arch solutions such as All-on-4 delivered by our experienced implantologist at Supernova Dental in Bridgwater, Somerset.'
+          serviceDescription='Discover single-tooth implants and advanced full-arch solutions such as All-on-4/6 delivered by our experienced implantologist at Supernova Dental in Bridgwater, Somerset.'
           formTitle='Register for your Dental Implant Consultation'
-          formDescription='Book a no-obligation consultation with our expert implantologist, and explore single implants or full-arch options like All-on-4 for a permanent, natural-looking smile.'
+          formDescription='Book a no-obligation consultation with our expert implantologist, and explore single implants or full-arch options like All-on-4/6 for a permanent, natural-looking smile.'
           // imgSrc='/assets/images/dr-cameran-armaghani-implantologist-supernova-dental-bridgwater.webp'
           formId='implants-enquiry-form'
           eventType='DentalImplantsLead'
