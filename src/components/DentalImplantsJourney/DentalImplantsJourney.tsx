644cee121cc0423f4b7aad09247b3f680973e1ec
Adam <adamyoungy678@gmail.com>
2026-04-05 16:50:32 +0100

    feat: remove TypeScript error comments for image imports in DentalImplantsJourney component

--- src/components/DentalImplantsJourney/DentalImplantsJourney.tsx
+++ src/components/DentalImplantsJourney/DentalImplantsJourney.tsx
@@ -5,12 +5,8 @@ import React from 'react';
 import { DentalImplantTimeline } from '../ServiceTimelines/DentalImplantTimeline';
 
 // Static imports so we can use intrinsic width/height
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-expect-error
 import ConsultationImage from '../../../public/assets/images/cbct_supernova_dental_bridgwater.webp';
 import RestorationImage from '../../../public/assets/images/Dr-Cameran-Armaghani-implant-case-study.webp';
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-expect-error
 import SurgeryImage from '../../../public/assets/images/sunlight_suite_supernova_dental_bridgwater.webp';
 
 export function DentalImplantsJourney() {
