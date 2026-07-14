22db6eb51974c71028f799493e07d988fe95d90f
Adam <adamyoungy678@gmail.com>
2026-07-14 17:45:57 +0100

    fix: Update backend URL in ServiceForm to use new environment variable

--- src/components/ServiceForm/ServiceForm.tsx
+++ src/components/ServiceForm/ServiceForm.tsx
@@ -100,7 +100,7 @@ export default function ServiceForm({
       };
 
       // Backend request
-      const backendRes = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/promotion`, {
+      const backendRes = await fetch(`${process.env.NEXT_PUBLIC_SUPERNOVA_BE_URL}/promotion`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(dataWithTracking),
