45d2d52fb9c6767af59887a68fe618afb4fa0c0c
Adam <adamyoungy678@gmail.com>
2026-07-19 19:45:37 +0100

    feat: update CareersOutlineCards to include Associate Dentist card with responsive layout

--- src/components/OutlineCards/CareersOutlineCards.tsx
+++ src/components/OutlineCards/CareersOutlineCards.tsx
@@ -6,8 +6,12 @@ import { FiArrowRight } from 'react-icons/fi';
 export const CareersOutlineCards = () => {
   return (
     <section className='overflow-hidden max-w-5xl md:max-w-6xl lg:max-w-7xl px-8 py-10'>
-      <div className='mx-auto  items-center justify-center gap-8 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4'>
-        <Card title='Dental Nurse' href='/careers/dental-nurse' bgUrl='/assets/images/dental_nurses_1.jpeg' />
+      <div className='mx-auto  items-center justify-center gap-8 grid grid-cols-1'>
+        <Card
+          title='Associate Dentist'
+          href='/careers/associate-dentist-bridgwater'
+          bgUrl='/assets/images/invisalign_landscape_2.jpg'
+        />
       </div>
     </section>
   );
@@ -21,7 +25,7 @@ const Card = ({ bgUrl, title, href }: { bgUrl: string; title: string; href: stri
         backgroundImage: `url(${bgUrl})`,
         backgroundPosition: 'center',
       }}
-      className='bg-cover bg-no-repeat outline-card flex aspect-[4/4] w-full flex-col justify-end overflow-hidden rounded-lg bg-neutral-400  shadow-xl shadow-neutral-900/30'
+      className='bg-cover bg-no-repeat outline-card flex aspect-[4/4] max-w-[30rem] mx-auto w-full flex-col justify-end overflow-hidden rounded-lg bg-neutral-400  shadow-xl shadow-neutral-900/30'
     >
       <div className='pointer-events-none flex items-center justify-between bg-gradient-to-t from-black to-black/0 p-6 pt-8 text-xl font-medium text-white md:text-2xl'>
         <h3>{title}</h3>
