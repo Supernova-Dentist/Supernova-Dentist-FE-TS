a418c78326203dcab11a3c688d19c899d234da4b
Adam <adamyoungy678@gmail.com>
2026-07-22 13:08:24 +0100

    fix: update enquiryType and source values in form submission for Associate Dentist application

--- src/app/careers/associate-dentist-bridgwater-somerset/utils/associate-dentist-bridgwater-form-content.tsx
+++ src/app/careers/associate-dentist-bridgwater-somerset/utils/associate-dentist-bridgwater-form-content.tsx
@@ -67,14 +67,14 @@ export function AssociateDentistBridgwaterFormContent() {
     console.log('submitting');
     setLoading(true);
 
-    // const decodedSource = decodeURIComponent(pathname);
-    // const cleanedSource = decodedSource.startsWith('/') ? decodedSource.slice(1) : decodedSource;
+    const decodedSource = decodeURIComponent(pathname);
+    const cleanedSource = decodedSource.startsWith('/') ? decodedSource.slice(1) : decodedSource;
 
     try {
       const formData = new FormData();
       // Add referralType first
-      formData.append('enquiryType', 'Dentist-Application');
-      formData.append('source', 'Dentist Application');
+      formData.append('enquiryType', 'Dentist Application');
+      formData.append('source', cleanedSource);
       // Then append the rest of the fields
       for (const key in data) {
         if (key !== 'referralType' && data[key]) {
